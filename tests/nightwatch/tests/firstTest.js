337f57bee3fbcbfff8778414028a786443e8e899
Hasan Farook <hasanfar@gmail.com>
2021-09-13 23:21:45 -0400

    commenting test

---
+++ tests/nightwatch/tests/firstTest.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "My first test case"(browser) {
+    browser.url(`${process.env.APPLICATION_ENDPOINT}`);
+    // .waitForElementVisible(".lander")
+    // .assert.containsText(".lander", "APS Submission App")
+    // .saveScreenshot("tests_output/My_first_test_case_screenshot.png");
+  },
+};
