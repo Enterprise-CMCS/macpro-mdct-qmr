e74e3063c3071d0ea9351cffb5e310c27f78f981
Guli <gabudusalamu@collabralink.com>
2021-09-17 05:25:15 -0400

    finished the step definition for two scenario

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -6,3 +6,10 @@ module.exports = (on, config) => {
 
   return config;
 }
+
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
