64109237613aded5561509704c45cac70b912163
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-01 12:24:00 -0600

    Refactor

--- services/ui-src/src/containers/Login/Login.js
+++ services/ui-src/src/containers/Login/Login.js
@@ -45,7 +45,7 @@ export default function Login() {
         </Bootstrap.Col>
         <Bootstrap.Row className="mt-4">
           <Bootstrap.Col>
-            {!isAuthenticated ? <LocalLogins /> : ""}
+            {!isAuthenticated && <LocalLogins />}
           </Bootstrap.Col>
         </Bootstrap.Row>
       </Bootstrap.Row>
