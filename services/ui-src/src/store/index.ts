641921ab2e3a7837d8ef82b83a6244b892903f09
James Dinh <>
2021-11-01 17:13:19 -0700

    fixed absolute import in indext.ts file

--- services/ui-src/src/store/index.ts
+++ services/ui-src/src/store/index.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./reducers";
+import rootReducer from "@/src/store/reducers";
 
 // Consolidate middleware
 let middlewareArray = [thunk];
