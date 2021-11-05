35739f18baeffbccdb4e9cd67f19e25a400c91ae
James Dinh <>
2021-11-05 10:26:14 -0700

    fixed issue where loader button test were failing because there are multiple loader buttons and we imported the incorrect one

--- services/ui-src/src/components/LoaderButton/LoaderButton.test.tsx
+++ services/ui-src/src/components/LoaderButton/LoaderButton.test.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import LoaderButton from "components/LoaderButton";
+import LoaderButton from "./LoaderButton";
 import { render } from "@testing-library/react";
 
 describe("Test LoaderButton.js", () => {
   test("Check the main element, with classname LoaderButton, exists", () => {
-    const { container } = render(
-      <LoaderButton isLoading={true} type="submit" disabled={false} />
-    );
+    const { getByTestId } = render(<LoaderButton />);
 
-    expect(container.getElementsByClassName("LoaderButton")).toHaveLength(1);
+    expect(getByTestId("LoaderButton")).toBeVisible();
   });
 });
