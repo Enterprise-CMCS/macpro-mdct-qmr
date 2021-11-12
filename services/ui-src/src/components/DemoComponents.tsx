0abb370f615867c5c51c2b962464c629162091f9
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-12 12:13:45 -0700

    Add TextArea comp

--- services/ui-src/src/components/DemoComponents.tsx
+++ services/ui-src/src/components/DemoComponents.tsx
@@ -1,9 +1,20 @@
+import React from "react";
 import { TextArea } from "components/Inputs/TextArea";
-
+import * as CUI from "@chakra-ui/react";
 
 export default function DemoComponents(): JSX.Element {
-  return (<>
-  <TextArea/>
-  <h2>Components</h2>
-  </>)
+  const [textAreaValue, setTextAreaValue] = React.useState("");
+
+  return (
+    <CUI.Container>
+      <CUI.Heading size="md">Components</CUI.Heading>
+      <TextArea
+        value={textAreaValue}
+        onChange={(e) => setTextAreaValue(e.target.value)}
+        label="test text area"
+        helperText="put in something here"
+        errorMessage="You dum"
+      />
+    </CUI.Container>
+  );
 }
--- services/ui-src/src/components/Inputs/TextArea/index.tsx
+++ services/ui-src/src/components/Inputs/TextArea/index.tsx
@@ -1,10 +1,43 @@
-import * as MUI from "@mui/material";
+import * as CUI from "@chakra-ui/react";
 
-
-export const TextArea = () => {
-    return (<MUI.TextareaAutosize
-    aria-label="empty textarea"
-    placeholder="We're awesome!!!"
-    style={{ width: 200 }}
-  />)
+interface TextAreaProps {
+  label?: string;
+  helperText?: string;
+  formControlProps?: CUI.FormControlProps;
+  formLabelProps?: CUI.FormLabelProps;
+  textAreaProps?: CUI.TextareaProps;
+  placeholder?: string;
+  value: string;
+  isInvalid?: boolean;
+  errorMessage?: string;
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
 }
+
+export const TextArea = ({
+  label,
+  helperText,
+  value,
+  onChange,
+  formControlProps,
+  formLabelProps,
+  placeholder,
+  textAreaProps,
+  isInvalid,
+  errorMessage,
+}: TextAreaProps) => {
+  return (
+    <CUI.FormControl {...formControlProps} isInvalid={isInvalid}>
+      {label && <CUI.FormLabel {...formLabelProps}>{label}</CUI.FormLabel>}
+      <CUI.Textarea
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        {...textAreaProps}
+      />
+      <CUI.FormErrorMessage>
+        {errorMessage || "An Error Occured"}
+      </CUI.FormErrorMessage>
+      {helperText && <CUI.FormHelperText>{helperText}</CUI.FormHelperText>}
+    </CUI.FormControl>
+  );
+};
