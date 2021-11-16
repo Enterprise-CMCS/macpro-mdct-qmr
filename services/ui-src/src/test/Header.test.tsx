e6bc5e0184767483018182d4c77273316e530315
James Dinh <>
2021-11-16 08:32:03 -0800

    remove unused enzyme tests from header

--- services/ui-src/src/test/Header.test.tsx
+++ services/ui-src/src/test/Header.test.tsx
@@ -1,8 +1,5 @@
 import { render } from "@testing-library/react";
 import Header from "components/Header";
-// import { UsaBanner } from "@cmsgov/design-system";
-// import QMRLogo from "components/QMRLogo";
-// import * as Bootstrap from "react-bootstrap";
 
 jest.mock("react-redux", () => ({
   ...jest.requireActual("react-redux"),
@@ -15,12 +12,4 @@ describe("Test Header.tsx", () => {
   test("Check that the header exists", () => {
     expect(screen.getByText("My Account")).toBeVisible();
   });
-
-  // test("Check that header is rendering its children", () => {
-  //   expect(screen.find(UsaBanner).length).toBe(1);
-  //   expect(screen.find(QMRLogo).length).toBe(1);
-  //   expect(screen.find(Bootstrap.Navbar).length).toBe(1);
-  //   expect(screen.find(Bootstrap.NavDropdown).length).toBe(1);
-  //   expect(screen.find({ title: "My Account" }).length).toBe(1);
-  // });
 });
