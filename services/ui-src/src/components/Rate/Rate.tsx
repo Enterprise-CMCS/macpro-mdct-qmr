ffdf0a37291992b969d5ec273c512e27b2920bda
hasanfar <hasanfar@gmail.com>
2021-11-17 13:13:35 -0500

    adding rate component shell

---
+++ services/ui-src/src/components/Rate/Rate.tsx
@@ -0,0 +1,25 @@
+import * as CUI from "@chakra-ui/react";
+import * as Inputs from "components/Inputs";
+
+export function Rate(): JSX.Element {
+    // interface TextInputProps extends InputWrapperProps {
+
+    // }
+    return (
+        <CUI.Container mb="12">
+            <CUI.Stack spacing="4">
+                <Inputs.TextInput 
+                    label="Desribe the rate:"
+                    value={"ADSD"}
+                    onChange={() => ""}
+                    isInvalidFunc={(value) => String(value).length > 3}
+                    helperText="For example,specify the age groups and whether you are rep[orting on a certain indicator:"
+                    errorMessage="Text is too long"
+                ></Inputs.TextInput>
+            </CUI.Stack>
+            <CUI.Stack spacing="4">
+                
+            </CUI.Stack>
+        </CUI.Container>
+    );
+}
