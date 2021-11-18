25f3fb4dc80405992f9b9aed3ce3f0918e566dca
gitsierra <35947499+gitsierra@users.noreply.github.com>
2021-11-18 14:06:40 -0500

    Oy2 8204 Number w/ mask - Question Type Component (#60)
    * starting number input component
    
    * Still working on onchange
    
    * fix number input props
    
    * Working on displaying value with decimals percent
    
    * fixed masking
    
    * revert to number
    
    * change rounding to floor
    
    * added percent, WIP tests
    
    * Get component tests working
    
    * test mask util
    
    * Merged master and changed label
    
    * prettier
    
    * remove redundant logic
    
    * Do Validation in onChange
    
    * prettier
    
    * more prettier
    
    * prettier?
    
    * prettier for real
    
    * Why does ben stink?
    
    * regex and shifting
    
    * prettier
    
    * add regex test coverage
    
    * prettier
    
    * add test to demo page
    
    Co-authored-by: Andrew Belzer <abelzer@collabralink.com>

---
+++ services/ui-src/src/components/Inputs/NumberInput/index.tsx
@@ -0,0 +1,57 @@
+import * as CUI from "@chakra-ui/react";
+import { InputWrapper, InputWrapperProps } from "components/InputWrapper";
+
+interface NumberInputProps extends InputWrapperProps {
+  placeholder?: string;
+  value: string;
+  onChange: (s: string) => void;
+  numberInputProps?: CUI.InputProps;
+  displayPercent?: boolean;
+}
+
+export const decimalMask = (inputValue: string): boolean => {
+  return /^\d*\.?\d{0,4}$/.test(inputValue);
+};
+
+export const NumberInput = ({
+  numberInputProps,
+  placeholder,
+  onChange,
+  value,
+  isInvalidFunc,
+  displayPercent,
+  ...rest
+}: NumberInputProps) => {
+  let isInvalid = false;
+  if (isInvalidFunc) {
+    isInvalid = isInvalidFunc(value);
+  }
+
+  const handleChange = (v: string) => {
+    if (decimalMask(v)) {
+      onChange(v);
+    }
+  };
+
+  return (
+    <InputWrapper {...rest} isInvalid={isInvalid}>
+      <CUI.InputGroup>
+        <CUI.Input
+          type="text"
+          placeholder={placeholder ?? ""}
+          onChange={(e) => handleChange(e.target.value)}
+          value={value}
+          {...numberInputProps}
+        />
+        {displayPercent && (
+          <CUI.InputRightElement
+            pointerEvents="none"
+            color="black.300"
+            fontSize="1.3em"
+            children="%"
+          />
+        )}
+      </CUI.InputGroup>
+    </InputWrapper>
+  );
+};
