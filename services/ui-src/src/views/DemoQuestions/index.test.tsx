811baf66a859028060895a33078bc161ea0b8d8f
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-29 16:51:18 -0700

    Add Demo Questions Page test

---
+++ services/ui-src/src/views/DemoQuestions/index.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { DemoQuestions } from "views";
+
+describe("Test Demo Questions Component", () => {
+  it("renders a text area when question 2 is answered yes", async () => {
+    render(<DemoQuestions />);
+    userEvent.click(screen.getByText("I am reporting provisional data"));
+    expect(
+      screen.getByLabelText("I am reporting provisional data")
+    ).toBeChecked();
+    expect(
+      await screen.findByLabelText(
+        "Please provide additional information such as when the data will be final and if you plan to modify the data reported here:"
+      )
+    ).toBeInTheDocument();
+  });
+});
