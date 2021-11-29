00d870a1a2d446bd5914dfce040742be5b5aa8b2
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-29 12:00:50 -0700

    Add auth something

--- services/ui-src/src/components/AuthenticatedRoute/index.tsx
+++ services/ui-src/src/components/AuthenticatedRoute/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import { Route, Redirect, useLocation } from "react-router-dom";
+import { Auth } from "aws-amplify";
 
 export function AuthenticatedRoute({
   children,
   ...rest
 }: IAuthenticatedRouteProps): JSX.Element {
   const { pathname, search } = useLocation();
-  const isAuthenticated = false;
+  const isAuthenticated = true;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const auth = await Auth.currentAuthenticatedUser();
+        console.log({ auth });
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
+
   return (
     <Route {...rest}>
       {isAuthenticated ? (
