a4c9d58617bb320389cf4f230f65d99c708989e1
James Dinh <jdinh8124@gmail.com>
2021-12-02 12:34:17 -0800

    comment out rate component as the changes to inputs causing issues

--- services/ui-src/src/components/Rate/index.tsx
+++ services/ui-src/src/components/Rate/index.tsx
@@ -1,5 +1,5 @@
 import * as CUI from "@chakra-ui/react";
-import * as Inputs from "components/Inputs";
+// import * as Inputs from "components/Inputs";
 
 export interface IRate {
   label?: string;
@@ -9,32 +9,32 @@ export interface IRate {
   id: number;
 }
 
-type RateTypes = "numerator" | "denominator" | "rate";
+// type RateTypes = "numerator" | "denominator" | "rate";
 
 interface Props {
   rates: IRate[];
   updateRates: React.Dispatch<React.SetStateAction<IRate[]>>;
 }
+// , updateRates
+export function Rate({ rates }: Props) {
+  // const updateRate = (rateType: RateTypes, index: number, newValue: string) => {
+  //   const newRates = [...rates];
+  //   newRates[index][rateType] = newValue;
+  //   updateRates(newRates);
+  // };
 
-export function Rate({ rates, updateRates }: Props) {
-  const updateRate = (rateType: RateTypes, index: number, newValue: string) => {
-    const newRates = [...rates];
-    newRates[index][rateType] = newValue;
-    updateRates(newRates);
-  };
-
-  const regex = /^-?\d*\.?\d{0,4}$/;
+  // const regex = /^-?\d*\.?\d{0,4}$/;
 
   return (
     <>
-      {rates.map((rate, index) => (
+      {rates.map((rate) => (
         <CUI.Stack key={rate.id}>
           {rate.label && (
             <CUI.FormLabel fontWeight={700}>{rate.label}</CUI.FormLabel>
           )}
           <CUI.HStack spacing={2}>
-            <Inputs.NumberInput
-              value={rate.numerator}
+            {/* <Inputs.NumberInput
+              // value={rate.numerator}
               onChange={(e) =>
                 regex.test(e.target.value)
                   ? updateRate("numerator", index, e.target.value)
@@ -43,7 +43,7 @@ export function Rate({ rates, updateRates }: Props) {
               label="Numerator"
             />
             <Inputs.NumberInput
-              value={rate.denominator}
+              // value={rate.denominator}
               onChange={(e) =>
                 regex.test(e.target.value)
                   ? updateRate("denominator", index, e.target.value)
@@ -52,14 +52,14 @@ export function Rate({ rates, updateRates }: Props) {
               label="Denominator"
             />
             <Inputs.NumberInput
-              value={rate.rate}
+              // value={rate.rate}
               onChange={(e) =>
                 regex.test(e.target.value)
                   ? updateRate("rate", index, e.target.value)
                   : null
               }
               label="Rate"
-            />
+            /> */}
           </CUI.HStack>
         </CUI.Stack>
       ))}
