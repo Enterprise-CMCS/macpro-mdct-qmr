63ec02a24990025216fdc0ecd4c0fd87a1883c51
13bfrancis <13bfrancis@gmail.com>
2021-12-03 02:29:27 -0500

    add string typechecking to custom register hook

--- services/ui-src/src/components/CoreQuestions/AreYouReporting/index.tsx
+++ services/ui-src/src/components/CoreQuestions/AreYouReporting/index.tsx
@@ -1,6 +1,7 @@
 import { CoreQuestionWrapper } from "..";
 import * as QMR from "components/Inputs/RadioButton";
 import { useCustomRegister } from "hooks/useCustomRegister";
+import { DemoFormType } from "views/DemoQuestions/DemoFormType";
 
 interface Props {
   options: QMR.RadioButtonOption[];
@@ -10,7 +11,7 @@ export const AreYouReporting = ({ options }: Props) => {
   return (
     <CoreQuestionWrapper label="Are you reporting on this measure?">
       <QMR.RadioButton
-        {...useCustomRegister("areYouReporting")}
+        {...useCustomRegister<DemoFormType>("areYouReporting")}
         options={options}
       />
     </CoreQuestionWrapper>
--- services/ui-src/src/components/CoreQuestions/StatusOfDataReported/index.tsx
+++ services/ui-src/src/components/CoreQuestions/StatusOfDataReported/index.tsx
@@ -1,6 +1,7 @@
 import { CoreQuestionWrapper } from "..";
 import * as QMR from "components/Inputs";
 import { useCustomRegister } from "hooks/useCustomRegister";
+import { DemoFormType } from "views/DemoQuestions/DemoFormType";
 
 interface Props {
   options: QMR.RadioButtonOption[];
@@ -10,7 +11,9 @@ export const StatusOfDataReported = ({ options }: Props) => {
   return (
     <CoreQuestionWrapper label="Status of Data Reported">
       <QMR.RadioButton
-        {...useCustomRegister("statusOfDataReporting.statusOfDataReporting")}
+        {...useCustomRegister<DemoFormType>(
+          "statusOfDataReporting.statusOfDataReporting"
+        )}
         options={options}
       />
     </CoreQuestionWrapper>
