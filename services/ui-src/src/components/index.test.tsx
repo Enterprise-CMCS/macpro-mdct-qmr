324d49f5d6c86c03636528ac6b8aea1f121035f8
Samuel Sierra <ssierra@collabralink.com>
2021-12-07 13:40:00 -0500

    iterating

--- services/ui-src/src/components/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as everything from "./index";
-
-describe("Test index.tsx", () => {
-  test("Check that the file is there", () => {
-    console.log(everything);
-    expect(1).toBe(1);
-  });
-});
