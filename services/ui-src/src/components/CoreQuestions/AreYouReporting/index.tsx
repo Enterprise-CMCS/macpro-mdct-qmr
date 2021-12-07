9c42ad0f64b45dc8c4e9c30ed23f501f90afcf72
13bfrancis <13bfrancis@gmail.com>
2021-12-06 20:58:11 -0500

    simplify error handling logic in demo questions page

--- services/ui-src/src/components/CoreQuestions/AreYouReporting/index.tsx
+++ services/ui-src/src/components/CoreQuestions/AreYouReporting/index.tsx
@@ -2,22 +2,16 @@ import { CoreQuestionWrapper } from "..";
 import * as QMR from "components/Inputs/RadioButton";
 import { useCustomRegister } from "hooks/useCustomRegister";
 import { DemoForm } from "views/DemoQuestions/DemoFormType";
-import { useFormContext } from "react-hook-form";
 
 interface Props {
   options: QMR.RadioButtonOption[];
 }
 
 export const AreYouReporting = ({ options }: Props) => {
-  const {
-    formState: { errors },
-  } = useFormContext<DemoForm.DemoFormType>();
   return (
     <CoreQuestionWrapper label="Are you reporting on this measure?">
       <QMR.RadioButton
         {...useCustomRegister<DemoForm.DemoFormType>("areYouReporting")}
-        isInvalid={!!errors.areYouReporting}
-        errorMessage={errors.areYouReporting?.message}
         options={options}
       />
     </CoreQuestionWrapper>
