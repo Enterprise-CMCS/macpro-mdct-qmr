acbbe251e2ee5be437801fb9f4813617c9a89dc7
Samuel Sierra <ssierra@collabralink.com>
2021-12-07 12:28:25 -0500

    adding more tests

---
+++ services/ui-src/src/components/index.test.tsx
@@ -0,0 +1,8 @@
+import * as everything from "./index";
+
+describe("Test index.tsx", () => {
+  test("Check that the file is there", () => {
+    console.log(everything);
+    expect(1).toBe(1);
+  });
+});
