834dbbb3e8d7663f70fc43c1fdfbc52177214fdb
Daniel Belcher <daniel.belcher.cs@gmail.com>
2021-12-08 16:01:19 -0600

    mobbing handover

--- services/ui-src/src/views/DemoQuestions/index.tsx
+++ services/ui-src/src/views/DemoQuestions/index.tsx
@@ -1,19 +1,16 @@
 import * as CUI from "@chakra-ui/react";
 import * as CoreQs from "components/CoreQuestions";
 import * as QMR from "components";
-import { useForm, FormProvider } from "react-hook-form";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
 import { DemoForm } from "views/DemoQuestions/DemoFormType";
 import { joiResolver } from "@hookform/resolvers/joi";
 import { validationSchema } from "./ValidationSchema";
 import { useCustomRegister } from "hooks/useCustomRegister";
 
 export const DemoQuestions = () => {
-  const methods = useForm<DemoForm.DemoFormType>({
-    shouldUnregister: true,
-    mode: "all",
-    resolver: joiResolver(validationSchema),
-  });
-  const { register } = methods;
+  const methods = useFormContext<DemoForm.DemoFormType>();
+  const { register, watch } = methods;
+  const watchAdminOtherDescription = watch("dataSource") || undefined;
   return (
     <QMR.StateLayout
       breadcrumbItems={[
@@ -22,91 +19,104 @@ export const DemoQuestions = () => {
         { path: `/OH/2021/ACS/AIF-HH`, name: `DQT-AD: Demo Questions` },
       ]}
     >
-      <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
-        <CUI.Container maxW="2xl">
-          <CoreQs.AreYouReporting
-            options={[
-              {
-                displayValue: `Yes, I am reporting Admission to an Institution from the Community (AIF-HH) for FFY 2021 quality params.measure reporting.`,
-                value: "Yes, I am reporting",
-              },
-              {
-                displayValue: `No, I am not reporting Admission to an Institution from the Community (AIF-HH) for FFY 2021 quality measure reporting.`,
-                value: "No, I am not reporting",
-              },
-            ]}
-          />
-          <CoreQs.StatusOfDataReported
-            options={[
-              {
-                displayValue: "I am reporting provisional data",
-                value: "I am reporting provisional data",
-                children: [
-                  <QMR.TextArea
-                    {...register(
-                      "statusOfDataReporting.statusOfDataReportingAdditional"
-                    )}
-                    label="Please provide additional information such as when the data will be final and if you plan to modify the data reported here:"
-                    formLabelProps={{
-                      fontWeight: "normal",
-                      fontSize: "normal",
-                    }}
-                    key="status-2a"
-                  />,
-                ],
-              },
-              {
-                displayValue: "I am reporting final data",
-                value: "I am reporting final data",
-              },
-            ]}
-          />
-          <CoreQs.DataSource
-            options={[
-              {
-                displayValue: "Administrative Data",
-                value: "I am reporting provisional data",
-                children: [
-                  <QMR.Checkbox
-                    {...useCustomRegister("dataSource.adminData")}
-                    options={[
-                      {
-                        displayValue:
-                          "Medicaid Management Information System (MMIS)",
-                        value: "Medicaid Management Information System (MMIS)",
-                      },
-                      {
-                        displayValue: "Other",
-                        value: "AdministrativeDataOther",
-                      },
-                    ]}
-                    label="If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below."
-                  />,
-                ],
-              },
-              {
-                displayValue:
-                  "Hybrid (Administrative and Medical Records Data)",
-                value: "I am reporting final data",
-              },
-              {
-                displayValue: "Other",
-                value: "other",
-              },
-            ]}
-          />
-          <button>Submit</button>
-        </CUI.Container>
-      </form>
+      <CUI.Container maxW="2xl">
+        <CoreQs.AreYouReporting
+          options={[
+            {
+              displayValue: `Yes, I am reporting Admission to an Institution from the Community (AIF-HH) for FFY 2021 quality params.measure reporting.`,
+              value: "Yes, I am reporting",
+            },
+            {
+              displayValue: `No, I am not reporting Admission to an Institution from the Community (AIF-HH) for FFY 2021 quality measure reporting.`,
+              value: "No, I am not reporting",
+            },
+          ]}
+        />
+        <CoreQs.StatusOfDataReported
+          options={[
+            {
+              displayValue: "I am reporting provisional data",
+              value: "I am reporting provisional data",
+              children: [
+                <QMR.TextArea
+                  {...register(
+                    "statusOfDataReporting.statusOfDataReportingAdditional"
+                  )}
+                  label="Please provide additional information such as when the data will be final and if you plan to modify the data reported here:"
+                  formLabelProps={{
+                    fontWeight: "normal",
+                    fontSize: "normal",
+                  }}
+                  key="status-2a"
+                />,
+              ],
+            },
+            {
+              displayValue: "I am reporting final data",
+              value: "I am reporting final data",
+            },
+          ]}
+        />
+        <CoreQs.DataSource
+          options={[
+            {
+              displayValue: "Administrative Data",
+              value: "I am reporting provisional data",
+              children: [
+                <QMR.Checkbox
+                  {...useCustomRegister("dataSource.adminData")}
+                  options={[
+                    {
+                      displayValue:
+                        "Medicaid Management Information System (MMIS)",
+                      value: "Medicaid Management Information System (MMIS)",
+                    },
+                    {
+                      displayValue: "Other",
+                      value: "AdministrativeDataOther",
+                      children: [
+                        <QMR.TextInput
+                          label="Describe the data:"
+                          {...register("dataSource.adminData.other")}
+                        />,
+                        <QMR.TextArea
+                          label={JSON.stringify(watchAdminOtherDescription)}
+                          {...register("dataSource.adminData.other.desc")}
+                        />,
+                      ],
+                    },
+                  ]}
+                  label="If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below."
+                />,
+              ],
+            },
+            {
+              displayValue: "Hybrid (Administrative and Medical Records Data)",
+              value: "I am reporting final data",
+            },
+            {
+              displayValue: "Other",
+              value: "other",
+            },
+          ]}
+        />
+        <button>Submit</button>
+      </CUI.Container>
     </QMR.StateLayout>
   );
 };
 
 export const DemoQuestionsWrapper = () => {
-  const methods = useForm();
+  const methods = useForm({
+    shouldUnregister: true,
+    mode: "all",
+    resolver: joiResolver(validationSchema),
+  });
   return (
     <FormProvider {...methods}>
-      <DemoQuestions />
+      <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+        <DemoQuestions />
+      </form>
     </FormProvider>
   );
 };
