5422dee34d1f42c1bbad81055473c9b097328f0b
James Dinh <jdinh8124@gmail.com>
2021-12-09 11:55:49 -0800

    added UI for most completed data source options

--- services/ui-src/src/views/DemoMeasure/questions/DataSource.tsx
+++ services/ui-src/src/views/DemoMeasure/questions/DataSource.tsx
@@ -23,6 +23,7 @@ export const DataSource = () => {
                 {...useCustomRegister<DemoForm.DemoFormType>(
                   "dataSource.adminData.selections"
                 )}
+                key="Administrative data"
                 options={[
                   {
                     displayValue:
@@ -31,7 +32,7 @@ export const DataSource = () => {
                   },
                   {
                     displayValue: "Other",
-                    value: "AdministrativeDataOther",
+                    value: "Administrative Data Other",
                     children: [
                       <QMR.TextInput
                         label="Describe the data:"
@@ -50,21 +51,120 @@ export const DataSource = () => {
                       </CUI.Text>,
                       <QMR.TextArea
                         key="Administrative Data Source:"
-                        label={`Administrative Data Source: ${watch(
-                          "dataSource.adminData.other.dataSource"
-                        )}`}
+                        label={`Administrative Data Source: ${
+                          watch("dataSource.adminData.other.dataSource") || ""
+                        }`}
                         {...register("dataSource.adminData.other.explain")}
                       />,
                     ],
                   },
                 ]}
-                label="If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below."
+                label="What is the Administrative Data Source"
+                formLabelProps={{ fontWeight: 700 }}
               />,
             ],
           },
           {
-            displayValue: "Other Data Source",
-            value: "other data wource",
+            displayValue: "Hybird (Administrative and Medical Record Data)",
+            value: "I am reporting hybrid",
+            children: [
+              <QMR.Checkbox
+                {...useCustomRegister<any>("dataSource.hybrid.selections")}
+                key="Hybrid data"
+                options={[
+                  {
+                    displayValue:
+                      "Medicaid Management Information System (MMIS)",
+                    value: "Medicaid Management Information System (MMIS)",
+                  },
+                  {
+                    displayValue: "Other",
+                    value: "Hybrid Data Other",
+                    children: [
+                      <QMR.TextInput
+                        label="Describe the data:"
+                        key="Describe the data:"
+                        {...register("dataSource.hybrid.other.dataSource")}
+                      />,
+                      <CUI.Text
+                        fontSize="sm"
+                        py="2"
+                        fontWeight="bold"
+                        key="If the data source differed across"
+                      >
+                        If the data source differed across health plans or
+                        delivery systems, identify the number of plans that used
+                        each data source:
+                      </CUI.Text>,
+                      <QMR.TextArea
+                        key="Administrative Data Source:"
+                        label={`Administrative Data Source: ${
+                          watch("dataSource.hybrid.other.dataSource") || ""
+                        }`}
+                        {...register("dataSource.hybrid.other.explain")}
+                      />,
+                    ],
+                  },
+                ]}
+                label="What is the Administrative Data Source"
+                formLabelProps={{ fontWeight: 700 }}
+              />,
+
+              <QMR.RadioButton
+                label="What is the Medical Records Data Source"
+                formLabelProps={{ fontWeight: 700, paddingTop: 5 }}
+                key="Hybrid Radio Button Data"
+                {...useCustomRegister<any>(
+                  "dataSource.hybrid.medicalRecordDataSoruce"
+                )}
+                options={[
+                  {
+                    displayValue: "Electronic Health Record (EHR) Data",
+                    value: "Electronic Health Record",
+                  },
+                  {
+                    displayValue: "Paper",
+                    value: "Paper",
+                  },
+                  {
+                    displayValue: "Both (EHR and Paper)",
+                    value: "Both EHR and Paper",
+                  },
+                ]}
+              />,
+            ],
+          },
+          {
+            displayValue: "Electronic Health Record",
+            value: "electron health record",
+            children: [
+              <QMR.TextInput
+                label="Describe Electronic Record data source:"
+                key="Describe the electronic data source:"
+                {...register("dataSource.electronicRecord.dataSource")}
+              />,
+              <CUI.Text
+                fontSize="sm"
+                py="2"
+                fontWeight="bold"
+                key="If the data source differed across data"
+              >
+                If the data source differed across health plans or delivery
+                systems, identify the number of plans that used each data
+                source:
+              </CUI.Text>,
+              <QMR.TextArea
+                key="Electronic Record Data Source data:"
+                label={`Electronic Record Data Source: ${
+                  watch("dataSource.electronicRecord.dataSource") || ""
+                }`}
+                {...register("dataSource.electronicRecord.explain")}
+              />,
+            ],
+          },
+          {
+            displayValue: "Other",
+            value: "other data source",
             children: [
               <QMR.TextInput
                 label="Describe the data source:"
@@ -82,10 +182,10 @@ export const DataSource = () => {
                 source:
               </CUI.Text>,
               <QMR.TextArea
-                key="Administrative Data Source data:"
-                label={`Other Data Source: ${watch(
-                  "dataSource.other.dataSource"
-                )}`}
+                key="Other Data Source data:"
+                label={`Other Data Source: ${
+                  watch("dataSource.other.dataSource") || ""
+                }`}
                 {...register("dataSource.other.explain")}
               />,
             ],
