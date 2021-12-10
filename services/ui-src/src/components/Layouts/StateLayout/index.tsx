34d7b801934a937b61dffa2c1f764f383909c8b7
Benjamin Paige <benjamin.paige@gmail.com>
2021-12-09 19:09:43 -0700

    Add sticky nav thing to layout

--- services/ui-src/src/components/Layouts/StateLayout/index.tsx
+++ services/ui-src/src/components/Layouts/StateLayout/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import * as CUI from "@chakra-ui/react";
 import * as QMR from "components";
 import { BreadCrumbItems } from "components";
@@ -7,18 +8,41 @@ interface Props {
 }
 
 export const StateLayout: React.FC<Props> = ({ children, breadcrumbItems }) => {
+  const [sticky, setsticky] = useState(false);
+
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      const windowHeight = window.scrollY;
+      windowHeight > 89 ? setsticky(true) : setsticky(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickNavbar);
+    return () => window.removeEventListener("scroll", stickNavbar);
+  }, []);
+
   const singleNavigationItem = breadcrumbItems.length === 1;
   return (
     <>
-      <CUI.Box bg={singleNavigationItem ? "blue.700" : "blue.100"}>
-        <CUI.Container maxW="7xl" py="5">
-          <QMR.Breadcrumbs
-            items={breadcrumbItems}
-            color={singleNavigationItem ? "white" : "inherit"}
-          />
-        </CUI.Container>
+      <CUI.Box
+        position={sticky ? "fixed" : "relative"}
+        zIndex="2"
+        padding="none"
+        w="full"
+        top="0"
+        left="0"
+      >
+        <CUI.Box bg={singleNavigationItem ? "blue.700" : "blue.100"}>
+          <CUI.Container maxW="7xl" py="5">
+            <QMR.Breadcrumbs
+              items={breadcrumbItems}
+              color={singleNavigationItem ? "white" : "inherit"}
+            />
+          </CUI.Container>
+        </CUI.Box>
       </CUI.Box>
-      <CUI.Container maxW="7xl" py="5">
+      <CUI.Container maxW="7xl" mt={sticky ? "65px" : "0"} py="6">
         {children}
       </CUI.Container>
     </>
