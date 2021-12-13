cf52417a566d3caf411b7387389a1028ae6fe82b
Andrew Belzer <abelzer@collabralink.com>
2021-12-13 13:31:16 -0600

    referencing things correctly

--- services/app-api/handlers/coreSets/alter.js
+++ services/app-api/handlers/coreSets/alter.js
@@ -1,7 +1,35 @@
 import handler from "../../libs/handler-lib";
+import dynamoDb from "../../libs/dynamodb-lib";
 
 export const createCoreSet = handler(async (event, context) => {
-  return event;
+  // The State Year and ID are all part of the path
+  const stateYearId = event.path.split("/");
+  const state = stateYearId[2];
+  const year = stateYearId[3];
+  const id = stateYearId[4];
+  // Dynamo only accepts one row as a key, so we are using a combination for the dynamoKey
+  const dynamoKey = `${state}${year}${id}`;
+
+  const params = {
+    TableName: process.env.CORESET_TABLE_NAME,
+    Item: {
+      compoundKey: dynamoKey,
+      state: state,
+      year: year,
+      id: id,
+      metaData: {
+        createdAt: Date.now(),
+        lastAltered: Date.now(),
+        lastAlteredBy: event.headers["cognito-identity-id"],
+      },
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return params;
+
+  // return params.Item;
 });
 
 export const editCoreSet = handler(async (event, context) => {
--- services/app-api/libs/dynamodb-lib.js
+++ services/app-api/libs/dynamodb-lib.js
@@ -18,6 +18,7 @@ const client = new AWS.DynamoDB.DocumentClient(dyanmoConfig);
 export default {
   get: (params) => client.get(params).promise(),
   put: (params) => client.put(params).promise(),
+  post: (params) => client.post(params).promise(),
   query: (params) => client.query(params).promise(),
   update: (params) => client.update(params).promise(),
   delete: (params) => client.delete(params).promise(),
