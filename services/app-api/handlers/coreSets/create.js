b54c0b6da05d44743b98d93a81ae27628a6e551b
Andrew Belzer <abelzer@collabralink.com>
2021-12-13 17:40:08 -0600

    reference pathParameters correctly

--- services/app-api/handlers/coreSets/create.js
+++ services/app-api/handlers/coreSets/create.js
@@ -3,10 +3,9 @@ import dynamoDb from "../../libs/dynamodb-lib";
 
 export const createCoreSet = handler(async (event, context) => {
   // The State Year and ID are all part of the path
-  const stateYearId = event.path.split("/");
-  const state = stateYearId[2];
-  const year = stateYearId[3];
-  const id = stateYearId[4];
+  const state = event.pathParameters.state;
+  const year = event.pathParameters.year;
+  const id = event.pathParameters.coreSetId;
   // Dynamo only accepts one row as a key, so we are using a combination for the dynamoKey
   const dynamoKey = `${state}${year}${id}`;
 
