5624cc2bc261970ff548d50ac3e03746ebd5c70d
Andrew Belzer <abelzer@collabralink.com>
2021-12-13 18:16:30 -0600

    do more stuff

--- services/app-api/handlers/coreSets/get.js
+++ services/app-api/handlers/coreSets/get.js
@@ -7,9 +7,7 @@ export const coreSetList = handler(async (event, context) => {
   // The State Year and ID are all part of the path
   const state = event.pathParameters.state;
   const year = event.pathParameters.year;
-  const id = event.pathParameters.coreSetId;
   // Dynamo only accepts one row as a key, so we are using a combination for the dynamoKey
-  const dynamoKey = `${state}${year}${id}`;
   const params = {
     TableName: process.env.CORESET_TABLE_NAME,
     ProjectionExpression: "#yr, #st",
