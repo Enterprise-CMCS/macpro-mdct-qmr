0345175ae6b3c47ba7101c402f434ed44973499e
James Dinh <jdinh8124@gmail.com>
2021-12-15 11:50:48 -0800

    fix failing tests because of nesting from conflict

--- services/ui-src/src/views/DemoMeasure/index.test.tsx
+++ services/ui-src/src/views/DemoMeasure/index.test.tsx
@@ -52,36 +52,37 @@ describe("Test Demo Questions Component", () => {
         /Specify the version of HEDIS measurement year used/i
       )
     ).toBeInTheDocument();
-    it("should render children when the user clicks into data source options", async () => {
-      userEvent.click(screen.getByLabelText(/Administrative Data/i));
+  });
 
-      expect(screen.getByLabelText(/Administrative Data/i)).toBeChecked();
-      expect(
-        await screen.findByText(/What is the Administrative Data Source/i)
-      ).toBeInTheDocument();
+  it("should render children when the user clicks into data source options", async () => {
+    userEvent.click(screen.getByLabelText(/Administrative Data/i));
 
-      userEvent.click(
-        screen.getByLabelText(/Medicaid Management Information System/i)
-      );
-      expect(
-        screen.getByLabelText(/Medicaid Management Information System/i)
-      ).toBeChecked();
-    });
+    expect(screen.getByLabelText(/Administrative Data/i)).toBeChecked();
+    expect(
+      await screen.findByText(/What is the Administrative Data Source/i)
+    ).toBeInTheDocument();
 
-    it("should render additional notes and its children when the user clicks into its options", async () => {
-      userEvent.type(
-        screen.getByLabelText(
-          /Please add any additional notes or comments on the measure not otherwise captured above/i
-        ),
-        "hello"
-      );
+    userEvent.click(
+      screen.getByLabelText(/Medicaid Management Information System/i)
+    );
+    expect(
+      screen.getByLabelText(/Medicaid Management Information System/i)
+    ).toBeChecked();
+  });
 
-      expect(
-        screen.getByLabelText(
-          /Please add any additional notes or comments on the measure not otherwise captured above/i
-        )
-      ).toHaveValue("hello");
-    });
+  it("should render additional notes and its children when the user clicks into its options", async () => {
+    userEvent.type(
+      screen.getByLabelText(
+        /Please add any additional notes or comments on the measure not otherwise captured above/i
+      ),
+      "hello"
+    );
+
+    expect(
+      screen.getByLabelText(
+        /Please add any additional notes or comments on the measure not otherwise captured above/i
+      )
+    ).toHaveValue("hello");
   });
 
   test("Check that the nav renders", () => {
