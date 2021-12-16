fabdf2fb27c7f4c45d5d91d20f9ccbf7a821a09e
Benjamin Paige <benjamin.paige@gmail.com>
2021-12-16 11:12:44 -0700

    Update reporting year select

--- services/ui-src/src/views/StateHome/index.tsx
+++ services/ui-src/src/views/StateHome/index.tsx
@@ -1,48 +1,56 @@
 import * as CUI from "@chakra-ui/react";
 import * as QMR from "components";
 import config from "config";
-import { useParams } from "react-router-dom";
-import { FormProvider, useForm } from "react-hook-form";
+import { useParams, useNavigate } from "react-router-dom";
 import { Params } from "Routes";
 import { CoreSet } from "components/Table/types";
 import { coreSetActions } from "./actions";
 import { AddCoreSetCards } from "./AddCoreSetCards";
+import { TiArrowUnsorted } from "react-icons/ti";
 
 // This will be updated when we know exactly what we need to add coreset
 const data: CoreSet.Data[] = [
   {
-    path: "/adult",
+    path: "ACS",
     title: "Adult Core Set Measures",
     type: CoreSet.Type.ADULT,
     progress: { numAvailable: 12, numComplete: 0 },
-    actions: coreSetActions[CoreSet.Type.ADULT]("test1"),
+    actions: coreSetActions[CoreSet.Type.ADULT]("OH2021-ACS"),
     submitted: false,
-    id: "test1",
+    id: "OH2021-ACS",
     year: "2021",
   },
 ];
 
 const ReportingYear = () => {
+  const navigate = useNavigate();
+  const { state, year } = useParams<Params>();
+
   const reportingyearOptions = config.reportingYears.map((year: string) => ({
     displayValue: year + " Core Set",
     value: year,
   }));
 
   return (
-    <form>
-      <CUI.Box w={{ base: "full", md: "48" }}>
-        <CUI.Text fontSize="sm" fontWeight="600" mb="2">
-          Reporting Year
-        </CUI.Text>
-        <QMR.Select
-          name="reporting-year"
-          options={reportingyearOptions}
-          selectProps={{
-            onChange: (e) => console.log(e.target.value),
-          }}
-        />
-      </CUI.Box>
-    </form>
+    <CUI.Box w={{ base: "full", md: "48" }}>
+      <CUI.Text fontSize="sm" fontWeight="600" mb="2">
+        Reporting Year
+      </CUI.Text>
+      <CUI.Select
+        borderRadius="sm"
+        icon={<TiArrowUnsorted />}
+        value={year}
+        onChange={(e) => {
+          navigate(`/${state}/${e.target.value}`);
+        }}
+      >
+        {reportingyearOptions.map(({ displayValue, value }) => (
+          <option value={value} key={value}>
+            {displayValue}
+          </option>
+        ))}
+      </CUI.Select>
+    </CUI.Box>
   );
 };
 
@@ -66,20 +74,17 @@ const Heading = () => {
 
 export const StateHome = () => {
   const { state, year } = useParams<Params>();
-  const methods = useForm();
   return (
-    <FormProvider {...methods}>
-      <QMR.StateLayout
-        breadcrumbItems={[
-          { path: `/${state}/${year}`, name: "Core Set Measures" },
-        ]}
-      >
-        <Heading />
-        <QMR.Table data={data} columns={QMR.coreSetColumns} />
-        <CUI.HStack spacing="6">
-          <AddCoreSetCards />
-        </CUI.HStack>
-      </QMR.StateLayout>
-    </FormProvider>
+    <QMR.StateLayout
+      breadcrumbItems={[
+        { path: `/${state}/${year}`, name: "Core Set Measures" },
+      ]}
+    >
+      <Heading />
+      <QMR.Table data={data} columns={QMR.coreSetColumns} />
+      <CUI.HStack spacing="6">
+        <AddCoreSetCards />
+      </CUI.HStack>
+    </QMR.StateLayout>
   );
 };
