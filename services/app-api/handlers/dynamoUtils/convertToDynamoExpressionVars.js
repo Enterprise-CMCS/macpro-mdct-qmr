c763808aa3d5f5029c95bff8076aab3110fe7fbc
Andrew Belzer <abelzer@collabralink.com>
2021-12-16 08:59:50 -0600

    add expression types

--- services/app-api/handlers/dynamoUtils/convertToDynamoExpressionVars.js
+++ services/app-api/handlers/dynamoUtils/convertToDynamoExpressionVars.js
@@ -3,21 +3,30 @@
 //   year: "2021",
 // };
 
-export const convertToDynamoExpressionVars = (listOfVars) => {
+export const convertToDynamoExpressionVars = (listOfVars, expressionType) => {
   let expressionAttributeNames = {};
   let expressionAttributeValues = {};
-  let filterExpression = "";
+  let expression = "";
   Object.keys(listOfVars).each((key, index) => {
     expressionAttributeNames[`:${key}`] = key;
     expressionAttributeValues[`#${key}`] = listOfVars[key];
-    filterExpression =
-      index === 0
-        ? `:${key} = #${key} `
-        : `${filterExpression} AND :${key} = #${key}`;
+
+    if (expressionType === "list") {
+      expression =
+        index === 0
+          ? `:${key} = #${key} `
+          : `${filterExpression} AND :${key} = #${key}`;
+    }
+    if (expressionType === "post") {
+      expression =
+        index === 0
+          ? `set :${key} = #${key} `
+          : `${filterExpression}, :${key} = #${key}`;
+    }
   });
   return {
     expressionAttributeNames,
     expressionAttributeValues,
-    filterExpression,
+    expression,
   };
 };
