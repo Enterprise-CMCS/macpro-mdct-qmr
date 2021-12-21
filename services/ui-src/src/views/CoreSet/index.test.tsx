8183484ab4d71f50698791f3bbe18d6eaf42a8a0
James Dinh <jdinh8124@gmail.com>
2021-12-21 14:00:02 -0800

    Oy2 14511/14512/14514 - Adult/Child+/HealthHome Core Set Measure List Page (#316)
    * added measure data into a lib and laid out basic table UI
    
    * add id for each measure
    
    * UI laid out but will clarify spacing for styling props on containers
    
    * added basic testing that might need additional test behaviors
    
    * removed accidental list of measures in containedButton
    
    * added to testing and added a future link to component
    
    * removed unnecessary props
    
    * adjusted chakra layout to make more sense using flex
    
    * fix accessibility issue
    
    * ran prettier on measure data
    
    * making changes based on bens recommendations and PR review
    
    * added additional enums for titles and texts

--- services/ui-src/src/views/CoreSet/index.test.tsx
+++ services/ui-src/src/views/CoreSet/index.test.tsx
@@ -1,15 +1,31 @@
 import { CoreSet } from "./index";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { RouterWrappedComp } from "utils/testing";
 
 describe("Test CoreSet.tsx", () => {
-  test("Check that the contact us renders", () => {
-    const { getByTestId } = render(
+  beforeEach(() => {
+    render(
       <RouterWrappedComp>
         <CoreSet />
       </RouterWrappedComp>
     );
+  });
 
-    expect(getByTestId("core-set")).toBeVisible();
+  describe("Test coreset component", () => {
+    test("Check that the nav renders", () => {
+      expect(screen.getByTestId("state-layout-container")).toBeVisible();
+    });
+
+    it("renders the correct child measure table data components", () => {
+      expect(
+        screen.getByText(
+          /Complete all Adult Core Set Questions and Adult Core Set Measures to submit FFY 2021/i
+        )
+      ).toBeInTheDocument();
+
+      expect(screen.getByText(/Submit Measures/i)).toBeInTheDocument();
+
+      expect(screen.getByText(/Reporting FFY 2021/i)).toBeInTheDocument();
+    });
   });
 });
