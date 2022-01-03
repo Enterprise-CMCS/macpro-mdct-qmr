5b9d0c2507ddd9a610f4f93f7313dfc47b015eae
Andrew Belzer <abelzer@collabralink.com>
2022-01-02 19:46:05 -0600

    tests

--- services/ui-src/src/components/MeasureButtons/index.test.tsx
+++ services/ui-src/src/components/MeasureButtons/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MeasureButtons } from "./index";
 
 describe("Test MeasureButtons", () => {
