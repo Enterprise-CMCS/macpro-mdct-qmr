6badf1dd6c5f31fcfcafba983ddc5d58845a4fe3
Benjamin Paige <benjamin.paige@gmail.com>
2022-01-03 15:12:48 -0700

    Update statehome test

--- services/ui-src/src/views/StateHome/__tests__/index.test.tsx
+++ services/ui-src/src/views/StateHome/__tests__/index.test.tsx
@@ -38,8 +38,8 @@ describe("Test StateHome", () => {
 
   test("Check that the route is correct when reporting year is changed", () => {
     fireEvent.change(screen.getByTestId("select"), {
-      target: { value: "2022" },
+      target: { value: "2021" },
     });
-    expect(mockedNavigate).toHaveBeenCalledWith("/OH/2022");
+    expect(mockedNavigate).toHaveBeenCalledWith("/OH/2021");
   });
 });
