57feaa3c096628942649dca7cb4da66e727f46b1
Samuel Sierra <ssierra@collabralink.com>
2022-01-03 09:42:23 -0500

    s3 in progress

--- services/ui-src/src/components/Upload/index.tsx
+++ services/ui-src/src/components/Upload/index.tsx
@@ -3,6 +3,7 @@ import * as CUI from "@chakra-ui/react";
 import { FolderIcon } from "components/FolderIcon";
 import { useDropzone } from "react-dropzone";
 import { useController, useFormContext } from "react-hook-form";
+import { Storage } from "aws-amplify";
 
 interface IUploadProps {
   maxSize?: number;
@@ -69,6 +70,55 @@ export const Upload = ({
       maxSize,
     });
 
+  function ensureLowerCaseFileExtension(file: any) {
+    const extensionStartIndex = file.name.lastIndexOf(".");
+    const fileNameText = file.name.slice(0, extensionStartIndex);
+    const fileNameExtension = file.name.substring(extensionStartIndex);
+    const lowerCaseFileNameExtension = fileNameExtension.toLowerCase();
+
+    if (fileNameExtension === lowerCaseFileNameExtension) {
+      return file;
+    } else {
+      const updatedFileName = fileNameText.concat(lowerCaseFileNameExtension);
+      const updatedFile = new File([file], updatedFileName, {
+        type: file.type,
+      });
+      // updatedFile.title = file.title;
+      return updatedFile;
+    }
+  }
+
+  async function uploadFile(file: any) {
+    const fileToUpload = ensureLowerCaseFileExtension(file);
+
+    let retPromise;
+    const targetPathname = `${Date.now()}/${fileToUpload.name}`;
+    console.log(window);
+
+    try {
+      const stored = await Storage.vault.put(targetPathname, fileToUpload, {
+        level: "protected",
+        contentType: fileToUpload.type,
+      });
+
+      const url = await Storage.vault.get(stored.key, { level: "protected" });
+
+      let result = {
+        s3Key: stored.key,
+        filename: fileToUpload.name,
+        contentType: fileToUpload.type,
+        url: url.split("?", 1)[0], //We only need the permalink part of the URL since the S3 bucket policy allows for public read
+        title: fileToUpload.title,
+      };
+
+      retPromise = Promise.resolve(result);
+    } catch (error) {
+      console.log(error);
+      retPromise = Promise.reject(error);
+    }
+    return retPromise;
+  }
+
   return (
     <>
       {label && <CUI.Text>{label}</CUI.Text>}
@@ -113,27 +163,30 @@ export const Upload = ({
           </CUI.AlertTitle>
         </CUI.Alert>
       ))}
-      {acceptedFiles.map((file, index) => (
-        <CUI.HStack
-          key={`${index}-${file.name}`}
-          background="blue.50"
-          pl="1rem"
-          my="2"
-          borderRadius="10"
-          justifyContent="space-between"
-        >
-          <CUI.Text variant="xl">
-            File Name: {file.name} ({convertFileSize(file.size)})
-          </CUI.Text>
-          <CUI.Button
-            data-testid={`test-delete-btn-${index}`}
-            background="none"
-            onClick={() => clearFile(index)}
+      {acceptedFiles.map((file, index) => {
+        uploadFile(file);
+        return (
+          <CUI.HStack
+            key={`${index}-${file.name}`}
+            background="blue.50"
+            pl="1rem"
+            my="2"
+            borderRadius="10"
+            justifyContent="space-between"
           >
-            x
-          </CUI.Button>
-        </CUI.HStack>
-      ))}
+            <CUI.Text variant="xl">
+              File Name: {file.name} ({convertFileSize(file.size)})
+            </CUI.Text>
+            <CUI.Button
+              data-testid={`test-delete-btn-${index}`}
+              background="none"
+              onClick={() => clearFile(index)}
+            >
+              x
+            </CUI.Button>
+          </CUI.HStack>
+        );
+      })}
     </>
   );
 };
