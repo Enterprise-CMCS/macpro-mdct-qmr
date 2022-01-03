5a9b4d316328df70f67b83c7196d3fd82493fa3a
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-01-03 16:36:13 -0500

    Move Text Areas below (#354)

--- services/ui-src/src/measures/2021/FUAAD/questions/DataSource.tsx
+++ services/ui-src/src/measures/2021/FUAAD/questions/DataSource.tsx
@@ -5,9 +5,17 @@ import { useCustomRegister } from "hooks/useCustomRegister";
 import { Measure } from "measures/types";
 
 export const DataSource = () => {
-  const methods = useFormContext();
+  const methods = useFormContext<Measure.Form>();
   const register = useCustomRegister<Measure.Form>();
   const { watch } = methods;
+  // Watch Checkboxes for DataSource Choices
+  const watchOtherDataSource = watch("DataSource") ?? [];
+  const watchAdminDataSource = watch("DataSource-Administrative") ?? [];
+  // Return True or False based on if Other is selected or not
+  const isOtherDataSource =
+    watchOtherDataSource?.indexOf("Other Data Source") !== -1;
+  const isOtherAdminDataSource =
+    watchAdminDataSource?.indexOf("Administrative Data Other") !== -1;
 
   return (
     <QMR.CoreQuestionWrapper label="Data Source">
@@ -34,24 +42,6 @@ export const DataSource = () => {
                         label="Describe the data source:"
                         {...register("DataSource-Administrative-Other")}
                       />,
-                      <CUI.Text
-                        fontSize="sm"
-                        py="2"
-                        fontWeight="bold"
-                        key="If the data source differed across"
-                      >
-                        If the data source differed across health plans or
-                        delivery systems, identify the number of plans that used
-                        each data source:
-                      </CUI.Text>,
-                      <QMR.TextArea
-                        label={`Administrative Data Source: ${
-                          watch("DataSource-Administrative-Other") || ""
-                        }`}
-                        {...register(
-                          "DataSource-Administrative-Other-Explanation"
-                        )}
-                      />,
                     ],
                   },
                 ]}
@@ -68,25 +58,40 @@ export const DataSource = () => {
                 label="Describe the data source:"
                 {...register("DataSource-Other")}
               />,
-              <CUI.Text
-                fontSize="sm"
-                py="2"
-                fontWeight="bold"
-                key="If the data source differed across data"
-              >
-                If the data source differed across health plans or delivery
-                systems, identify the number of plans that used each data
-                source:
-              </CUI.Text>,
-              <QMR.TextArea
-                label={`Other Data Source: ${watch("DataSource-Other") || ""}`}
-                {...register("DataSource-Other-Explanation")}
-              />,
             ],
           },
         ]}
         label="If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below."
       />
+      {(isOtherAdminDataSource || isOtherDataSource) && (
+        <CUI.Text
+          fontSize="sm"
+          py="2"
+          fontWeight="bold"
+          key="If the data source differed across"
+        >
+          For each data source selected above, describe which reporting entities
+          used each data source (e.g., health plans, FFS). If the data source
+          differed across health plans or delivery systems, identify the number
+          of plans that used each data source:
+        </CUI.Text>
+      )}
+      {/* This will be visible when Other is selected for the Admin Data Source */}
+      {isOtherAdminDataSource && (
+        <QMR.TextArea
+          label={`Administrative Data Source: ${
+            watch("DataSource-Administrative-Other") || ""
+          }`}
+          {...register("DataSource-Administrative-Other-Explanation")}
+        />
+      )}
+      {/* This will be visible if Other is selected at the root of the data source question */}
+      {isOtherDataSource && (
+        <QMR.TextArea
+          label={`Other Data Source: ${watch("DataSource-Other") || ""}`}
+          {...register("DataSource-Other-Explanation")}
+        />
+      )}
     </QMR.CoreQuestionWrapper>
   );
 };
