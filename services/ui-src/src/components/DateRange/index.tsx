591bd71ca454922da8a3e10bc4519f8f17f63386
Daniel Belcher <47116905+daniel-belcher@users.noreply.github.com>
2022-01-04 09:04:17 -0600

    Oy2 15094 (#355)
    * feat: validation on dateRange
    
    * feat: monthPicker now shows errors from form
    
    - trigger for rerender on end date year change which failed to revalidate on it's own
    
    * chore: remove console logs
    
    * fix: change re-validation to only run on DateRange
    
    * fix: trigger is a dependency and month is now max
    
    * fix: missing name in dependency list
    
    Co-authored-by: James Dinh <jdinh8124@gmail.com>

--- services/ui-src/src/components/DateRange/index.tsx
+++ services/ui-src/src/components/DateRange/index.tsx
@@ -1,5 +1,8 @@
 import * as CUI from "@chakra-ui/react";
 import { MonthPicker } from "components/MonthPicker";
+import objectPath from "object-path";
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
 
 interface Props {
   name: string;
@@ -12,14 +15,46 @@ interface Props {
 }
 
 export const DateRange = ({ name }: Props) => {
+  const {
+    watch,
+    trigger,
+    formState: { errors },
+  } = useFormContext();
+
+  // trigger needed all three for rerender
+  const watchEndMonth = watch(`${name}.endDate.selectedMonth`);
+  const watchEndYear = watch(`${name}.endDate.selectedYear`);
+  const watchDateRange = watch(name);
+
+  const errorMessage =
+    objectPath.get(errors, name + ".startDate.selectedMonth")?.message ||
+    objectPath.get(errors, name + ".startDate.selectedYear")?.message;
+  const isInvalid =
+    !!objectPath.get(errors, name + ".startDate.selectedMonth") ||
+    !!objectPath.get(errors, name + ".startDate.selectedYear")?.message;
+
+  useEffect(() => {
+    trigger(name);
+  }, [watchEndMonth, watchEndYear, watchDateRange, trigger, name]);
+
   return (
     <CUI.Stack>
-      <CUI.FormLabel fontWeight={500}>{"Start Date"}</CUI.FormLabel>
-      <MonthPicker name={`${name}.startDate`} />
-      <CUI.FormLabel pt={5} fontWeight={500}>
-        {"End Date"}
-      </CUI.FormLabel>
-      <MonthPicker name={`${name}.endDate`} />
+      <CUI.FormControl isInvalid={isInvalid}>
+        <CUI.FormLabel fontWeight={500}>{"Start Date"}</CUI.FormLabel>
+        <MonthPicker name={`${name}.startDate`} isInvalid={isInvalid} />
+        <CUI.FormLabel pt={5} fontWeight={500}>
+          {"End Date"}
+        </CUI.FormLabel>
+        <MonthPicker name={`${name}.endDate`} isInvalid={isInvalid} />
+        <CUI.FormErrorMessage>
+          <CUI.Stack>
+            <>
+              <CUI.Divider my="4" />
+              {errorMessage || "An Error Occured"}
+            </>
+          </CUI.Stack>
+        </CUI.FormErrorMessage>
+      </CUI.FormControl>
     </CUI.Stack>
   );
 };
