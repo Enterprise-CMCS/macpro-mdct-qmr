0bbf90907907d0561fd2fdbec88fb9408300c6fb
hasanfar <hasanfar@gmail.com>
2022-01-05 10:08:24 -0500

    prettified

--- services/ui-src/src/coreSets/adultMeasures/schema.ts
+++ services/ui-src/src/coreSets/adultMeasures/schema.ts
@@ -3,9 +3,12 @@ import { Qualifier } from "./types";
 
 // This is the validation schema for any/all state measures
 export const validationSchema = Joi.object<Qualifier.Form>({
-  deliverySystem:Joi.array().items(Joi.object()),
+  deliverySystem: Joi.array().items(Joi.object()),
   isAudited: Joi.string(),
-  whoAudited: Joi.when('audit',{is: Joi.exist(), then:Joi.array().items(Joi.string()).required()}),
+  whoAudited: Joi.when("audit", {
+    is: Joi.exist(),
+    then: Joi.array().items(Joi.string()).required(),
+  }),
   hasExternalContractor: Joi.string(),
   audit: Joi.array(),
   contractorType: Joi.array().items(Joi.string()),
