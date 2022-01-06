7fddccb1faea135e9ec5358e854a08694b4ee7ba
13bfrancis <13bfrancis@gmail.com>
2022-01-06 10:25:05 -0500

    remove console.log statements

--- services/ui-src/src/hooks/useCustomValidation.tsx
+++ services/ui-src/src/hooks/useCustomValidation.tsx
@@ -8,14 +8,11 @@ export const useJoiValidationResolver = (
 ) =>
   useCallback(
     async (data: any, context: any, options: any) => {
-      console.log(`context`, context);
-      console.log(`options`, options);
       const resolver = joiResolver(validationSchema);
       let results = await resolver(data, context, options);
       additionalValidationFns.forEach((fn) => {
         results = fn(results);
       });
-      console.log(results);
       return results;
     },
     [validationSchema, additionalValidationFns]
