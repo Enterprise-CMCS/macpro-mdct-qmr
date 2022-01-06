77286f0262125ba2c4aaa1a0e425fa8a4ad38371
gitsierra <35947499+gitsierra@users.noreply.github.com>
2022-01-06 11:08:14 -0500

    S3 functionality (#367)
    * s3 in progress
    
    * testing
    
    * testing1
    
    * testing2
    
    * testing3
    
    * testing4
    
    * testing5
    
    * testing6
    
    * testing7
    
    * can upload now, uploading too much and no memory
    
    * testing8
    
    * testing9
    
    * upload functionality need to register paths
    
    * s3 upload functionality
    
    * prettier
    
    * prettier
    
    * var fix
    
    * pr changes
    
    * pr changes v2
    
    * pr changes oops
    
    * dont need to test s3
    
    * typo

--- services/ui-src/src/index.tsx
+++ services/ui-src/src/index.tsx
@@ -10,6 +10,20 @@ import { theme } from "theme";
 import { UserProvider } from "hooks/authHooks";
 
 Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    oauth: {
+      domain: config.cognito.APP_CLIENT_DOMAIN,
+      redirectSignIn: config.cognito.REDIRECT_SIGNIN,
+      redirectSignOut: config.cognito.REDIRECT_SIGNOUT,
+      scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
+      responseType: "token",
+    },
+  },
   Storage: {
     region: config.s3.REGION,
     bucket: config.s3.BUCKET,
