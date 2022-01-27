6d5efe265a5a72ba8c016a1f035e05171c044661
aalousi <aalousi@collabralink.com>
2022-01-27 13:00:41 -0500

    tests for oy215672tests

--- tests/cypress/support/pages/FUAADPage.js
+++ tests/cypress/support/pages/FUAADPage.js
@@ -17,6 +17,19 @@ const verifyTextFileIsUploaded =
 const verifyPictureFileIsUploaded =
   "//body/div[@id='root']/div[@id='app-wrapper']/main[@id='main-wrapper']/div[2]/div[1]/form[1]/section[1]/div[11]/div[2]/div[4]";
 
+// element is xpath, please use cy.xapth() instead of cy.get();
+const verifyNcqaText =
+  "//p[contains(text(),'NCQA, the measure steward, changed its naming conv')]";
+const hedisDropdown = "//select[@id='field-237']";
+const verifyFFY2021 =
+  "//option[contains(text(),'HEDIS MY 2020 (FFY 2021 Core Set Reporting)')]";
+const verifyFFY2020 =
+  "//option[contains(text(),'HEDIS 2020 (FFY 2020 Core Set Reporting)')]";
+const verifyFFY2019 =
+  "//option[contains(text(),'HEDIS 2019 (FFY 2019 Core Set Reporting)')]";
+const nationalCommitteForQualityAssuranceRadioBTN =
+  "//p[contains(text(),'National Committee for Quality Assurance (NCQA)/He')]";
+
 export class FUAADPAGE {
   verifyURLContainsFUAAD() {
     cy.url().should("include", "FUA-AD");
@@ -53,6 +66,25 @@ export class FUAADPAGE {
   }
   verifyPictureFileIsUploaded() {
     cy.xpath(verifyPictureFileIsUploaded).should("be.visible");
+  }
+
+  verifyNcqaText() {
+    cy.xpath(verifyNcqaText).should("be.visible");
+  }
+  hedisDropdown() {
+    cy.xpath(hedisDropdown).click();
+  }
+  verifyFFY2021() {
+    cy.xpath(verifyFFY2021).should("be.visible");
+  }
+  verifyFFY2020() {
+    cy.xpath(verifyFFY2020).should("be.visible");
+  }
+  verifyFFY2019() {
+    cy.xpath(verifyFFY2019).should("be.visible");
+  }
+  clicknationalCommitteForQualityAssuranceRadioBTN() {
+    cy.xpath(nationalCommitteForQualityAssuranceRadioBTN).click();
   }
 }
 export default FUAADPAGE;
