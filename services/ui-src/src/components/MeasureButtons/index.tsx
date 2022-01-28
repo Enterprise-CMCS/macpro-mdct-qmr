baf81a7fb59ed02d6d6cedfe1fd22f6e878762b0
Benjamin Paige <benjamin.paige@gmail.com>
2022-01-28 12:23:44 -0700

    Fuaad (#464)

--- services/ui-src/src/components/MeasureButtons/index.tsx
+++ services/ui-src/src/components/MeasureButtons/index.tsx
@@ -3,60 +3,37 @@ import * as QMR from "components";
 import { FaCheckCircle } from "react-icons/fa";
 import { useUser } from "hooks/authHooks";
 interface Props {
-  handleSave?: () => void;
-  handleSubmit: () => void;
-  submitButtonText?: string;
+  handleSave: () => void;
   lastSavedText?: string;
 }
 
-export const MeasureButtons = ({
-  handleSave,
-  handleSubmit,
-  submitButtonText,
-  lastSavedText,
-}: Props) => {
+export const MeasureButtons = ({ handleSave, lastSavedText }: Props) => {
   const { isStateUser } = useUser();
 
   const showCheck = lastSavedText?.toLowerCase() === "saved moments ago";
 
   return (
-    <CUI.Stack>
-      <CUI.HStack pl="2">
-        {handleSave && (
-          <QMR.ContainedButton
-            disabledStatus={!isStateUser}
-            buttonText={"Save"}
-            buttonProps={{
-              colorScheme: "blue",
-              textTransform: "capitalize",
-              type: "submit",
-            }}
-            onClick={handleSave}
-          />
-        )}
+    <CUI.Stack width="15rem">
+      <CUI.HStack pl="1">
         <QMR.ContainedButton
           disabledStatus={!isStateUser}
-          buttonText={submitButtonText || "Complete Measure"}
+          buttonText={"Save"}
           buttonProps={{
+            minWidth: "10rem",
             colorScheme: "blue",
             textTransform: "capitalize",
-            type: "submit",
           }}
-          onClick={handleSubmit}
+          onClick={handleSave}
         />
       </CUI.HStack>
-      <CUI.Flex
-        justifyContent="center"
-        mb={{ base: "1", lg: "0" }}
-        data-testid="last-saved-text"
-      >
+      <CUI.Flex mb={{ base: "1", lg: "0" }} data-testid="last-saved-text">
         {showCheck && (
           <CUI.Box mt="1">
             <FaCheckCircle data-testid="circle-check-icon" />
           </CUI.Box>
         )}
         {lastSavedText && (
-          <CUI.Text ml="2" fontSize="sm">
+          <CUI.Text ml={showCheck ? 2 : 0} fontSize="sm">
             {lastSavedText}
           </CUI.Text>
         )}
