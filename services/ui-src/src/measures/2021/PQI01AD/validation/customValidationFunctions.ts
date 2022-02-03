5a5c1d65f712689ade376c91e8477a805de99a28
James Dinh <jdinh8124@gmail.com>
2022-02-03 13:04:46 -0800

    working through validating measure errorrs

--- services/ui-src/src/measures/2021/PQI01AD/validation/customValidationFunctions.ts
+++ services/ui-src/src/measures/2021/PQI01AD/validation/customValidationFunctions.ts
@@ -1,14 +1,23 @@
-const PQI01Validation = (data: Measure.Form) => {
+import {
+  atLeastOneRateCompleteSingleArray,
+  validateNumeratorsLessThanDenominatorsSingleArray,
+  validateNoNonZeroNumOrDenomSingleArray,
+  validateEqualDenominatorsSingleArray,
+} from "../../globalValidations/validationsLib";
+const PQI01Validation = (data: any) => {
   const OPM = data["OtherPerformanceMeasure-Rates"];
 
   const performanceMeasureArray = data["PerformanceMeasure-AgeRates"];
   let errorArray: any[] = [];
   errorArray = [
     ...errorArray,
-    ...atLeastOneRateComplete(performanceMeasureArray, OPM),
-    ...validateNumeratorsLessThanDenominators(performanceMeasureArray, OPM),
-    ...validateEqualDenominators(performanceMeasureArray),
-    ...validateNoNonZeroNumOrDenom(performanceMeasureArray, OPM),
+    ...atLeastOneRateCompleteSingleArray(performanceMeasureArray, OPM),
+    ...validateNumeratorsLessThanDenominatorsSingleArray(
+      performanceMeasureArray,
+      OPM
+    ),
+    ...validateEqualDenominatorsSingleArray(performanceMeasureArray),
+    ...validateNoNonZeroNumOrDenomSingleArray(performanceMeasureArray, OPM),
   ];
 
   return errorArray;
--- services/ui-src/src/measures/2021/globalValidations/validationsLib.tsx
+++ services/ui-src/src/measures/2021/globalValidations/validationsLib.tsx
@@ -5,7 +5,6 @@ export const atLeastOneRateComplete = (
   OPM: any,
   ageGroups: string[]
 ) => {
-  debugger;
   let error = true;
   let errorArray: any[] = [];
   const OPMError = OPM ? "Other " : "";
@@ -61,7 +60,7 @@ export const validateDualPopInformation = (
   let errorArray: any[] = [];
   let filledInData: any[] = [];
   const i = age65PlusIndex;
-  performanceMeasureArray.forEach((performanceMeasure) => {
+  performanceMeasureArray.forEach((performanceMeasure: any) => {
     if (
       performanceMeasure &&
       performanceMeasure[i] &&
@@ -101,7 +100,7 @@ export const validateNumeratorsLessThanDenominators = (
   let errorArray: any[] = [];
   const OPMError = OPM ? "Other " : "";
   ageGroups.forEach((_ageGroup, i) => {
-    performanceMeasureArray.forEach((performanceMeasure) => {
+    performanceMeasureArray.forEach((performanceMeasure: any) => {
       if (
         performanceMeasure &&
         performanceMeasure[i] &&
@@ -187,7 +186,7 @@ export const validateNoNonZeroNumOrDenom = (
   let errorArray: any[] = [];
   const OPMError = OPM ? "Other " : "";
   ageGroups.forEach((_ageGroup, i) => {
-    performanceMeasureArray.forEach((performanceMeasure) => {
+    performanceMeasureArray.forEach((performanceMeasure: any) => {
       if (
         performanceMeasure &&
         performanceMeasure[i] &&
@@ -236,6 +235,189 @@ export const validateNoNonZeroNumOrDenom = (
   if (zeroRateError) {
     errorArray.push({
       errorLocation: `${OPMError}Performance Measure`,
+      errorMessage: `Manually entered rate should not be 0 if numerator and denominator are not 0`,
+    });
+  }
+  return zeroRateError || nonZeroRateError ? errorArray : [];
+};
+
+// These validations are for Single Array performance measure arrays
+
+export const atLeastOneRateCompleteSingleArray = (
+  performanceMeasureArray: any,
+  OPMs: any
+) => {
+  let error = true;
+  let errorArray: any[] = [];
+  let OPMError = false;
+
+  // Check OPM first
+  OPMs?.forEach((opm: any) => {
+    if (
+      opm.description &&
+      opm.rate[0]?.denominator &&
+      opm.rate[0]?.numerator &&
+      opm.rate[0]?.rate
+    ) {
+      error = false;
+    }
+
+    if (error) OPMError = true;
+  });
+
+  // Then Check regular Performance Measures
+  performanceMeasureArray?.forEach((performanceObj: any) => {
+    if (
+      performanceObj &&
+      performanceObj.denominator &&
+      performanceObj.numerator
+    ) {
+      error = false;
+    }
+  });
+
+  if (error) {
+    errorArray.push({
+      errorLocation: `${OPMError ? "Other " : ""}Performance Measure`,
+      errorMessage: `At least one performance measure must be completed`,
+    });
+  }
+  return error ? errorArray : [];
+};
+
+export const validateNumeratorsLessThanDenominatorsSingleArray = (
+  performanceMeasureArray: any,
+  OPM: any
+) => {
+  let error = false;
+  let errorArray: any[] = [];
+  let OPMError = false;
+
+  OPM?.forEach((performanceMeasure: any) => {
+    performanceMeasure.rate.forEach((rate: any) => {
+      if (parseInt(rate.numerator) > parseInt(rate.denominator)) {
+        error = true;
+        OPMError = true;
+      }
+    });
+  });
+
+  performanceMeasureArray?.forEach((performanceObj: any) => {
+    if (
+      performanceObj &&
+      performanceObj.denominator &&
+      performanceObj.numerator
+    ) {
+      if (
+        parseFloat(performanceObj.denominator) <
+        parseFloat(performanceObj.numerator)
+      ) {
+        error = true;
+      }
+    }
+  });
+  if (error) {
+    errorArray.push({
+      errorLocation: `${OPMError ? "Other " : ""}Performance Measure`,
+      errorMessage: `Numerators must be less than Denominators for all applicable performance measures`,
+    });
+  }
+  return error ? errorArray : [];
+};
+
+export const validateEqualDenominatorsSingleArray = (
+  performanceMeasureArray: any
+) => {
+  let error;
+  let errorArray: any[] = [];
+  let filledInData: any[] = [];
+
+  performanceMeasureArray?.forEach((performanceObj: any) => {
+    if (performanceObj && performanceObj.denominator) {
+      filledInData.push(performanceObj);
+    }
+  });
+  if (filledInData.length > 1) {
+    const firstDenominator = filledInData[0].denominator;
+    let denominatorsNotEqual = false;
+    filledInData.forEach((_filledInDataObj, index) => {
+      if (filledInData[index].denominator !== firstDenominator) {
+        denominatorsNotEqual = true;
+      }
+    });
+    if (denominatorsNotEqual) {
+      error = {
+        errorLocation: "Performance Measure",
+        errorMessage:
+          "Denominators must be the same for all performance measures",
+      };
+
+      errorArray.push(error);
+    }
+  }
+  return error ? errorArray : [];
+};
+
+// If a user manually over-rides a rate it must not violate two rules:
+// It must be zero if the numerator is zero or
+// It Must be greater than zero if the Num and Denom are greater than zero
+export const validateNoNonZeroNumOrDenomSingleArray = (
+  performanceMeasureArray: any,
+  OPM: any
+) => {
+  let nonZeroRateError = false;
+  let zeroRateError = false;
+  let errorArray: any[] = [];
+  let OPMError = false;
+
+  performanceMeasureArray?.forEach((performanceMeasure: any) => {
+    if (
+      performanceMeasure &&
+      performanceMeasure.denominator &&
+      performanceMeasure.numerator &&
+      performanceMeasure.rate
+    ) {
+      if (
+        parseInt(performanceMeasure.rate) !== 0 &&
+        parseInt(performanceMeasure.numerator) === 0
+      ) {
+        nonZeroRateError = true;
+      }
+      if (
+        parseInt(performanceMeasure.rate) === 0 &&
+        parseInt(performanceMeasure.numerator) !== 0 &&
+        parseInt(performanceMeasure.denominator) !== 0
+      ) {
+        zeroRateError = true;
+      }
+    }
+  });
+
+  OPM?.forEach((performanceMeasure: any) => {
+    performanceMeasure.rate.forEach((rate: any) => {
+      if (parseInt(rate.numerator) === 0 && parseInt(rate.rate) !== 0) {
+        nonZeroRateError = true;
+        OPMError = true;
+      }
+      if (
+        parseInt(rate.numerator) !== 0 &&
+        parseInt(rate.denominator) !== 0 &&
+        parseInt(rate.rate) === 0
+      ) {
+        zeroRateError = true;
+        OPMError = true;
+      }
+    });
+  });
+  if (nonZeroRateError) {
+    errorArray.push({
+      errorLocation: `${OPMError ? "Other " : ""}Performance Measure`,
+      errorMessage: `Manually entered rate should be 0 if numerator is 0`,
+    });
+  }
+  if (zeroRateError) {
+    errorArray.push({
+      errorLocation: `${OPMError ? "Other " : ""}Performance Measure`,
       errorMessage: `Manually entered rate should not be 0 if numerator and denominator are not 0`,
     });
   }
