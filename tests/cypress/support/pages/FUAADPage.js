9606678a37a0b85622db935a16d1d276b5e7a012
James Dinh <jdinh8124@gmail.com>
2022-02-08 12:22:08 -0800

    attempt to fix xpath for validation message

--- tests/cypress/support/pages/FUAADPage.js
+++ tests/cypress/support/pages/FUAADPage.js
@@ -28,7 +28,8 @@ const validateMeasureButton = "//button[@class='chakra-button css-gy0j0y']";
 const completeMeasureButton = "//button[@class='chakra-button css-qobkj']";
 const sentenceUnderCompleteMeasureOne = "//p[@class='chakra-text css-thvrsi']";
 const sentenceUnderCompleteMeasureTwo = "//p[@class='chakra-text css-1xpb69n']";
-const errorMessagePerformanceMeasure = "((//div[@class='css-0'])[27]/div)[1]";
+const errorMessagePerformanceMeasure =
+  "//body/div[@id='root']/div[@id='app-wrapper']/main[@id='main-wrapper']/div[2]/div[1]/form[1]/section[1]";
 const errorMessageContent =
   "//div[contains(text(),'At least one Performance Measure Numerator, Denomi')]";
 const popupBoxMessageHeader = "#yes-no-header";
@@ -358,8 +359,8 @@ export class FUAADPAGE {
 
   clickCompleteMeasureButtonVerify() {
     cy.xpath(completeMeasureButton).click();
-    cy.xpath(popupBoxMessageHeader).should("be.visible");
-    cy.xpath(popupBoxMessageContent).contains(
+    cy.get(popupBoxMessageHeader).should("be.visible");
+    cy.get(popupBoxMessageContent).contains(
       "There are still errors on this measure, would you still like to complete?"
     );
   }
