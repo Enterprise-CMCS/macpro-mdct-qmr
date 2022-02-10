7200e8c2b91ce827fbc5e3675093beb6b583403a
aalousi <aalousi@collabralink.com>
2022-02-10 14:59:42 -0500

    tests for oy2-15211

---
+++ tests/cypress/cypress/integration/oy2_15211_Reporting_in_FY21_Tag_for_Alt_Data_Sources.spec.js
@@ -0,0 +1,20 @@
+const emailForCognito = "//input[@name='email']";
+const passwordForCognito = "//input[@name='password']";
+
+describe("OY2 15211 Reporting in FY21 Tag for Alt Data Sources", () => {
+  beforeEach(() => {
+    // Seed database with test data
+    cy.visit("/");
+    cy.xpath(emailForCognito).type("stateuser1@test.com");
+    cy.xpath(passwordForCognito).type("p@55W0rd!");
+    cy.get('[data-cy="login-with-cognito-button"]').click();
+  });
+
+  it("N/A And Completed Statuses", () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('a > .chakra-text').click();
+    cy.get(':nth-child(22) > :nth-child(3) > .chakra-text').should('have.text', 'N/A');
+    cy.get('.css-1utstxm > .css-dw5ttn > .css-sv7ztq').should('have.text', 'complete');
+    /* ==== End Cypress Studio ==== */
+  });
+});
