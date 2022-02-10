3b6b800425f3d6a1f32d79e5f9b82d403b6ec03e
Gavin St. Ours <gst-ours@fearless.tech>
2022-02-10 12:25:44 -0500

    Improve CompleteMeasureFooter tests

--- services/ui-src/src/components/CompleteMeasureFooter/index.test.tsx
+++ services/ui-src/src/components/CompleteMeasureFooter/index.test.tsx
@@ -1,9 +1,12 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { CompleteMeasureFooter } from "components/CompleteMeasureFooter";
+import config from "config";
 
 describe("Test CompleteMeasureFooter", () => {
   test("Check that the Contained Buttons in this footer component render", () => {
-    const { getByText } = render(
+    config.BRANCH_NAME = "test";
+
+    render(
       <CompleteMeasureFooter
         handleClear={() => {}}
         handleSubmit={() => {}}
@@ -11,9 +14,31 @@ describe("Test CompleteMeasureFooter", () => {
       />
     );
 
-    expect(getByText(/Validate Measure/i)).toBeVisible();
-    expect(getByText(/Complete Measure/i)).toBeVisible();
-    expect(getByText(/Clear Data/i)).toBeVisible();
+    expect(
+      screen.getByRole("button", { name: "Validate Measure" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Complete Measure" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Clear Data" })
+    ).toBeInTheDocument();
+  });
+
+  test("That the Clear Data button does not appear in prod environment", () => {
+    config.BRANCH_NAME = "prod";
+
+    render(
+      <CompleteMeasureFooter
+        handleClear={() => {}}
+        handleSubmit={() => {}}
+        handleValidation={() => {}}
+      />
+    );
+
+    expect(
+      screen.queryByRole("button", { name: "Clear Data" })
+    ).not.toBeInTheDocument();
   });
 
   /* TODO: Possibly mock out the functions for each button, in the interim
