5e43d994068d1255c297c2050160525a536efdcc
James Dinh <jdinh8124@gmail.com>
2022-02-10 09:30:44 -0800

    fixed issues besides x100000 calculations

--- services/ui-src/src/measures/2021/PQI01AD/validation/customValidationFunctions.ts
+++ services/ui-src/src/measures/2021/PQI01AD/validation/customValidationFunctions.ts
@@ -2,21 +2,39 @@ import {
   atLeastOneRateComplete,
   validateNumeratorsLessThanDenominators,
   validateNoNonZeroNumOrDenom,
+  validateDualPopInformation,
 } from "../../globalValidations/validationsLib";
 const PQI01Validation = (data: any) => {
   const OPM = data["OtherPerformanceMeasure-Rates"];
+  const age65PlusIndex = 0;
+  const DefinitionOfDenominator = data["DefinitionOfDenominator"];
 
   const performanceMeasureArray = data["PerformanceMeasure-AgeRates"];
   let errorArray: any[] = [];
+  const performanceMeasureArrayToCheck = performanceMeasureArray?.map(
+    (pma: PerformanceMeasure[]) => [pma]
+  );
+  const validateDualPopInformationArray = [performanceMeasureArrayToCheck[1]];
+
   errorArray = [
     ...errorArray,
-    ...atLeastOneRateComplete([performanceMeasureArray], OPM, ["age-groups"]),
-    ...validateNumeratorsLessThanDenominators([performanceMeasureArray], OPM, [
+    ...atLeastOneRateComplete(performanceMeasureArrayToCheck, OPM, [
       "age-groups",
     ]),
-    ...validateNoNonZeroNumOrDenom([performanceMeasureArray], OPM, [
+    ...validateNumeratorsLessThanDenominators(
+      performanceMeasureArrayToCheck,
+      OPM,
+      ["age-groups"]
+    ),
+    ...validateNoNonZeroNumOrDenom(performanceMeasureArrayToCheck, OPM, [
       "age-groups",
     ]),
+    ...validateDualPopInformation(
+      validateDualPopInformationArray,
+      OPM,
+      age65PlusIndex,
+      DefinitionOfDenominator
+    ),
   ];
 
   return errorArray;
