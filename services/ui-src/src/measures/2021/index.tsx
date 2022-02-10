c01ee9081444fe95f2e81625aca11d0799a18f5b
James Dinh <jdinh8124@gmail.com>
2022-02-10 11:50:59 -0800

    working through automation for fumad

--- services/ui-src/src/measures/2021/index.tsx
+++ services/ui-src/src/measures/2021/index.tsx
@@ -6,23 +6,23 @@ the key should be the measure id as a string (with '-XX' included)
 import { AMMAD } from "./AMMAD";
 import { CPAAD } from "./CPAAD";
 import { FUAAD } from "./FUAAD";
+import { FUMAD } from "./FUMAD";
 import { IETAD } from "./IETAD";
 import { LBWCH } from "./LBWCH";
 import { LRCDCH } from "./LRCDCH";
 import { NCIDDSAD } from "./NCIDDSAD";
 import { PDENTCH } from "./PDENTCH";
-import { FUMAD } from "./FUMAD";
 
 const twentyTwentyOneMeasures = {
   "AMM-AD": AMMAD,
   "CPA-AD": CPAAD,
   "FUA-AD": FUAAD,
+  "FUM-AD": FUMAD,
   "IET-AD": IETAD,
   "LBW-CH": LBWCH,
   "LRCD-CH": LRCDCH,
   "NCIDDS-AD": NCIDDSAD,
   "PDENT-CH": PDENTCH,
-  "FUM-AD": FUMAD,
 };
 
 export default twentyTwentyOneMeasures;
