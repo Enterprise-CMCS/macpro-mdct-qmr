111c97641616798a91273e56b8b6b2be938be435
aalousi <aalousi@collabralink.com>
2022-02-11 12:54:43 -0500

    updating element on href

--- tests/cypress/cypress/integration/oy2_7763_FAQ_Page_State_View.spec.js
+++ tests/cypress/cypress/integration/oy2_7763_FAQ_Page_State_View.spec.js
@@ -10,7 +10,9 @@ describe("OY2 7763 FAQ Page State View ", () => {
     cy.get('[data-cy="login-with-cognito-button"]').click();
   });
   it("Verify FAQ Page Exists", () => {
-    cy.get('[href="/faq"] > svg > path').click();
+    cy.get(
+      "body.chakra-ui-light:nth-child(2) div.css-4ma18r:nth-child(1) div.css-1gy6ge8 div.chakra-container.css-1vsk1kk div.css-1ibkps6 > a:nth-child(4)"
+    ).click();
     cy.get(".chakra-heading").should("have.text", "FAQ");
   });
 });
