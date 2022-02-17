f285e1116a95d43deca4b277126c2bdb68e5c359
aalousi <aalousi@collabralink.com>
2022-02-17 10:58:46 -0500

    adding wait times

--- tests/cypress/cypress/integration/oy2_16039_Data_source_Rate_to_auto_calculate_in_OPM.spec.js
+++ tests/cypress/cypress/integration/oy2_16039_Data_source_Rate_to_auto_calculate_in_OPM.spec.js
@@ -13,7 +13,9 @@ describe("OY2 16039 Data source/ Rate to auto calculate in OPM", () => {
   it("Check Auto Calculation with Administrative Data", () => {
     cy.get('[data-cy="ACS"]').click();
     cy.xpath("//p[contains(text(),'FUA-AD')]").click();
-    cy.xpath("//button[contains(text(),'Clear Data')]").click();
+    cy.wait(2000);
+    cy.get(`[data-cy="Clear Data"]`).click();
+    cy.wait(2000);
     cy.xpath("//p[contains(text(),'FUA-AD')]").click();
     cy.get(
       '[data-cy="DataSource0"] > .chakra-checkbox__label > .chakra-text'
@@ -41,7 +43,9 @@ describe("OY2 16039 Data source/ Rate to auto calculate in OPM", () => {
   it("Check Auto Calculation with Other", () => {
     cy.get('[data-cy="ACS"]').click();
     cy.xpath("//p[contains(text(),'FUA-AD')]").click();
+    cy.wait(2000);
     cy.xpath("//button[contains(text(),'Clear Data')]").click();
+    cy.wait(2000);
     cy.xpath("//p[contains(text(),'FUA-AD')]").click();
     cy.get("#MeasurementSpecification-Other").click({ force: true });
     cy.get(
@@ -60,18 +64,18 @@ describe("OY2 16039 Data source/ Rate to auto calculate in OPM", () => {
       '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.denominator"]'
     ).type("2");
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.rate"]').click();
-    cy.get(":nth-child(8) > .css-1bpnzr3").click();
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.rate"]').click();
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.rate"]').clear();
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.rate"]').type(
       "60"
     );
-    cy.get(":nth-child(8) > .css-1bpnzr3").click();
   });
   it("Check Auto Calculation with Administrative Data and Other", () => {
     cy.get('[data-cy="ACS"]').click();
     cy.xpath("//p[contains(text(),'FUA-AD')]").click();
+    cy.wait(2000);
     cy.xpath("//button[contains(text(),'Clear Data')]").click();
+    cy.wait(2000);
     cy.xpath("//p[contains(text(),'FUA-AD')]").click();
     cy.get("#MeasurementSpecification-Other").click({ force: true });
     cy.get(
@@ -96,6 +100,5 @@ describe("OY2 16039 Data source/ Rate to auto calculate in OPM", () => {
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.rate"]').type(
       "60"
     );
-    cy.get(":nth-child(8) > .css-1bpnzr3 > .css-0").click();
   });
 });
