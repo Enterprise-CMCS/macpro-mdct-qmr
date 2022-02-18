d6638e968c4114fa5913781fb7d9b099a1f139a9
JP Hechter <jhechter@fearless.tech>
2022-02-18 13:00:33 -0500

    implement StatusOfData component in MSCAD

--- services/ui-src/src/measures/2021/MSCAD/questions/index.tsx
+++ services/ui-src/src/measures/2021/MSCAD/questions/index.tsx
@@ -6,7 +6,6 @@ export * from "./DefinitionOfPopulation";
 export * from "./DeviationFromMeasureSpec";
 export * from "./MeasurementSpecification";
 export * from "./Reporting";
-export * from "./Status";
 export * from "./WhyAreYouNotReporting";
 export * from "./PerformanceMeasure";
 export * from "./OtherPerformanceMeasure";
