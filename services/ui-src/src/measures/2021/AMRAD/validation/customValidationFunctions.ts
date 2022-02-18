7228e7204d02430650befea75903ff0f37aae29c
JP Hechter <jhechter@fearless.tech>
2022-02-18 14:10:07 -0500

    prettier

--- services/ui-src/src/measures/2021/AMRAD/validation/customValidationFunctions.ts
+++ services/ui-src/src/measures/2021/AMRAD/validation/customValidationFunctions.ts
@@ -8,12 +8,12 @@ import {
 
 const AMRADValidation = (data: Measure.Form) => {
   const ageGroups = ["Ages 19 to 50", "Ages 51 to 64", "Total (Ages 19 to 64)"];
-  
+
   const OPM = data["OtherPerformanceMeasure-Rates"];
   const performanceMeasureArray = [
     data["PerformanceMeasure-AgeRates-Persistent-Asthma"],
   ];
-  
+
   let errorArray: any[] = [];
   errorArray = [
     ...atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
