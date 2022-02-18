6a83785f92ed8bcdc3df35cc1f95d0114d53df1a
JP Hechter <jhechter@fearless.tech>
2022-02-18 12:53:56 -0500

    implement StatusOfData component in CCPAD

--- services/ui-src/src/measures/2021/CCPAD/index.tsx
+++ services/ui-src/src/measures/2021/CCPAD/index.tsx
@@ -1,4 +1,5 @@
 import * as Q from "./questions";
+import * as CMQ from "../CommonQuestions";
 import { useFormContext, useWatch } from "react-hook-form";
 import { Measure } from "./validation/types";
 import { useEffect } from "react";
@@ -91,7 +92,7 @@ export const CCPAD = ({
 
       {!watchReportingRadio?.includes("No") && (
         <>
-          <Q.Status />
+          <CMQ.StatusOfData />
           <Q.MeasurementSpecification />
           <Q.DataSource />
           <Q.DateRange type="adult" />
