085d00a8745640b1a6eb4777076583bd960d947d
Gavin St. Ours <gst-ours@fearless.tech>
2022-02-22 12:54:22 -0500

    Write first part of total test in rate component

--- services/ui-src/src/components/Rate/index.test.tsx
+++ services/ui-src/src/components/Rate/index.test.tsx
@@ -2,6 +2,7 @@ import { screen } from "@testing-library/react";
 import fireEvent from "@testing-library/user-event";
 import { Rate } from ".";
 import { renderWithHookForm } from "utils/testUtils/reactHookFormRenderer";
+import userEvent from "@testing-library/user-event";
 
 const TestComponent = () => {
   const rates = [
@@ -30,6 +31,93 @@ const TestComponent2 = () => {
 
   return <Rate rates={rates} name="test-component" readOnly={false} />;
 };
+
+const TestTotalComponent = () => {
+  const rates = [
+    {
+      label: "test1",
+      denominator: "",
+      numerator: "",
+      rate: "",
+      id: 1,
+    },
+    {
+      label: "test2",
+      denominator: "",
+      numerator: "",
+      rate: "",
+      id: 2,
+    },
+    {
+      label: "total",
+      denominator: "",
+      numerator: "",
+      rate: "",
+      id: 3,
+      isTotal: true,
+    },
+  ];
+
+  return <Rate rates={rates} name="test-total-component" readOnly={false} />;
+};
+
+describe.only("Test the total Rate component", () => {
+  beforeEach(() => {
+    renderWithHookForm(<TestTotalComponent />, {
+      defaultValues: {
+        "test-total-component": [
+          {
+            numerator: "1",
+            denominator: "1",
+            rate: "1",
+          },
+          {
+            numerator: "2",
+            denominator: "2",
+            rate: "1",
+          },
+        ],
+      },
+    });
+  });
+
+  test("Check that the components render and include their labels", () => {
+    expect(screen.getByText(/test1/i)).toBeVisible();
+    expect(screen.getByText(/test2/i)).toBeVisible();
+    expect(screen.getByText(/total/i)).toBeVisible();
+  });
+
+  test("Check numerators for expected default values", () => {
+    expect(screen.getAllByLabelText(/numerator/i)[0]).toHaveValue("1");
+    expect(screen.getAllByLabelText(/numerator/i)[1]).toHaveValue("2");
+    expect(screen.getAllByLabelText(/numerator/i)[2]).toHaveValue("");
+  });
+
+  test("Check denominators for expected default values", () => {
+    expect(screen.getAllByLabelText(/denominator/i)[0]).toHaveValue("1");
+    expect(screen.getAllByLabelText(/denominator/i)[1]).toHaveValue("2");
+    expect(screen.getAllByLabelText(/denominator/i)[2]).toHaveValue("");
+  });
+
+  test("Check that numerator total is calculated correctly", () => {
+    const numerator1 = screen.getAllByLabelText(/numerator/i)[0];
+    const numerator2 = screen.getAllByLabelText(/numerator/i)[1];
+
+    const numeratorTotal = screen.getAllByLabelText(/numerator/i)[2];
+    const denominatorTotal = screen.getAllByLabelText(/denominator/i)[2];
+
+    // Change the numerator from 1 to 5
+    userEvent.type(numerator1, "5");
+    expect(numerator1).toHaveValue("5");
+    // Expect the rate to add the numerator (5) and denominator (2)
+    expect(numeratorTotal).toHaveValue("7");
+    expect(denominatorTotal).toHaveValue("3");
+
+    userEvent.type(numerator2, "5");
+    expect(numerator2).toHaveValue("5");
+    expect(numeratorTotal).toHaveValue("10");
+  });
+});
 
 describe("Test the Rate component", () => {
   beforeEach(() => {
