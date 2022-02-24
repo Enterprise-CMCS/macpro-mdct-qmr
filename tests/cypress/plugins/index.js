d6304470902bee19ce4543c1c9adcdec2c6ca5a8
aalousi <aalousi@collabralink.com>
2022-02-24 10:57:35 -0500

    change

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,9 +1,3 @@
-module.exports = (on, config) => {
-  config.baseUrl = process.env.APPLICATION_ENDPOINT || "http://localhost:3000";
-
-  return config;
-};
-
 const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = (on, config) => {
