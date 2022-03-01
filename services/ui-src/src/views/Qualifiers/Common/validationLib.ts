072d1d28b3d2bcf05eacf6b7da4e690cfb904794
James Dinh <jdinh8124@gmail.com>
2022-03-01 07:21:01 -0800

    Oy2 16265  - Validation text needs to appear in CH/AD qualifiers (#579)
    * working through adding a handle validation button
    
    * match pattern for validating vs submitting
    
    * working through validating submit methods
    
    * fixed issue where we were using the wrong variable to save when completing a measure
    
    * added validation logic to qualifiers
    
    * remove unnecessary optional chain
    
    * cleanned component with shared lib in common folder
    
    * adjust test that were failing
    
    * adjusted logic to use navigation to move to the previous page when successfully submitted
    
    * added test oy2-16265 and updated yml file
    
    * prettier format
    
    * updates on oy2-16265
    
    * updates on oy2-16265
    
    * updates on oy2-16265
    
    * added extra waiting time for oy2-16265
    
    * prettier format
    
    Co-authored-by: Guli <gabudusalamu@collabralink.com>
    Co-authored-by: Guli <72164092+GL2020-QM@users.noreply.github.com>

---
+++ services/ui-src/src/views/Qualifiers/Common/validationLib.ts
@@ -0,0 +1,26 @@
+import { ACSQualifierForm } from "../Adult/types";
+import { CCSQualifierForm } from "../Child/types";
+import { CCSCQualifierForm } from "../ChildCHIP/types";
+import { CCSMQualifierForm } from "../ChildMedicaid/types";
+
+export function validateData(
+  validationFunctions: Function[],
+  data:
+    | CCSMQualifierForm
+    | CCSQualifierForm
+    | ACSQualifierForm
+    | CCSCQualifierForm
+): any {
+  return validationFunctions.reduce((acc: any, current: any) => {
+    const error = current(data);
+    let errorArray = [];
+
+    if (Array.isArray(error)) {
+      errorArray = [...error];
+    } else {
+      errorArray = [error];
+    }
+
+    return error ? [...acc, ...errorArray] : acc;
+  }, []);
+}
