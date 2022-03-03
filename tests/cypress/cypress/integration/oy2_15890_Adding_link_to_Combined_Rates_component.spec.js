4bfdec104240edddb2f0fbf063cfa8f6bdbc1f76
pboggarapu <77852304+pboggarapu@users.noreply.github.com>
2022-03-03 15:51:26 -0500

    State-Level Rate Brief link

---
+++ tests/cypress/cypress/integration/oy2_15890_Adding_link_to_Combined_Rates_component.spec.js
@@ -0,0 +1,26 @@
+const emailForCognito = "//input[@name='email']";
+const passwordForCognito = "//input[@name='password']";
+
+describe("OY2 16411 Restructuring Data Source Text boxes", () => {
+  beforeEach(() => {
+    // Seed database with test data
+    cy.visit("/");
+    cy.xpath(emailForCognito).type("stateuser1@test.com");
+    cy.xpath(passwordForCognito).type("p@55W0rd!");
+    cy.get('[data-cy="login-with-cognito-button"]').click();
+  });
+
+  it("Verify that user is able to click State-Level Rate Brief and PDF opens in a new tab", () => {
+    cy.get('[data-cy="ACS"]').click();
+    cy.get('[data-cy="CCP-AD"]').click();
+    cy.wait(2000);
+    cy.xpath("//button[contains(text(),'Clear Data')]").click();
+    cy.wait(2000);
+    cy.get('[data-cy="CCP-AD"]').click();
+    cy.get('[data-cy="Combined Rate(s) from Multiple Reporting Units"]').should('have.text', 'Combined Rate(s) from Multiple Reporting Units');
+    cy.get('.css-bxak8j').should('have.text', 'For additional information refer to the State-Level Rate Brief.');   
+    cy.get('.css-bxak8j > .chakra-link').click();
+    
+  });
+
+  });
