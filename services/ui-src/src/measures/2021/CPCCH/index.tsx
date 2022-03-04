88c816a7cbd7b1b999f441ac211b1e2c557381e0
James Dinh <jdinh8124@gmail.com>
2022-03-04 13:05:20 -0800

    copied and pasted structure

---
+++ services/ui-src/src/measures/2021/CPCCH/index.tsx
@@ -0,0 +1,31 @@
+import * as Q from "./questions";
+import * as CMQ from "measures/CommonQuestions";
+import { useParams } from "react-router-dom";
+import * as Types from "measures/CommonQuestions/types";
+import { useFormContext } from "react-hook-form";
+
+export const CPCCH = ({ name, year }: Types.MeasureWrapperProps) => {
+  const { watch } = useFormContext<Types.DefaultFormData>();
+  const { coreSetId } = useParams();
+  const data = watch();
+
+  return (
+    <>
+      <Q.Reporting
+        reportingYear={year}
+        measureName={name}
+        measureAbbreviation={coreSetId as string}
+      />
+      {data["DidCollect"] !== "no" && (
+        <>
+          <Q.HowDidYouReport />
+          <CMQ.MeasurementSpecification type="AHRQ" />
+          <Q.DataSource />
+          <Q.DefinitionOfPopulation />
+          <Q.PerformanceMeasure />
+        </>
+      )}
+      <CMQ.AdditionalNotes />
+    </>
+  );
+};
