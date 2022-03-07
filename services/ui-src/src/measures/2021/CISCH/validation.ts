1628fe3c658da4ab4955ece11c3c4c56fcd1188d
James Dinh <jdinh8124@gmail.com>
2022-03-07 14:05:44 -0800

    working through validations

--- services/ui-src/src/measures/2021/CISCH/validation.ts
+++ services/ui-src/src/measures/2021/CISCH/validation.ts
@@ -2,7 +2,6 @@ import * as PMD from "./data";
 import {
   atLeastOneRateComplete,
   ensureBothDatesCompletedInRange,
-  validateDualPopInformation,
   validateNumeratorsLessThanDenominators,
   validateEqualDenominators,
   validateNoNonZeroNumOrDenom,
@@ -14,24 +13,10 @@ import { FormData } from "./types";
 
 const CISCHValidation = (data: FormData) => {
   const ageGroups = PMD.qualifiers;
-  const age65PlusIndex = 1;
   const whyNotReporting = data["WhyAreYouNotReporting"];
   const OPM = data["OtherPerformanceMeasure-Rates"];
   const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
   const dateRange = data["DateRange"];
-  const DefinitionOfDenominator = data["DefinitionOfDenominator"];
-
-  const totalInitiation = performanceMeasureArray.filter(
-    (_, idx) =>
-      PMD.data.categories?.[idx].includes("Initiation") &&
-      PMD.data.categories?.[idx].includes("Total")
-  )[0];
-
-  const totalEngagement = performanceMeasureArray.filter(
-    (_, idx) =>
-      PMD.data.categories?.[idx].includes("Engagement") &&
-      PMD.data.categories?.[idx].includes("Total")
-  )[0];
 
   let errorArray: any[] = [];
   if (data["DidReport"] === "no") {
@@ -39,45 +24,15 @@ const CISCHValidation = (data: FormData) => {
     return errorArray;
   }
 
-  let unfilteredSameDenominatorErrors: any[] = [];
-  for (let i = 0; i < performanceMeasureArray.length; i += 2) {
-    unfilteredSameDenominatorErrors = [
-      ...unfilteredSameDenominatorErrors,
-      ...validateEqualDenominators(
-        [performanceMeasureArray[i], performanceMeasureArray[i + 1]],
-        ageGroups
-      ),
-    ];
-  }
-  unfilteredSameDenominatorErrors = [
-    ...unfilteredSameDenominatorErrors,
-    ...validateEqualDenominators([totalInitiation, totalEngagement], ageGroups),
-  ];
-
-  let filteredSameDenominatorErrors: any = [];
-  let errorList: string[] = [];
-  unfilteredSameDenominatorErrors.forEach((error) => {
-    if (!(errorList.indexOf(error.errorMessage) > -1)) {
-      errorList.push(error.errorMessage);
-      filteredSameDenominatorErrors.push(error);
-    }
-  });
-
   errorArray = [
     ...errorArray,
     ...atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
-    ...validateDualPopInformation(
-      performanceMeasureArray,
-      OPM,
-      age65PlusIndex,
-      DefinitionOfDenominator
-    ),
     ...validateNumeratorsLessThanDenominators(
       performanceMeasureArray,
       OPM,
       ageGroups
     ),
-    ...filteredSameDenominatorErrors,
+    ...validateEqualDenominators(performanceMeasureArray, ageGroups),
     ...validateNoNonZeroNumOrDenom(performanceMeasureArray, OPM, ageGroups),
     ...validateRequiredRadioButtonForCombinedRates(data),
     ...ensureBothDatesCompletedInRange(dateRange),
