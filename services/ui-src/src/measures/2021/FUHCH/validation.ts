c713172309b9e11ed4f9177d31d3f41aac7e40c6
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-03-08 10:41:28 -0600

    add custom rate validation for 7 days and 30 days

--- services/ui-src/src/measures/2021/FUHCH/validation.ts
+++ services/ui-src/src/measures/2021/FUHCH/validation.ts
@@ -11,6 +11,32 @@ import {
 import { getPerfMeasureRateArray } from "../../globalValidations";
 import { FormData } from "./types";
 
+const validate7DaysGreaterThan30Days = (data: any) => {
+  const sevenDays = data["PerformanceMeasure"]["rates"]["7DayFollowUp"];
+  const thirtyDays = data["PerformanceMeasure"]["rates"]["30DayFollowUp"];
+  let error;
+  const errorArray: any[] = [];
+
+  if (sevenDays && thirtyDays) {
+    console.log(sevenDays, thirtyDays);
+    sevenDays.forEach((_sevenDaysObj: any, index: any) => {
+      if (
+        sevenDays[index] &&
+        thirtyDays[index] &&
+        parseFloat(sevenDays[index]?.rate) > parseFloat(thirtyDays[index]?.rate)
+      ) {
+        error = {
+          errorLocation: "Performance Measure",
+          errorMessage: "7 Days Rate should not be higher than 30 Days Rate",
+        };
+
+        errorArray.push(error);
+      }
+    });
+  }
+  return errorArray;
+};
+
 const FUHValidation = (data: FormData) => {
   const ageGroups = PMD.qualifiers;
   const whyNotReporting = data["WhyAreYouNotReporting"];
@@ -23,7 +49,6 @@ const FUHValidation = (data: FormData) => {
     errorArray = [...validateReasonForNotReporting(whyNotReporting)];
     return errorArray;
   }
-
   let unfilteredSameDenominatorErrors: any[] = [];
   for (let i = 0; i < performanceMeasureArray.length; i += 2) {
     unfilteredSameDenominatorErrors = [
@@ -56,6 +81,7 @@ const FUHValidation = (data: FormData) => {
     ...validateNoNonZeroNumOrDenom(performanceMeasureArray, OPM, ageGroups),
     ...validateRequiredRadioButtonForCombinedRates(data),
     ...ensureBothDatesCompletedInRange(dateRange),
+    ...validate7DaysGreaterThan30Days(data),
   ];
 
   return errorArray;
