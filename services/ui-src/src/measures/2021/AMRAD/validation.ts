7d929af91a6f42cc2e54b0ef3d19a2a229b4ce22
Gavin St. Ours <gst-ours@fearless.tech>
2022-03-09 13:48:57 -0500

    Display correct validation warning when not reporting

--- services/ui-src/src/measures/2021/AMRAD/validation.ts
+++ services/ui-src/src/measures/2021/AMRAD/validation.ts
@@ -6,16 +6,22 @@ import {
   validateNumeratorsLessThanDenominators,
   validateEqualDenominators,
   validateNoNonZeroNumOrDenom,
+  validateReasonForNotReporting,
   validateTotalNDR,
 } from "measures/globalValidations/validationsLib";
 
 const AMRADValidation = (data: Measure.Form) => {
   const ageGroups = ["Ages 19 to 50", "Ages 51 to 64", "Total (Ages 19 to 64)"];
-
   const OPM = data["OtherPerformanceMeasure-Rates"];
   const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
+  const whyNotReporting = data["WhyAreYouNotReporting"];
 
   let errorArray: any[] = [];
+  if (data["DidReport"] === "no") {
+    errorArray = [...validateReasonForNotReporting(whyNotReporting)];
+    return errorArray;
+  }
+
   errorArray = [
     ...atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
     ...validateNumeratorsLessThanDenominators(
