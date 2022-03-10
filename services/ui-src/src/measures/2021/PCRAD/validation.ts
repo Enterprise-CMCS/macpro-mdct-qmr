b17e2e0cfdaa2be9c924902f505ef43c8e8cc9f4
JP Hechter <jhechter@fearless.tech>
2022-03-10 12:58:03 -0500

    finish adding support for validations found in other measures

--- services/ui-src/src/measures/2021/PCRAD/validation.ts
+++ services/ui-src/src/measures/2021/PCRAD/validation.ts
@@ -2,10 +2,15 @@ import { Measure } from "./types";
 import { getPerfMeasureRateArray } from "measures/globalValidations";
 import {
   ensureBothDatesCompletedInRange,
-  validateNoNonZeroNumOrDenom,
   validateRequiredRadioButtonForCombinedRates,
 } from "../../globalValidations/validationsLib";
 import * as PMD from "./data";
+
+interface NDRforumla {
+  numerator: number;
+  denominator: number;
+  rateIndex: number;
+}
 
 const PCRADValidation = (data: Measure.Form) => {
   let errorArray: any[] = [];
@@ -14,29 +19,104 @@ const PCRADValidation = (data: Measure.Form) => {
   const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
   const OPM = data["OtherPerformanceMeasure-Rates"];
 
-  console.log(performanceMeasureArray);
+  const ndrForumlas = [
+    {
+      numerator: 1,
+      denominator: 0,
+      rateIndex: 2,
+    },
+    {
+      numerator: 3,
+      denominator: 0,
+      rateIndex: 4,
+    },
+    {
+      numerator: 1,
+      denominator: 3,
+      rateIndex: 5,
+    },
+    {
+      numerator: 7,
+      denominator: 6,
+      rateIndex: 8,
+    },
+  ];
+
+  // Quick reference list of all rate indices
+  // const rateLocations = ndrForumlas.map((ndr) => ndr.rateIndex);
+
+  // console.log(performanceMeasureArray);
   errorArray = [
     ...PCRADatLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
     ...ensureBothDatesCompletedInRange(dateRange),
+    ...PCRADnoNonZeroNumOrDenom(performanceMeasureArray, OPM, ndrForumlas),
   ];
   return errorArray;
 };
 
-const validateNonZeroDenom = (data: Measure.Form) => {
-  const memeRates =
-    data.PerformanceMeasure?.rates?.[
-      `${PMD.qualifiers[0].replace(/[^\w]/g, "")}`
-    ] ?? [];
-  const larcRates =
-    data.PerformanceMeasure?.rates?.[
-      `${PMD.qualifiers[1].replace(/[^\w]/g, "")}`
-    ] ?? [];
+/* Validation for manually entered rates */
+const PCRADnoNonZeroNumOrDenom = (
+  performanceMeasureArray: any,
+  OPM: any,
+  ndrFormulas: NDRforumla[]
+) => {
+  let nonZeroRateError = false;
+  let zeroRateError = false;
+  let errorArray: any[] = [];
+  performanceMeasureArray?.forEach((performanceMeasure: any) => {
+    if (performanceMeasure) {
+      ndrFormulas.forEach((ndr: NDRforumla) => {
+        if (
+          performanceMeasure[ndr.numerator].value &&
+          performanceMeasure[ndr.denominator].value &&
+          performanceMeasure[ndr.rateIndex].value
+        ) {
+          if (
+            parseFloat(performanceMeasure[ndr.rateIndex].value!) !== 0 &&
+            parseFloat(performanceMeasure[ndr.numerator].value!) === 0
+          ) {
+            nonZeroRateError = true;
+          }
+          if (
+            parseFloat(performanceMeasure[ndr.rateIndex].value!) === 0 &&
+            parseFloat(performanceMeasure[ndr.numerator].value!) !== 0 &&
+            parseFloat(performanceMeasure[ndr.denominator].value!) !== 0
+          ) {
+            zeroRateError = true;
+          }
+        }
+      });
+    }
+  });
 
-  return validateNoNonZeroNumOrDenom(
-    [memeRates, larcRates],
-    data["OtherPerformanceMeasure-Rates"],
-    [""]
-  );
+  OPM &&
+    OPM.forEach((performanceMeasure: any) => {
+      performanceMeasure.rate.forEach((rate: any) => {
+        if (parseFloat(rate.numerator) === 0 && parseFloat(rate.rate) !== 0) {
+          nonZeroRateError = true;
+        }
+        if (
+          parseFloat(rate.numerator) !== 0 &&
+          parseFloat(rate.denominator) !== 0 &&
+          parseFloat(rate.rate) === 0
+        ) {
+          zeroRateError = true;
+        }
+      });
+    });
+  if (nonZeroRateError) {
+    errorArray.push({
+      errorLocation: `Performance Measure/Other Performance Measure`,
+      errorMessage: `Manually entered rate should be 0 if numerator is 0`,
+    });
+  }
+  if (zeroRateError) {
+    errorArray.push({
+      errorLocation: `Performance Measure/Other Performance Measure`,
+      errorMessage: `Manually entered rate should not be 0 if numerator and denominator are not 0. If the calculated rate is less than 0.5, disregard this validation.`,
+    });
+  }
+  return zeroRateError || nonZeroRateError ? errorArray : [];
 };
 
 /* At least one NDR set must be complete (OPM or PM) */
@@ -47,6 +127,7 @@ const PCRADatLeastOneRateComplete = (
 ) => {
   let error = true;
   let errorArray: any[] = [];
+
   // Check OPM first
   OPM &&
     OPM.forEach((measure: any) => {
@@ -55,7 +136,7 @@ const PCRADatLeastOneRateComplete = (
       }
     });
 
-  // Then check regular Performance Measures if cannot validate OPM
+  // Check regular Performance Measures if cannot validate OPM
   // For each Performance Measure
   //    Check that the performance measure has a field representation for each age groups
   //    Check that each field has a "value" and it is not an empty string
@@ -84,6 +165,5 @@ const PCRADatLeastOneRateComplete = (
 
 export const validationFunctions = [
   PCRADValidation,
-  validateNonZeroDenom,
   validateRequiredRadioButtonForCombinedRates,
 ];
