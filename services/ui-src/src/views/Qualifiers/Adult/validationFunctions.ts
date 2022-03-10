2616f838c52dc1e941122fae96e72329480d94b8
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-03-10 12:14:20 -0600

    add validation

--- services/ui-src/src/views/Qualifiers/Adult/validationFunctions.ts
+++ services/ui-src/src/views/Qualifiers/Adult/validationFunctions.ts
@@ -3,6 +3,7 @@ import { DeliverySystem } from "./types";
 
 const validate21To64EqualsToOneHundredPercent = (data: ACSQualifierForm) => {
   const values = data["PercentageEnrolledInEachDeliverySystem"];
+  console.log(values);
   const errorArray: any[] = [];
   const total21To64Percent = values?.reduce(
     (acc: number, curr: DeliverySystem) => {
@@ -11,15 +12,28 @@ const validate21To64EqualsToOneHundredPercent = (data: ACSQualifierForm) => {
     0
   );
 
-  if (total21To64Percent === 0) {
+  const total64PlusPercent = values?.reduce(
+    (acc: number, curr: DeliverySystem) => {
+      return acc + parseFloat(curr.GreaterThanSixtyFour || "0");
+    },
+    0
+  );
+
+  if (total21To64Percent === 0 || total64PlusPercent === 0) {
     errorArray.push({
       errorLocation: "Delivery System",
-      errorMessage: "Entries for Ages 21 to 64 column must have values",
+      errorMessage: "Entries for each age group's column must have values",
     });
-  } else if (total21To64Percent < 99 || total21To64Percent > 101) {
+  }
+  if (
+    total21To64Percent < 99 ||
+    total21To64Percent > 101 ||
+    total64PlusPercent < 99 ||
+    total64PlusPercent > 101
+  ) {
     errorArray.push({
       errorLocation: "Delivery System",
-      errorMessage: "Entries for Ages 21 to 64 column must total 100",
+      errorMessage: "Entries for each age group's column must total 100",
     });
   }
   return errorArray.length ? errorArray : [];
