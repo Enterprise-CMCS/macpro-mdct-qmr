4ac317546ed7827e6c8f4fadc77c79fb38614329
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-03-10 10:10:26 -0600

    add description

--- services/ui-src/src/measures/2021/BCSAD/data.ts
+++ services/ui-src/src/measures/2021/BCSAD/data.ts
@@ -33,6 +33,7 @@ export const dataSourceData: DataDrivenTypes.DataSource = {
     },
     {
       value: "Electronic Health Records",
+      description: true,
     },
     {
       value: "Other Data Source",
