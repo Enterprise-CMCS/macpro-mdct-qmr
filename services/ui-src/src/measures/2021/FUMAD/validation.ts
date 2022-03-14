d4d7c38dcad0387bdb2bbf8520b8c7c145800d18
Daniel Belcher <daniel.belcher.cs@gmail.com>
2022-03-14 12:51:56 -0500

    remove duplicate validation

--- services/ui-src/src/measures/2021/FUMAD/validation.ts
+++ services/ui-src/src/measures/2021/FUMAD/validation.ts
@@ -70,129 +70,6 @@ const FUMADValidation = (data: FormData) => {
   return errorArray;
 };
 
-const validateDualPopulationInformation = (data: FormData) => {
-  const sevenDays65orOlder =
-    data.PerformanceMeasure?.rates?.[
-      `${PMD.categories[1].replace(/[^\w]/g, "")}`
-    ] ?? [];
-  const thirtyDays65orOlder =
-    data.PerformanceMeasure?.rates?.[
-      `${PMD.categories[0].replace(/[^\w]/g, "")}`
-    ] ?? [];
-  const DualEligibleCheck = data["DefinitionOfDenominator"] ?? [];
-
-  let error;
-
-  if (sevenDays65orOlder || thirtyDays65orOlder) {
-    if (sevenDays65orOlder[1] || thirtyDays65orOlder[1]) {
-      if (
-        sevenDays65orOlder[1]?.numerator ||
-        thirtyDays65orOlder[1]?.numerator ||
-        sevenDays65orOlder[1]?.denominator ||
-        thirtyDays65orOlder[1]?.denominator
-      ) {
-        if (
-          DualEligibleCheck.indexOf(
-            "DenominatorIncMedicareMedicaidDualEligible"
-          ) === -1
-        ) {
-          error = {
-            errorLocation: "Performance Measure",
-            errorMessage:
-              "Information has been included in the Age 65 and older Performance Measure but the checkmark for (Denominator Includes Medicare and Medicaid Dually-Eligible population) is missing",
-          };
-        }
-      }
-    }
-  }
-  if (
-    DualEligibleCheck.indexOf("DenominatorIncMedicareMedicaidDualEligible") !==
-    -1
-  ) {
-    if (!sevenDays65orOlder && !thirtyDays65orOlder) {
-      error = {
-        errorLocation: "Performance Measure",
-        errorMessage:
-          "Missing data on Performance Measure for Age 65 and older",
-      };
-    } else if (!sevenDays65orOlder[1] && !thirtyDays65orOlder[1]) {
-      error = {
-        errorLocation: "Performance Measure",
-        errorMessage:
-          "Missing data on Performance Measure for Age 65 and older",
-      };
-    } else if (
-      (!sevenDays65orOlder[1]?.numerator || // either not filled in
-        !sevenDays65orOlder[1]?.denominator) && // either not filled in
-      !thirtyDays65orOlder[1]?.numerator && //both filled in
-      !thirtyDays65orOlder[1]?.denominator //both filled in
-    ) {
-      return {
-        errorLocation: "Performance Measure",
-        errorMessage:
-          "Missing data on Performance Measure for Age 65 and older",
-      };
-    } else if (
-      (!thirtyDays65orOlder[1]?.numerator ||
-        !thirtyDays65orOlder[1]?.denominator) &&
-      !sevenDays65orOlder[1]?.numerator &&
-      !sevenDays65orOlder[1]?.denominator
-    ) {
-      return {
-        errorLocation: "Performance Measure",
-        errorMessage:
-          "Missing data on Performance Measure for Age 65 and older",
-      };
-    }
-  }
-  return error;
-};
-
-const validateAtLeastOneNDRSet = (data: FormData) => {
-  let error;
-  const measureSpecification = data["MeasurementSpecification"];
-  const sevenDays =
-    data.PerformanceMeasure?.rates?.[
-      `${PMD.categories[1].replace(/[^\w]/g, "")}`
-    ] ?? [];
-  const thirtyDays =
-    data.PerformanceMeasure?.rates?.[
-      `${PMD.categories[0].replace(/[^\w]/g, "")}`
-    ] ?? [];
-  const otherPerformanceRates = data["OtherPerformanceMeasure-Rates"] ?? [];
-  const isHEDIS = measureSpecification === "NCQA/HEDIS";
-
-  let doesOtherNDRExist = false;
-  otherPerformanceRates.forEach((ndr) => {
-    const ndrRate = ndr?.rate?.[0]?.rate;
-    if (ndrRate) {
-      doesOtherNDRExist = true;
-    }
-  });
-
-  if (
-    isHEDIS &&
-    !sevenDays?.[0]?.rate &&
-    !sevenDays?.[1]?.rate &&
-    !thirtyDays?.[0]?.rate &&
-    !thirtyDays?.[1]?.rate
-  ) {
-    error = {
-      errorLocation: "Performance Measure",
-      errorMessage:
-        "At least one Performance Measure Numerator, Denominator, and Rate must be completed",
-    };
-  } else if (measureSpecification && !isHEDIS && !doesOtherNDRExist) {
-    error = {
-      errorLocation: "Other Performance Measure",
-      errorMessage:
-        "At least one Other Performance Measure Numerator, Denominator, and Rate must be completed",
-    };
-  }
-
-  return error;
-};
-
 const validateAtLeastOneDeviationNDR = (data: FormData) => {
   const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
   const deviationArray = getDeviationNDRArray(
@@ -242,8 +119,6 @@ const validateOMS = (data: FormData) => {
 
 export const validationFunctions = [
   FUMADValidation,
-  validateAtLeastOneNDRSet,
-  validateDualPopulationInformation,
   validateAtLeastOneDeviationNDR,
   validateRequiredRadioButtonForCombinedRates,
   validateBothDatesCompletedInRange,
