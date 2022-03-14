462ae89fa7cb38a4826e014433089b26b100f7a4
James Dinh <jdinh@collabralink.com>
2022-03-14 14:03:27 -0700

    pull new validations into cisch

--- services/ui-src/src/measures/2021/CISCH/validation.ts
+++ services/ui-src/src/measures/2021/CISCH/validation.ts
@@ -1,4 +1,5 @@
 import * as PMD from "./data";
+import * as DC from "dataConstants";
 import {
   atLeastOneRateComplete,
   ensureBothDatesCompletedInRange,
@@ -7,9 +8,19 @@ import {
   validateNoNonZeroNumOrDenom,
   validateReasonForNotReporting,
   validateRequiredRadioButtonForCombinedRates,
-} from "../../globalValidations/validationsLib";
-import { getPerfMeasureRateArray } from "../../globalValidations";
+  validateAtLeastOneNDRInDeviationOfMeasureSpec,
+  getPerfMeasureRateArray,
+  getDeviationNDRArray,
+  omsLocationDictionary,
+} from "../../globalValidations";
 import { FormData } from "./types";
+import {
+  omsValidations,
+  validateDenominatorGreaterThanNumerator,
+  validateRateNotZero,
+  validateRateZero,
+} from "measures/globalValidations/omsValidationsLib";
+import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
 
 const CISCHValidation = (data: FormData) => {
   const ageGroups = PMD.qualifiers;
@@ -17,6 +28,12 @@ const CISCHValidation = (data: FormData) => {
   const OPM = data["OtherPerformanceMeasure-Rates"];
   const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data) ?? [];
   const dateRange = data["DateRange"];
+  const deviationArray = getDeviationNDRArray(
+    data.DeviationOptions,
+    data.Deviations,
+    true
+  );
+  const didCalculationsDeviate = data["DidCalculationsDeviate"] === DC.YES;
 
   let errorArray: any[] = [];
   if (data["DidReport"] === "no") {
@@ -26,6 +43,27 @@ const CISCHValidation = (data: FormData) => {
 
   errorArray = [
     ...errorArray,
+    ...omsValidations({
+      data,
+      qualifiers: PMD.qualifiers,
+      categories: PMD.categories,
+      locationDictionary: omsLocationDictionary(
+        OMSData(true),
+        PMD.qualifiers,
+        PMD.categories
+      ),
+      validationCallbacks: [
+        validateDenominatorGreaterThanNumerator,
+        validateRateZero,
+        validateRateNotZero,
+      ],
+    }),
+    ...validateAtLeastOneNDRInDeviationOfMeasureSpec(
+      performanceMeasureArray,
+      ageGroups,
+      deviationArray,
+      didCalculationsDeviate
+    ),
     ...atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
     ...validateNumeratorsLessThanDenominators(
       performanceMeasureArray,
