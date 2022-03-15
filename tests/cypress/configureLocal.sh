ea188274b27c20dd668fc6a4d4c57749cc8f9588
Samuel Sierra <ssierra@collabralink.com>
2022-03-15 18:13:51 -0400

    add config file to test suite

---
+++ tests/cypress/configureLocal.sh
@@ -0,0 +1,25 @@
+
+set -e
+
+echo """
+Please Note:
+  This script exists to enable a developer to run a react frontend locally while pointing to an AWS backend.
+  This is *not* the same as deploying the entire stack locally.
+  This script may be removed soon, as the much more powerful local workflow should make this osoblete.
+  However, it will be left for now.
+  Please make sure you understand what this script does and why you're using it.
+"""
+
+stage=${1:-dev}
+
+echo "Fetching information for stage $stage..."
+
+test_user_1=`sh ../../services/output.sh ../../services/ui-src TestUserOne $stage`
+test_password_1=`sh ../../services/output.sh ../../services/ui-src TestPasswordOne $stage`
+
+echo $test_user_1
+echo $test_password_1
+
+export TEST_USER_1=$test_user_1
+export TEST_PASSWORD_1=$test_password_1
+./env.sh
