182e0e872b3ae56c6a4bffeacfd2312becd0a957
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-03-15 12:31:23 -0500

    add ammad validations

--- services/ui-src/src/measures/2021/AMMAD/validation.ts
+++ services/ui-src/src/measures/2021/AMMAD/validation.ts
@@ -1,16 +1,29 @@
 import * as PMD from "./data";
+import * as DC from "dataConstants";
 import {
   atLeastOneRateComplete,
   ensureBothDatesCompletedInRange,
-  validateDualPopInformation,
   validateNumeratorsLessThanDenominators,
   validateEqualDenominators,
   validateNoNonZeroNumOrDenom,
   validateReasonForNotReporting,
   validateRequiredRadioButtonForCombinedRates,
-} from "../../globalValidations/validationsLib";
+  validateAtLeastOneNDRInDeviationOfMeasureSpec,
+  getDeviationNDRArray,
+  omsLocationDictionary,
+  validateDualPopInformation,
+} from "../../globalValidations";
 import { getPerfMeasureRateArray } from "../../globalValidations";
 import { FormData } from "./types";
+import {
+  OmsValidationCallback,
+  omsValidations,
+  validateDenominatorGreaterThanNumerator,
+  validateOneRateLessThanOther,
+  validateRateNotZero,
+  validateRateZero,
+} from "measures/globalValidations/omsValidationsLib";
+import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
 
 const validateContinuationGreaterThanAccute = (data: any) => {
   if (
@@ -48,6 +61,46 @@ const validateContinuationGreaterThanAccute = (data: any) => {
   return error ? [errorArray[0]] : [];
 };
 
+const cleanString = (s: string) => s.replace(/[^\w]/g, "");
+const sameDenominatorSets: OmsValidationCallback = ({
+  rateData,
+  locationDictionary,
+  categories,
+  qualifiers,
+  isOPM,
+  label,
+}) => {
+  if (isOPM) return [];
+  const errorArray: FormError[] = [];
+
+  for (const qual of qualifiers.map((s) => cleanString(s))) {
+    for (let initiation = 0; initiation < categories.length; initiation += 2) {
+      const engagement = initiation + 1;
+      const initRate =
+        rateData.rates?.[qual]?.[cleanString(categories[initiation])]?.[0];
+      const engageRate =
+        rateData.rates?.[qual]?.[cleanString(categories[engagement])]?.[0];
+
+      if (
+        initRate &&
+        engageRate &&
+        initRate.denominator !== engageRate.denominator
+      ) {
+        errorArray.push({
+          errorLocation: `Optional Measure Stratification: ${locationDictionary(
+            [...label, qual]
+          )}`,
+          errorMessage: `Denominators must be the same for ${locationDictionary(
+            [categories[initiation]]
+          )} and ${locationDictionary([categories[engagement]])}.`,
+        });
+      }
+    }
+  }
+
+  return errorArray;
+};
+
 const AMMADValidation = (data: FormData) => {
   const ageGroups = PMD.qualifiers;
   const age65PlusIndex = 1;
@@ -83,6 +136,13 @@ const AMMADValidation = (data: FormData) => {
     }
   });
 
+  const deviationArray = getDeviationNDRArray(
+    data.DeviationOptions,
+    data.Deviations,
+    true
+  );
+  const didCalculationsDeviate = data["DidCalculationsDeviate"] === DC.YES;
+
   errorArray = [
     ...errorArray,
     ...atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
@@ -102,6 +162,29 @@ const AMMADValidation = (data: FormData) => {
     ...validateRequiredRadioButtonForCombinedRates(data),
     ...ensureBothDatesCompletedInRange(dateRange),
     ...validateContinuationGreaterThanAccute(data),
+    ...validateAtLeastOneNDRInDeviationOfMeasureSpec(
+      performanceMeasureArray,
+      ageGroups,
+      deviationArray,
+      didCalculationsDeviate
+    ),
+    ...omsValidations({
+      data,
+      qualifiers: PMD.qualifiers,
+      categories: PMD.categories,
+      locationDictionary: omsLocationDictionary(
+        OMSData(true),
+        PMD.qualifiers,
+        PMD.categories
+      ),
+      validationCallbacks: [
+        validateOneRateLessThanOther,
+        validateDenominatorGreaterThanNumerator,
+        validateRateZero,
+        validateRateNotZero,
+        sameDenominatorSets,
+      ],
+    }),
   ];
 
   return errorArray;
