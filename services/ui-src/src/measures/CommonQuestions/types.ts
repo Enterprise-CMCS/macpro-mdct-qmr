852f31a54c4b15d8a5b233b89cbf6968420f0b03
James Dinh <jdinh@collabralink.com>
2022-03-18 11:58:54 -0700

    clean up data file

--- services/ui-src/src/measures/CommonQuestions/types.ts
+++ services/ui-src/src/measures/CommonQuestions/types.ts
@@ -43,10 +43,8 @@ export interface DefinitionOfPopulation {
   [DC.DEFINITION_OF_DENOMINATOR_SUBSET_EXPLAIN]: string; //Section rendered in child data source measures
   [DC.DELIVERY_SYS_FFS]: YesNo; // If FFS selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> Is all of your FFS population included in this measure?"
   [DC.DELIVERY_SYS_FFS_NO_PERCENT]: string; // If NO in DELIVERY_SYS_FFS -> what percent included in measure
-  [DC.DELIVERY_SYS_FFS_NO_POP]: string; // If NO in DELIVERY_SYS_FFS -> what number of your FFS population are included in the measure?
   [DC.DELIVERY_SYS_PCCM]: YesNo; // If PCCM selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> Is all of your PCCM population included in this measure?"
   [DC.DELIVERY_SYS_PCCM_NO_PERCENT]: string; // If NO in DELIVERY_SYS_PCCM -> what percent included in measure
-  [DC.DELIVERY_SYS_PCCM_NO_POP]: string; // if NO in DELIVERY_SYS_PCCM -> what number of your PCCM population are included in the measure?
   [DC.DELIVERY_SYS_MCO_PIHP]: YesNo; // If MCO-PIHP selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR and Is all of your MCO-PIHP population included in this measure?
   [DC.DELIVERY_SYS_MCO_PIHP_NUM_PLANS]: string; // If MCO-PIHP selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> what number
   [DC.DELIVERY_SYS_MCO_PIHP_NO_INC]: string; // If NO in DELIVERY_SYS_MCO_PIHP -> percentage included
@@ -57,7 +55,6 @@ export interface DefinitionOfPopulation {
   [DC.DELIVERY_SYS_OTHER]: string; // If OTHER selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> describe the denominator
   [DC.DELIVERY_SYS_OTHER_PERCENT]: string; // If OTHER selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> percentage represented
   [DC.DELIVERY_SYS_OTHER_NUM_HEALTH_PLANS]: string; // If OTHER selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> number of health plans represented
-  [DC.DELIVERY_SYS_OTHER_POP]: string; // If OTHER selected in DELIVERY_SYS_REPRESENTATION_DENOMINATOR -> number of population represented
 }
 
 export interface AdditionalNotes {
