5bbd0949302b15276c98459367eb6f0394c628cf
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-03-18 11:13:28 -0500

    add stuff

--- services/ui-src/src/measures/globalValidations/dataDrivenTools.ts
+++ services/ui-src/src/measures/globalValidations/dataDrivenTools.ts
@@ -92,19 +92,21 @@ export const getDeviationNDRArray = (
   let deviationArray: any[] = [];
   deviationOptions?.forEach((option) => {
     const objectToSearch = ageGroups ? data[option] : data;
-    if (ageGroups) {
-      if ("RateDeviationsSelected" in objectToSearch) {
-        deviationArray.push(objectToSearch);
-      } else {
-        for (const key of Object.keys(objectToSearch).filter(
-          (prop) => prop !== DC.SELECTED_OPTIONS
-        )) {
-          deviationArray.push(data[option][key as Types.DeviationKeys]);
+    if (objectToSearch) {
+      if (ageGroups) {
+        if (objectToSearch.RateDeviationsSelected) {
+          deviationArray.push(objectToSearch);
+        } else {
+          for (const key of Object.keys(objectToSearch).filter(
+            (prop) => prop !== DC.SELECTED_OPTIONS
+          )) {
+            deviationArray.push(data[option][key as Types.DeviationKeys]);
+          }
         }
-      }
-    } else {
-      if (data) {
-        deviationArray = Object.values(data);
+      } else {
+        if (data) {
+          deviationArray = Object.values(data);
+        }
       }
     }
   });
