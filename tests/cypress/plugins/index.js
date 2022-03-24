7f28ca391581b2112150f84a5c25da0216553e66
Benjamin Paige <benjamin.paige@gmail.com>
2022-03-24 14:12:26 -0600

    Trying something

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -8,20 +8,8 @@ module.exports = (on, config) => {
   // set default size for headless mode
   on("before:browser:launch", (browser = {}, launchOptions) => {
     if (browser.name === "chrome" && browser.isHeadless) {
-      launchOptions.args.push("--window-size=1400,1200");
+      launchOptions.args.push("--window-size=1400,9000");
       launchOptions.args.push("--force-device-scale-factor=1");
-    }
-    if (browser.name === "electron" && browser.isHeadless) {
-      // fullPage screenshot size is 1400x1200
-      launchOptions.preferences.width = 1400;
-      launchOptions.preferences.height = 1200;
-    }
-
-    if (browser.name === "firefox" && browser.isHeadless) {
-      // menubars take up height on the screen
-      // so fullPage screenshot size is 1400x1126
-      launchOptions.args.push("--width=1400");
-      launchOptions.args.push("--height=1200");
     }
     return launchOptions;
   });
