818e196621b9069b2815d93283ca8fd0e6f03cfd
Daniel Belcher <daniel.belcher.cs@gmail.com>
2022-03-24 17:55:57 -0500

    feat: add WCC and it's validation

---
+++ services/ui-src/src/measures/2021/WCCCH/validation.ts
@@ -0,0 +1,99 @@
+import * as PMD from "./data";
+import * as DC from "dataConstants";
+import { FormData } from "./types";
+import {
+  atLeastOneRateComplete,
+  validateNumeratorsLessThanDenominators,
+  validateEqualDenominators,
+  validateNoNonZeroNumOrDenom,
+  validateReasonForNotReporting,
+  validateAtLeastOneNDRInDeviationOfMeasureSpec,
+  validateOneRateHigherThanOther,
+  ensureBothDatesCompletedInRange,
+  validateRequiredRadioButtonForCombinedRates,
+  getDeviationNDRArray,
+  getPerfMeasureRateArray,
+  omsLocationDictionary,
+  omsValidations,
+  validateDenominatorGreaterThanNumerator,
+  validateDenominatorsAreTheSame,
+  validateOneRateLessThanOther,
+  validateRateNotZero,
+  validateRateZero,
+} from "measures/globalValidations";
+import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
+
+const WCCHValidation = (data: FormData) => {
+  const whyNotReporting = data["WhyAreYouNotReporting"];
+  const OPM = data["OtherPerformanceMeasure-Rates"];
+  const dateRange = data["DateRange"];
+  const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
+  const includesHybridDataSource = data["DataSource"]?.includes(
+    DC.HYBRID_ADMINSTRATIVE_AND_MEDICAL_RECORDS_DATA
+  );
+
+  const deviationArray = getDeviationNDRArray(
+    data.DeviationOptions,
+    data.Deviations,
+    true
+  );
+  const didCalculationsDeviate = data["DidCalculationsDeviate"] === DC.YES;
+
+  let errorArray: any[] = [];
+  if (data["DidReport"] === "no") {
+    errorArray = [...validateReasonForNotReporting(whyNotReporting)];
+    return errorArray;
+  }
+
+  let sameDenominatorError = [
+    ...validateEqualDenominators(performanceMeasureArray, PMD.qualifiers),
+  ];
+  sameDenominatorError =
+    sameDenominatorError.length > 0 ? [...sameDenominatorError] : [];
+  errorArray = [
+    ...errorArray,
+    ...atLeastOneRateComplete(performanceMeasureArray, OPM, PMD.qualifiers),
+    ...validateNumeratorsLessThanDenominators(
+      performanceMeasureArray,
+      OPM,
+      PMD.qualifiers
+    ),
+    ...sameDenominatorError,
+    ...validateNoNonZeroNumOrDenom(
+      performanceMeasureArray,
+      OPM,
+      PMD.qualifiers,
+      includesHybridDataSource
+    ),
+    ...validateOneRateHigherThanOther(data, PMD.data),
+    ...ensureBothDatesCompletedInRange(dateRange),
+    ...validateAtLeastOneNDRInDeviationOfMeasureSpec(
+      performanceMeasureArray,
+      PMD.qualifiers,
+      deviationArray,
+      didCalculationsDeviate
+    ),
+    ...omsValidations({
+      data,
+      qualifiers: PMD.qualifiers,
+      categories: PMD.categories,
+      locationDictionary: omsLocationDictionary(
+        OMSData(true),
+        PMD.qualifiers,
+        PMD.categories
+      ),
+      validationCallbacks: [
+        validateDenominatorGreaterThanNumerator,
+        validateDenominatorsAreTheSame,
+        validateOneRateLessThanOther,
+        validateRateNotZero,
+        ...(includesHybridDataSource ? [] : [validateRateZero]),
+      ],
+      ...validateRequiredRadioButtonForCombinedRates(data),
+    }),
+  ];
+
+  return errorArray;
+};
+
+export const validationFunctions = [WCCHValidation];
--- services/ui-src/src/measures/2021/index.tsx
+++ services/ui-src/src/measures/2021/index.tsx
@@ -32,6 +32,7 @@ import { PQI01AD } from "./PQI01AD";
 import { PQI05AD } from "./PQI05AD";
 import { PQI08AD } from "./PQI08AD";
 import { PQI15AD } from "./PQI15AD";
+import { WCCCH } from "./WCCCH";
 
 const twentyTwentyOneMeasures = {
   "AMM-AD": AMMAD,
@@ -64,6 +65,7 @@ const twentyTwentyOneMeasures = {
   "PQI05-AD": PQI05AD,
   "PQI08-AD": PQI08AD,
   "PQI15-AD": PQI15AD,
+  "WCC-CH": WCCCH,
 };
 
 export default twentyTwentyOneMeasures;
