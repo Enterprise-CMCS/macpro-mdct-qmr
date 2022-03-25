c3d18074f6ef1ef8aa3baf25f35a23fd0dc43246
13bfrancis <13bfrancis@gmail.com>
2022-03-25 11:30:43 -0400

    refactor to use GV as default named import

--- services/ui-src/src/measures/2021/WCCCH/validation.ts
+++ services/ui-src/src/measures/2021/WCCCH/validation.ts
@@ -1,40 +1,19 @@
 import * as PMD from "./data";
 import * as DC from "dataConstants";
 import { FormData } from "./types";
-import {
-  atLeastOneRateComplete,
-  validateNumeratorsLessThanDenominators,
-  validateEqualDenominators,
-  validateNoNonZeroNumOrDenom,
-  validateReasonForNotReporting,
-  validateAtLeastOneNDRInDeviationOfMeasureSpec,
-  validateOneRateHigherThanOther,
-  ensureBothDatesCompletedInRange,
-  validateRequiredRadioButtonForCombinedRates,
-  getDeviationNDRArray,
-  getPerfMeasureRateArray,
-  omsLocationDictionary,
-  omsValidations,
-  validateDenominatorGreaterThanNumerator,
-  validateDenominatorsAreTheSame,
-  validateOneRateLessThanOther,
-  validateRateNotZero,
-  validateRateZero,
-  validateTotalNDR,
-  validateOMSTotalNDR,
-} from "measures/globalValidations";
+import * as GV from "measures/globalValidations";
 import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
 
 const WCCHValidation = (data: FormData) => {
   const whyNotReporting = data["WhyAreYouNotReporting"];
   const OPM = data["OtherPerformanceMeasure-Rates"];
   const dateRange = data["DateRange"];
-  const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
+  const performanceMeasureArray = GV.getPerfMeasureRateArray(data, PMD.data);
   const includesHybridDataSource = data["DataSource"]?.includes(
     DC.HYBRID_ADMINSTRATIVE_AND_MEDICAL_RECORDS_DATA
   );
 
-  const deviationArray = getDeviationNDRArray(
+  const deviationArray = GV.getDeviationNDRArray(
     data.DeviationOptions,
     data.Deviations,
     true
@@ -43,58 +22,58 @@ const WCCHValidation = (data: FormData) => {
 
   let errorArray: any[] = [];
   if (data["DidReport"] === "no") {
-    errorArray = [...validateReasonForNotReporting(whyNotReporting)];
+    errorArray = [...GV.validateReasonForNotReporting(whyNotReporting)];
     return errorArray;
   }
 
   let sameDenominatorError = [
-    ...validateEqualDenominators(performanceMeasureArray, PMD.qualifiers),
+    ...GV.validateEqualDenominators(performanceMeasureArray, PMD.qualifiers),
   ];
   sameDenominatorError =
     sameDenominatorError.length > 0 ? [...sameDenominatorError] : [];
   errorArray = [
     ...errorArray,
-    ...atLeastOneRateComplete(performanceMeasureArray, OPM, PMD.qualifiers),
-    ...validateNumeratorsLessThanDenominators(
+    ...GV.atLeastOneRateComplete(performanceMeasureArray, OPM, PMD.qualifiers),
+    ...GV.validateNumeratorsLessThanDenominators(
       performanceMeasureArray,
       OPM,
       PMD.qualifiers
     ),
     ...sameDenominatorError,
-    ...validateNoNonZeroNumOrDenom(
+    ...GV.validateNoNonZeroNumOrDenom(
       performanceMeasureArray,
       OPM,
       PMD.qualifiers,
       includesHybridDataSource
     ),
-    ...validateOneRateHigherThanOther(data, PMD.data),
-    ...ensureBothDatesCompletedInRange(dateRange),
-    ...validateAtLeastOneNDRInDeviationOfMeasureSpec(
+    ...GV.validateOneRateHigherThanOther(data, PMD.data),
+    ...GV.ensureBothDatesCompletedInRange(dateRange),
+    ...GV.validateAtLeastOneNDRInDeviationOfMeasureSpec(
       performanceMeasureArray,
       PMD.qualifiers,
       deviationArray,
       didCalculationsDeviate
     ),
-    ...omsValidations({
+    ...GV.omsValidations({
       data,
       qualifiers: PMD.qualifiers,
       categories: PMD.categories,
-      locationDictionary: omsLocationDictionary(
+      locationDictionary: GV.omsLocationDictionary(
         OMSData(true),
         PMD.qualifiers,
         PMD.categories
       ),
       validationCallbacks: [
-        validateDenominatorGreaterThanNumerator,
-        validateDenominatorsAreTheSame,
-        validateOneRateLessThanOther,
-        validateRateNotZero,
-        validateOMSTotalNDR,
-        ...(includesHybridDataSource ? [] : [validateRateZero]),
+        GV.validateDenominatorGreaterThanNumerator,
+        GV.validateDenominatorsAreTheSame,
+        GV.validateOneRateLessThanOther,
+        GV.validateRateNotZero,
+        GV.validateOMSTotalNDR,
+        ...(includesHybridDataSource ? [] : [GV.validateRateZero]),
       ],
     }),
-    ...validateRequiredRadioButtonForCombinedRates(data),
-    ...validateTotalNDR(performanceMeasureArray, undefined, PMD.categories),
+    ...GV.validateRequiredRadioButtonForCombinedRates(data),
+    ...GV.validateTotalNDR(performanceMeasureArray, undefined, PMD.categories),
   ];
 
   return errorArray;
