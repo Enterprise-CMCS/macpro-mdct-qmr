8576a7c183cfc30e48edde3b6eebba1132391893
James Dinh <jdinh@collabralink.com>
2022-03-25 07:52:11 -0700

    ramp up wait time

--- tests/cypress/support/commands/commands.js
+++ tests/cypress/support/commands/commands.js
@@ -57,9 +57,8 @@ Cypress.Commands.add("goToMeasure", (measure) => {
   cy.get(`[data-cy="Clear Data"]`).click();
   cy.wait(2000);
   cy.reload();
-  cy.wait(5000);
+  cy.wait(15000);
   cy.get(`[data-cy="${measure}"]`).click();
-  cy.wait(5000);
 });
 
 // Correct sections visible when user is reporting data on measure
