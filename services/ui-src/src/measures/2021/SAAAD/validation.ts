fba6359b4ad8aab6b092290fcb776253821d39df
JP Hechter <jhechter@fearless.tech>
2022-03-28 12:26:53 -0400

    updated validation.ts

--- services/ui-src/src/measures/2021/SAAAD/validation.ts
+++ services/ui-src/src/measures/2021/SAAAD/validation.ts
@@ -5,51 +5,8 @@ import * as GV from "measures/globalValidations";
 
 import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
 
-const cleanString = (s: string) => s.replace(/[^\w]/g, "");
-
-/** For each qualifier the denominators neeed to be the same for both Initiaion and Engagement of the same category. */
-const sameDenominatorSets: GV.OmsValidationCallback = ({
-  rateData,
-  locationDictionary,
-  categories,
-  qualifiers,
-  isOPM,
-  label,
-}) => {
-  if (isOPM) return [];
-  const errorArray: FormError[] = [];
-
-  for (const qual of qualifiers.map((s) => cleanString(s))) {
-    for (let initiation = 0; initiation < categories.length; initiation += 2) {
-      const engagement = initiation + 1;
-      const initRate =
-        rateData.rates?.[qual]?.[cleanString(categories[initiation])]?.[0];
-      const engageRate =
-        rateData.rates?.[qual]?.[cleanString(categories[engagement])]?.[0];
-
-      if (
-        initRate &&
-        engageRate &&
-        initRate.denominator !== engageRate.denominator
-      ) {
-        errorArray.push({
-          errorLocation: `Optional Measure Stratification: ${locationDictionary(
-            [...label, qual]
-          )}`,
-          errorMessage: `Denominators must be the same for ${locationDictionary(
-            [categories[initiation]]
-          )} and ${locationDictionary([categories[engagement]])}.`,
-        });
-      }
-    }
-  }
-
-  return errorArray;
-};
-
 const SAAADValidation = (data: FormData) => {
   const ageGroups = PMD.qualifiers;
-  const age65PlusIndex = 1;
   const whyNotReporting = data[DC.WHY_ARE_YOU_NOT_REPORTING];
   const OPM = data[DC.OPM_RATES];
   const performanceMeasureArray = GV.getPerfMeasureRateArray(data, PMD.data);
@@ -61,52 +18,21 @@ const SAAADValidation = (data: FormData) => {
   );
   const didCalculationsDeviate = data[DC.DID_CALCS_DEVIATE] === DC.YES;
 
-  const DefinitionOfDenominator = data[DC.DEFINITION_OF_DENOMINATOR];
-
   let errorArray: any[] = [];
   if (data[DC.DID_REPORT] === DC.NO) {
     errorArray = [...GV.validateReasonForNotReporting(whyNotReporting)];
     return errorArray;
   }
 
-  let unfilteredSameDenominatorErrors: any[] = [];
-  for (let i = 0; i < performanceMeasureArray.length; i += 2) {
-    unfilteredSameDenominatorErrors = [
-      ...unfilteredSameDenominatorErrors,
-      ...GV.validateEqualDenominators(
-        [performanceMeasureArray[i], performanceMeasureArray[i + 1]],
-        ageGroups,
-        `Denominators must be the same for ${PMD.categories[i]} and ${
-          PMD.categories[i + 1]
-        }.`
-      ),
-    ];
-  }
-
-  let filteredSameDenominatorErrors: any = [];
-  let errorList: string[] = [];
-  unfilteredSameDenominatorErrors.forEach((error) => {
-    if (!(errorList.indexOf(error.errorMessage) > -1)) {
-      errorList.push(error.errorMessage);
-      filteredSameDenominatorErrors.push(error);
-    }
-  });
-
   errorArray = [
     ...errorArray,
     ...GV.atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
-    ...GV.validateDualPopInformation(
-      performanceMeasureArray,
-      OPM,
-      age65PlusIndex,
-      DefinitionOfDenominator
-    ),
+
     ...GV.validateNumeratorsLessThanDenominators(
       performanceMeasureArray,
       OPM,
       ageGroups
     ),
-    ...filteredSameDenominatorErrors,
     ...GV.validateNoNonZeroNumOrDenom(performanceMeasureArray, OPM, ageGroups),
     ...GV.omsValidations({
       data,
@@ -121,7 +47,6 @@ const SAAADValidation = (data: FormData) => {
         GV.validateDenominatorGreaterThanNumerator,
         GV.validateRateZero,
         GV.validateRateNotZero,
-        sameDenominatorSets,
       ],
     }),
     ...GV.validateRequiredRadioButtonForCombinedRates(data),
