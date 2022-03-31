3b7c051d89fb66fc489a3cd897ace348ab957b4f
James Dinh <jdinh@collabralink.com>
2022-03-31 08:26:39 -0700

    added ccwch measure

---
+++ services/ui-src/src/measures/2021/CCWCH/data.ts
@@ -0,0 +1,18 @@
+import { DataDrivenTypes } from "measures/CommonQuestions/types";
+export const qualifiers = [
+  "Most effective or moderately effective method of contraception",
+  "Long-acting reversible method of contraception (LARC)",
+];
+export const categories = [];
+
+export const data: DataDrivenTypes.PerformanceMeasure = {
+  questionText: [
+    "Among women ages 15 to 20 at risk of unintended pregnancy, the percentage that:",
+  ],
+  questionListItems: [
+    "Were provided a most effective or moderately effective method of contraception",
+    "Were provided a long-acting reversible method of contraception (LARC)",
+  ],
+  categories,
+  qualifiers,
+};
