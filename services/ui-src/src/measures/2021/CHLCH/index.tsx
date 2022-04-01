16ae528a474159fd947bfebe50b5bb5682934797
Samuel Sierra <ssierra@collabralink.com>
2022-04-01 10:03:27 -0400

    adult measure false

--- services/ui-src/src/measures/2021/CHLCH/index.tsx
+++ services/ui-src/src/measures/2021/CHLCH/index.tsx
@@ -56,7 +56,7 @@ export const CHLCH = ({
               performanceMeasureArray={performanceMeasureArray}
               qualifiers={PMD.qualifiers}
               categories={PMD.categories}
-              adultMeasure
+              adultMeasure={false}
               isSingleSex
             />
           )}
