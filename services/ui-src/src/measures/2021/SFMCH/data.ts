76b2df9c2f969e959be4f0d91ea31c4bec9d568e
Samuel Sierra <ssierra@collabralink.com>
2022-04-04 14:18:40 -0400

    prettier

--- services/ui-src/src/measures/2021/SFMCH/data.ts
+++ services/ui-src/src/measures/2021/SFMCH/data.ts
@@ -10,7 +10,7 @@ export const data: DataDrivenTypes.PerformanceMeasure = {
   questionText: [
     "Percentage of enrolled children who have ever received sealants on permanent first molar teeth: (1) at least one sealant and (2) all four molars sealed by the 10th birthdate.",
   ],
-  questionListItems:[],
+  questionListItems: [],
   categories,
   qualifiers,
 };
