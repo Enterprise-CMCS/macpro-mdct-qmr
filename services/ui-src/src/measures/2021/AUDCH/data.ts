f8583022c72f335dd1e5ba6501a55c7007248df0
Gavin St. Ours <gst-ours@fearless.tech>
2022-04-04 10:47:32 -0400

    Add AUD-CH types and data

---
+++ services/ui-src/src/measures/2021/AUDCH/data.ts
@@ -0,0 +1,13 @@
+import { DataDrivenTypes } from "measures/CommonQuestions/types";
+
+export const qualifiers = ["No Later than 3 Months of Age"];
+export const categories = [];
+
+export const data: DataDrivenTypes.PerformanceMeasure = {
+  questionText: [
+    "Percentage of newborns who did not pass hearing screening and have an audiological diagnosis no later than 3 months of age (90 days).",
+  ],
+  questionListItems: [],
+  categories,
+  qualifiers,
+};
