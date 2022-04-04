877f617fbce1780dca443ff42a26ef30e02ea0d2
Gavin St. Ours <gst-ours@fearless.tech>
2022-04-04 10:46:48 -0400

    Add CDC measurement specification type

--- services/ui-src/src/measures/CommonQuestions/MeasurementSpecification/index.tsx
+++ services/ui-src/src/measures/CommonQuestions/MeasurementSpecification/index.tsx
@@ -41,6 +41,7 @@ interface Props {
   type:
     | "AHRQ-NCQA"
     | "AHRQ"
+    | "CDC"
     | "CMS"
     | "HEDIS"
     | "HRSA"
@@ -59,6 +60,10 @@ const specifications = {
     displayValue:
       "Agency for Healthcare Research and Quality (AHRQ) (survey instrument) and National Committee for Quality Assurance (survey administrative protocol)",
     value: DC.AHRQ_NCQA,
+  },
+  CDC: {
+    displayValue: "Centers for Disease Contol and Prevention (CDC)",
+    value: DC.CDC,
   },
   CMS: {
     displayValue: "Centers for Medicare & Medicaid Services (CMS)",
