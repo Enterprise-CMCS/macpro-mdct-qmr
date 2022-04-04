e421f586c86bbe06e746b5656f39a583701ba6e2
Gavin St. Ours <gst-ours@fearless.tech>
2022-04-04 10:47:20 -0400

    Add AUD-CH measure to 2021 measures

--- services/ui-src/src/measures/2021/index.tsx
+++ services/ui-src/src/measures/2021/index.tsx
@@ -5,6 +5,7 @@ the key should be the measure id as a string (with '-XX' included)
 import { AMMAD } from "./AMMAD";
 import { AMRAD } from "./AMRAD";
 import { AMRCH } from "./AMRCH";
+import { AUDCH } from "./AUDCH";
 import { BCSAD } from "./BCSAD";
 import { CBPAD } from "./CBPAD";
 import { CCPAD } from "./CCPAD";
@@ -46,6 +47,7 @@ const twentyTwentyOneMeasures = {
   "AMM-AD": AMMAD,
   "AMR-AD": AMRAD,
   "AMR-CH": AMRCH,
+  "AUD-CH": AUDCH,
   "BCS-AD": BCSAD,
   "CBP-AD": CBPAD,
   "CCP-AD": CCPAD,
