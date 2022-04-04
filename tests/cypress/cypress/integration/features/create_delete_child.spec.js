bdd0a23bb56e9f87c25884513f0def140e4bbe7d
Benjamin Paige <benjamin.paige@gmail.com>
2022-04-04 13:02:19 -0600

    remove test config check

--- tests/cypress/cypress/integration/features/create_delete_child.spec.js
+++ tests/cypress/cypress/integration/features/create_delete_child.spec.js
@@ -5,26 +5,19 @@ describe("Child Core Sets Should be able to be deleted and created", () => {
 
   it.skip("Creates separate child core-set", () => {
     cy.deleteChildCoreSets();
-    cy.waitUntil(() =>
-      cy.get('[data-cy="Add Child Core Set"]').then(() => {
-        cy.get('[data-cy="Add Child Core Set"]').click({ force: true }); // clicking on adding child core set measures
-        cy.get("#ChildCoreSet-ReportType-separate").click({ force: true }); //selecting combined core set
-        cy.get('[data-cy="Create"]').click({ force: true }); //clicking create
-        cy.wait(5000);
-      })
-    );
+    cy.wait(5000);
+    cy.get('[data-cy="Add Child Core Set"]').click({ force: true }); // clicking on adding child core set measures
+    cy.get("#ChildCoreSet-ReportType-separate").click({ force: true }); //selecting combined core set
+    cy.get('[data-cy="Create"]').click({ force: true }); //clicking create
+    cy.wait(5000);
   });
 
   it("Creates combined child core-set", () => {
     cy.deleteChildCoreSets();
     cy.wait(5000);
-    // cy.waitUntil(() =>
-    // cy.get('[data-cy="Add Child Core Set"]').then(() => {
     cy.get('[data-cy="Add Child Core Set"]').click({ force: true }); // clicking on adding child core set measures
     cy.get("#ChildCoreSet-ReportType-combined").click({ force: true }); //selecting combined core set
     cy.get('[data-cy="Create"]').click({ force: true }); //clicking create
     cy.wait(5000);
-    // })
-    // );
   });
 });
