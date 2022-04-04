db4a70f25fa121502766982268d3552285addf30
Gavin St. Ours <gst-ours@fearless.tech>
2022-04-04 15:59:29 -0400

    Make qualifier more descriptive and consistent with other similar measures

--- services/ui-src/src/measures/2021/AUDCH/data.ts
+++ services/ui-src/src/measures/2021/AUDCH/data.ts
@@ -1,6 +1,8 @@
 import { DataDrivenTypes } from "measures/CommonQuestions/types";
 
-export const qualifiers = ["No Later than 3 Months of Age"];
+export const qualifiers = [
+  "Percentage of newborns who did not pass hearing screening and have an audiological diagnosis no later than 3 months of age",
+];
 export const categories = [];
 
 export const data: DataDrivenTypes.PerformanceMeasure = {
