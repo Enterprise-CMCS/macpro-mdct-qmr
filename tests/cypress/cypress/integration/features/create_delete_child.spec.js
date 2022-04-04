951f9de4a6abc71be86891e52ebe5581e27b6bb8
Benjamin Paige <benjamin.paige@gmail.com>
2022-04-04 11:14:31 -0600

    update deploy.yml

--- tests/cypress/cypress/integration/features/create_delete_child.spec.js
+++ tests/cypress/cypress/integration/features/create_delete_child.spec.js
@@ -21,6 +21,7 @@ describe("Child Core Sets Should be able to be deleted and created", () => {
         cy.get('[data-cy="Add Child Core Set"]').click({ force: true }); // clicking on adding child core set measures
         cy.get("#ChildCoreSet-ReportType-combined").click({ force: true }); //selecting combined core set
         cy.get('[data-cy="Create"]').click({ force: true }); //clicking create
+        cy.wait(5000);
       })
     );
   });
