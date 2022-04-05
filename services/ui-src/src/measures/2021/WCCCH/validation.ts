75419ad145ceab0c41fb8fdebdffe14e07cc8e2d
Daniel Belcher <daniel.belcher.cs@gmail.com>
2022-04-05 08:49:33 -0500

    add missing validation

--- services/ui-src/src/measures/2021/WCCCH/validation.ts
+++ services/ui-src/src/measures/2021/WCCCH/validation.ts
@@ -76,6 +76,7 @@ const WCCHValidation = (data: FormData) => {
     }),
     ...GV.validateRequiredRadioButtonForCombinedRates(data),
     ...GV.validateTotalNDR(performanceMeasureArray, undefined, PMD.categories),
+    ...GV.validateEqualDenominators(performanceMeasureArray, PMD.qualifiers),
   ];
 
   return errorArray;
