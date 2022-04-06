a0cfcebe007033024055e46d4d0fba171389c26b
Daniel Belcher <daniel.belcher.cs@gmail.com>
2022-04-06 14:39:04 -0500

    add extra tests from PCCAD

--- tests/cypress/cypress/integration/measures/PPCCH.spec.js
+++ tests/cypress/cypress/integration/measures/PPCCH.spec.js
@@ -107,4 +107,156 @@ describe("Measure: PPC-CH", () => {
       "aria-readonly"
     );
   });
+
+  it("Rate calculation should be correct", () => {
+    // select is reporting
+    cy.get('[data-cy="DidReport0"]').click({ force: true });
+
+    // select AHRQ for measurement specification
+    cy.get('[data-cy="MeasurementSpecification0"]').click();
+
+    cy.get('[id="DataSource0-checkbox"]').check({ force: true });
+    cy.get('[id="DataSource1-checkbox"]').uncheck({ force: true });
+
+    // Rate calculation should be = (N/D*100)
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("5");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("10");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "50.0"
+    );
+
+    // Ensure that auto calculate rate displays 1 decimal (even if the value is zero)
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("8");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "80.0"
+    );
+
+    // Ensure that numerical value after decimal is rounded up/down for auto calculated rate (up)
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("9");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "88.9"
+    );
+
+    // Ensure that numerical value after decimal is rounded up/down for auto calculated rate (down)
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("18");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "44.4"
+    );
+
+    // Ensure that user cannot manually enter rates if admin data is selected - (already selected)
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.attr",
+      "readonly"
+    );
+  });
+
+  it("User can manually adjust rate if other data source is selected", () => {
+    cy.get('[data-cy="DidReport0"]').click();
+    cy.get('[data-cy="DataSource1"]').click();
+    cy.get('[data-cy="MeasurementSpecification0"]').click();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("10");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("20");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "50.0"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "not.have.attr",
+      "readonly"
+    );
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
+    ).clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
+      "48.1"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "48.1"
+    );
+  });
+  // if Other measure spec is selected each age range/ custom description for which there are n/d/r
+  // values entered in other performance measure are presented in:
+  //  - Optional Measure specification
+  it("Age ranges are represented in OMS when other measure spec", () => {
+    cy.wait(2000);
+    // cy.get('[data-cy="DidReport0"]').click({ force: true });
+    cy.get('[data-cy="MeasurementSpecification1"]').click();
+    cy.get('[id="DataSource0-checkbox"]').check({ force: true });
+    cy.get('[id="DataSource1-checkbox"]').uncheck({ force: true });
+    cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.description"]').clear();
+    cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.description"]').type(
+      "example 1"
+    );
+    cy.get(
+      '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.numerator"]'
+    ).clear();
+    cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.numerator"]').type(
+      "5"
+    );
+    cy.get(
+      '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.denominator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.denominator"]'
+    ).type("10");
+    cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.rate"]').should(
+      "have.value",
+      "50.0"
+    );
+    cy.get(
+      '[data-cy="OptionalMeasureStratification.options0"] > .chakra-checkbox__control'
+    ).click();
+    cy.get(
+      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.options0"] > .chakra-checkbox__control'
+    ).click();
+    cy.get(
+      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.options0"] > .chakra-checkbox__label > .chakra-text'
+    ).should("have.text", "example 1");
+    cy.get(
+      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.options0"] > .chakra-checkbox__control'
+    ).click();
+    cy.get(
+      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.rates.example1.OPM.0.numerator"]'
+    ).type("3");
+  });
 });
