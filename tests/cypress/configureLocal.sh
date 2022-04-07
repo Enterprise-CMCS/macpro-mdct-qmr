4ae9b93f85332255fbb71e8c2fc1f168f3522a08
Benjamin Paige <benjamin.paige@gmail.com>
2022-04-06 19:02:52 -0600

    Add back in steps with secrets

--- tests/cypress/configureLocal.sh
+++ tests/cypress/configureLocal.sh
@@ -27,8 +27,8 @@ export TEST_PASSWORD_1=$test_password_1
 rm -rf ./cypress.env.json
 touch ./cypress.env.json
 echo "{" >> ./cypress.env.json
-echo "\"CYPRESS_USER_1\": \"$test_user_1\"," >> ./cypress.env.json
-echo "\"CYPRESS_USER_2\": \"$test_user_2\"," >> ./cypress.env.json
-echo "\"CYPRESS_USER_3\": \"$test_user_3\"," >> ./cypress.env.json
-echo "\"CYPRESS_PASSWORD_1\": \"$test_password_1\"" >> ./cypress.env.json
+echo "\"TEST_USER_1\": \"$test_user_1\"," >> ./cypress.env.json
+echo "\"TEST_USER_2\": \"$test_user_2\"," >> ./cypress.env.json
+echo "\"TEST_USER_3\": \"$test_user_3\"," >> ./cypress.env.json
+echo "\"TEST_PASSWORD_1\": \"$test_password_1\"" >> ./cypress.env.json
 echo "}" >> ./cypress.env.json
