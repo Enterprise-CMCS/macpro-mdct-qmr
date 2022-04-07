8cdf1703a707a1b25e6ecf21bc6bf208e2aafa45
Samuel Sierra <ssierra@collabralink.com>
2022-04-07 11:10:19 -0400

    fix test

--- tests/cypress/cypress/integration/measures/CHLCH.spec.js
+++ tests/cypress/cypress/integration/measures/CHLCH.spec.js
@@ -1,11 +1,6 @@
-const emailForCognito = "//input[@name='email']";
-const passwordForCognito = "//input[@name='password']";
-
 describe("Measure: oy2-9923 CHL-CH", () => {
   beforeEach(() => {
-    cy.visit("/");
     cy.login();
-    cy.deleteChildCoreSets();
     cy.goToChildCoreSetMeasures();
     cy.goToMeasure("CHL-CH");
   });
