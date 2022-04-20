1754db701a905d06be6e6d91b046a43ca2ac6e0b
Daniel Belcher <daniel.belcher.cs@gmail.com>
2022-04-20 09:09:49 -0500

    remove unneeded extra parameter

--- services/ui-src/src/hooks/api/useAddCoreSet.tsx
+++ services/ui-src/src/hooks/api/useAddCoreSet.tsx
@@ -7,23 +7,21 @@ interface AddCoreSet {
   state: string;
   year: string;
   coreSet: CoreSetAbbr;
-  id?: string;
 }
 
-const addCoreSet = async ({ state, year, coreSet, id }: AddCoreSet) => {
+const addCoreSet = async ({ state, year, coreSet }: AddCoreSet) => {
   return await createCoreSet({
     state,
     year,
     coreSet,
-    id,
   });
 };
 
 export const useAddCoreSet = () => {
   const { state, year } = useParams();
   if (state && year) {
-    return useMutation((coreSet: CoreSetAbbr, id?: string) =>
-      addCoreSet({ state, year, coreSet, id })
+    return useMutation((coreSet: CoreSetAbbr) =>
+      addCoreSet({ state, year, coreSet })
     );
   }
   throw Error("Missing required fields");
