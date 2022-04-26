509645c9a4532cc77a74f70a62d1ea4b4cdd13a4
Benjamin Paige <benjamin.paige@gmail.com>
2022-04-26 12:55:27 -0600

    Idle Timeout (#941)
    * Add SessionTimeout comp
    
    * Update queryClient
    
    * Add route change to api method
    
    * Update e2e tests
    
    * Add sessiontimeout to quallifiers
    
    * Update timeout and idle time
    
    Co-authored-by: abelzer212 <93547052+abelzer212@users.noreply.github.com>

--- services/ui-src/src/query.tsx
+++ services/ui-src/src/query.tsx
@@ -3,13 +3,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
 export const QueryProvider = ({ children }: any) => {
-  queryClient.setDefaultOptions({
-    queries: {
-      refetchInterval: 60000,
-      refetchIntervalInBackground: true,
-    },
-  });
-
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
