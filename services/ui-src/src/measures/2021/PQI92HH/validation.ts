f6db9e93b13a4447dc788b2027d34757b78b00ec
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-04-29 11:28:54 -0500

    add new health home

---
+++ services/ui-src/src/measures/2021/PQI92HH/validation.ts
@@ -0,0 +1,61 @@
+import * as DC from "dataConstants";
+import * as GV from "measures/globalValidations";
+import * as PMD from "./data";
+import { FormData } from "./types";
+import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
+
+const PQI92Validation = (data: FormData) => {
+  const OPM = data[DC.OPM_RATES];
+  const ageGroups = PMD.qualifiers;
+  const whyNotReporting = data[DC.WHY_ARE_YOU_NOT_REPORTING];
+  const didCalculationsDeviate = data[DC.DID_CALCS_DEVIATE] === DC.YES;
+  const deviationArray = GV.getDeviationNDRArray(
+    data.DeviationOptions,
+    data.Deviations
+  );
+  const dateRange = data[DC.DATE_RANGE];
+  const performanceMeasureArray = GV.getPerfMeasureRateArray(data, PMD.data);
+  let errorArray: any[] = [];
+
+  if (data[DC.DID_REPORT] === DC.NO) {
+    errorArray = [...GV.validateReasonForNotReporting(whyNotReporting)];
+    return errorArray;
+  }
+  errorArray = [
+    ...GV.validateRequiredRadioButtonForCombinedRates(data),
+    ...GV.validateOneDataSource(data),
+    ...GV.ensureBothDatesCompletedInRange(dateRange),
+
+    // Performance Measure Validations
+    ...GV.atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
+    ...GV.validateNoNonZeroNumOrDenom(
+      performanceMeasureArray,
+      OPM,
+      ageGroups,
+      data
+    ),
+    ...GV.validateAtLeastOneNDRInDeviationOfMeasureSpec(
+      performanceMeasureArray,
+      PMD.qualifiers,
+      deviationArray,
+      didCalculationsDeviate
+    ),
+
+    // OMS Validations
+    ...GV.omsValidations({
+      data,
+      qualifiers: PMD.qualifiers,
+      categories: PMD.categories,
+      locationDictionary: GV.omsLocationDictionary(
+        OMSData(true),
+        PMD.qualifiers,
+        PMD.categories
+      ),
+      validationCallbacks: [GV.validateRateZero, GV.validateRateNotZero],
+    }),
+  ];
+
+  return errorArray;
+};
+
+export const validationFunctions = [PQI92Validation];
