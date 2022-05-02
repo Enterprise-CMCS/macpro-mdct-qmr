c7b7fcc408317e81e6ed5bf9af422efb5db7bff1
Samuel Sierra <ssierra@collabralink.com>
2022-05-02 10:11:25 -0400

    fix hh unit tests

--- services/ui-src/src/views/Qualifiers/HealthHome/index.test.tsx
+++ services/ui-src/src/views/Qualifiers/HealthHome/index.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { RouterWrappedComp } from "utils/testing";
 import { HHCSQualifiers } from ".";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -28,44 +28,5 @@ beforeEach(() => {
 describe("test header (delivery system) component", () => {
   it("renders header properly", async () => {
     expect(await screen.findByText("Delivery System")).toBeInTheDocument();
-  });
-});
-
-describe("test audit or validation component", () => {
-  it("renders components properly", async () => {
-    expect(
-      await screen.findByText(
-        "Yes, some of the Core Set measures have been audited or validated"
-      )
-    ).toBeInTheDocument();
-    expect(
-      await screen.findByText("No, we calculated all the measures internally.")
-    ).toBeInTheDocument();
-
-    fireEvent.click(
-      screen.getByLabelText(
-        /Yes, some of the Core Set measures have been audited or validated/i
-      )
-    );
-    expect(await screen.findByText(/Select All/i)).toBeInTheDocument();
-  });
-});
-
-describe("test external ontractor component", () => {
-  it("renders components properly", async () => {
-    expect(
-      await screen.findByText(
-        "Yes, we did obtain assistance from one or more external contractors in collecting, calculating, and/or reporting Core Set data."
-      )
-    ).toBeInTheDocument();
-
-    fireEvent.click(
-      await screen.getByLabelText(
-        /Yes, we did obtain assistance from one or more external contractors in collecting, calculating, and\/or reporting Core Set data./i
-      )
-    );
-    expect(
-      await screen.findByText(/Data Analytics Contractor/i)
-    ).toBeInTheDocument();
   });
 });
