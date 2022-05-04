9129dd6b13659bbfd3992baa87808752e4a8871a
Daniel Belcher <daniel.belcher.cs@gmail.com>
2022-05-04 11:02:29 -0500

    unify logic and test oneQualDenomHigherThanOtherDenom

---
+++ services/ui-src/src/measures/globalValidations/validateOneQualDenomHigherThanOtherDenomOMS/index.test.ts
@@ -0,0 +1,134 @@
+import { SINGLE_CATEGORY } from "dataConstants";
+import {
+  validateOneQualDenomHigherThanOtherDenomOMS,
+  validateOneQualDenomHigherThanOtherDenomPM,
+} from "./index";
+
+import {
+  generatePmRateData,
+  generateOmsRateData,
+  locationDictionary,
+  doubleRate,
+  simpleRate,
+  partialRate,
+} from "utils/testUtils/validationHelpers";
+
+describe("Testing Qualifier Rate Higher Than Other Validation", () => {
+  const categories = ["Test Cat 1", "Test Cat 2"];
+  const qualifiers = ["Test Qual 1", "Test Qual 2"];
+  const singleCat = [SINGLE_CATEGORY];
+  const noCat: string[] = [];
+
+  const baseOMSInfo = {
+    categories,
+    qualifiers,
+    locationDictionary,
+    isOPM: false,
+    label: ["TestLabel"],
+  };
+
+  // PM
+  describe("PM Validation", () => {
+    it("should return no errors", () => {
+      const data = generatePmRateData({ categories, qualifiers }, [
+        doubleRate,
+        simpleRate,
+      ]);
+      const errors = validateOneQualDenomHigherThanOtherDenomPM(data, {
+        categories,
+        qualifiers,
+      });
+
+      expect(errors).toHaveLength(0);
+    });
+
+    it("should have error", () => {
+      const data = generatePmRateData({ categories, qualifiers }, [
+        simpleRate,
+        doubleRate,
+      ]);
+      const errors = validateOneQualDenomHigherThanOtherDenomPM(data, {
+        categories,
+        qualifiers,
+      });
+
+      expect(errors).toHaveLength(2);
+      expect(errors[0].errorLocation).toBe("Performance Measure");
+      expect(errors[0].errorMessage).toBe(
+        `${qualifiers[1]} denominator must be less than or equal to ${qualifiers[0]} denominator.`
+      );
+    });
+
+    it("should have error - single category", () => {
+      const data = generatePmRateData({ categories: noCat, qualifiers }, [
+        simpleRate,
+        doubleRate,
+      ]);
+      const errors = validateOneQualDenomHigherThanOtherDenomPM(data, {
+        categories: noCat,
+        qualifiers,
+      });
+
+      expect(errors).toHaveLength(1);
+      expect(errors[0].errorLocation).toBe("Performance Measure");
+      expect(errors[0].errorMessage).toBe(
+        `${qualifiers[1]} denominator must be less than or equal to ${qualifiers[0]} denominator.`
+      );
+    });
+
+    it("should NOT have error from empty rate value ", () => {
+      const data = generatePmRateData({ categories, qualifiers }, [
+        partialRate,
+        partialRate,
+      ]);
+      const errors = validateOneQualDenomHigherThanOtherDenomPM(data, {
+        categories,
+        qualifiers,
+      });
+
+      expect(errors).toHaveLength(0);
+    });
+  });
+
+  // OMS
+  describe("OMS Validation", () => {
+    it("should return no errors", () => {
+      const data = generateOmsRateData(categories, qualifiers, [
+        doubleRate,
+        simpleRate,
+      ]);
+      const errors = validateOneQualDenomHigherThanOtherDenomOMS()({
+        ...baseOMSInfo,
+        rateData: data,
+      });
+
+      expect(errors).toHaveLength(0);
+    });
+
+    it("should return with no error - isOPM", () => {
+      const errors = validateOneQualDenomHigherThanOtherDenomOMS()({
+        ...baseOMSInfo,
+        rateData: {},
+        isOPM: true,
+      });
+      expect(errors).toHaveLength(0);
+    });
+
+    it("should not show last string portion when OMS has no categories", () => {
+      const data = generateOmsRateData(singleCat, qualifiers, [
+        simpleRate,
+        doubleRate,
+      ]);
+      const errors = validateOneQualDenomHigherThanOtherDenomOMS()({
+        ...baseOMSInfo,
+        categories: singleCat,
+        rateData: data,
+      });
+
+      expect(errors).toHaveLength(1);
+      expect(errors[0].errorMessage).toBe(
+        `${qualifiers?.[1]} denominator must be less than or equal to ${qualifiers?.[0]} denominator.`
+      );
+    });
+  });
+});
--- services/ui-src/src/measures/globalValidations/validateOneQualDenomHigherThanOtherDenomOMS/index.ts
+++ services/ui-src/src/measures/globalValidations/validateOneQualDenomHigherThanOtherDenomOMS/index.ts
@@ -1,8 +1,57 @@
 import * as Types from "../../CommonQuestions/types";
 
-import { cleanString } from "utils/cleanString";
-import { OmsValidationCallback } from "../types";
+import { OmsValidationCallback, UnifiedValFuncProps as UVFP } from "../types";
+import {
+  convertOmsDataToRateArray,
+  getPerfMeasureRateArray,
+} from "../dataDrivenTools";
 
+interface ValProps extends UVFP {
+  lowerIndex: number;
+  higherIndex: number;
+}
+
+const _validation = ({
+  location,
+  qualifiers,
+  rateData,
+  higherIndex,
+  lowerIndex,
+}: ValProps) => {
+  const errorArray: FormError[] = [];
+
+  for (const ratefields of rateData) {
+    const highDenom = ratefields[higherIndex];
+    const lowerDenom = ratefields[lowerIndex];
+
+    if (
+      highDenom &&
+      lowerDenom &&
+      highDenom.denominator &&
+      lowerDenom.denominator
+    ) {
+      if (
+        parseFloat(lowerDenom.denominator) > parseFloat(highDenom.denominator)
+      ) {
+        errorArray.push({
+          errorLocation: location,
+          errorMessage: `${qualifiers?.[lowerIndex]} denominator must be less than or equal to ${qualifiers?.[higherIndex]} denominator.`,
+        });
+      }
+    }
+  }
+
+  return errorArray;
+};
+
+/**
+ * Validates that one qualifiers denominator is higher than the other specified qualifiers denominator
+ * @note this function returns the oms validation function
+ *
+ *
+ * @param higherIndex which qualifier index should have the higher denominator
+ * @param lowerIndex which qualifier index should have the lower denominator
+ */
 export const validateOneQualDenomHigherThanOtherDenomOMS = (
   higherIndex = 0,
   lowerIndex = 1
@@ -16,81 +65,37 @@ export const validateOneQualDenomHigherThanOtherDenomOMS = (
     isOPM,
   }) => {
     if (isOPM) return [];
-
-    const rateArr: Types.RateFields[] = [];
-
-    const errors: FormError[] = [];
-
-    const isRateLessThanOther = (rateArr: Types.RateFields[]) => {
-      if (rateArr.length !== 2) return true;
-
-      const compareValue = rateArr[higherIndex].denominator ?? "";
-
-      return (
-        parseFloat(rateArr[lowerIndex].denominator ?? "") <=
-        parseFloat(compareValue)
-      );
-    };
-
-    for (const qual of qualifiers) {
-      const cleanQual = cleanString(qual);
-
-      for (const cat of categories.map((s) => cleanString(s))) {
-        if (rateData.rates?.[cleanQual]?.[cat]) {
-          const temp = rateData.rates[cleanQual][cat][0];
-
-          if (temp && temp.rate) {
-            rateArr.push(temp);
-          }
-        }
-      }
-    }
-
-    if (!isRateLessThanOther(rateArr)) {
-      errors.push({
-        errorLocation: `Optional Measure Stratification: ${locationDictionary(
-          label
-        )}`,
-
-        errorMessage: `${qualifiers[lowerIndex]} denominator must be less than or equal to ${qualifiers[higherIndex]} denominator.`,
-      });
-    }
-
-    return errors;
+    return _validation({
+      categories,
+      qualifiers,
+      higherIndex,
+      lowerIndex,
+      location: `Optional Measure Stratification: ${locationDictionary(label)}`,
+      rateData: convertOmsDataToRateArray(categories, qualifiers, rateData),
+    });
   };
 };
 
+/**
+ * Validates that one qualifiers denominator is higher than the other specified qualifiers denominator
+ *
+ * @param data form data
+ * @param pmData data driven information
+ * @param higherIndex which qualifier index should have the higher denominator
+ * @param lowerIndex which qualifier index should have the lower denominator
+ */
 export const validateOneQualDenomHigherThanOtherDenomPM = (
-  data: Types.DefaultFormData,
+  data: Types.PerformanceMeasure,
   pmData: Types.DataDrivenTypes.PerformanceMeasure,
   higherIndex = 0,
   lowerIndex = 1
 ) => {
-  const errorArray: FormError[] = [];
-  const categories = pmData.categories?.length
-    ? pmData.categories
-    : ["singleCategory"];
-
-  for (const cat of categories.map((s) => cleanString(s))) {
-    const higherRate = data.PerformanceMeasure?.rates?.[cat]?.[higherIndex];
-    const lowerRate = data.PerformanceMeasure?.rates?.[cat]?.[lowerIndex];
-
-    if (
-      higherRate &&
-      higherRate.denominator &&
-      lowerRate &&
-      lowerRate.denominator
-    ) {
-      if (
-        parseFloat(lowerRate.denominator) > parseFloat(higherRate.denominator)
-      ) {
-        errorArray.push({
-          errorLocation: "Performance Measure",
-          errorMessage: `${pmData.qualifiers?.[lowerIndex]} denominator must be less than or equal to ${pmData.qualifiers?.[higherIndex]} denominator.`,
-        });
-      }
-    }
-  }
-
-  return errorArray;
+  return _validation({
+    higherIndex,
+    location: "Performance Measure",
+    lowerIndex,
+    rateData: getPerfMeasureRateArray(data, pmData),
+    categories: pmData.categories,
+    qualifiers: pmData.qualifiers,
+  });
 };
