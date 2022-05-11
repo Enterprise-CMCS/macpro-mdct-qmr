04af75d42c642a1668fe1df0934651e2b77a5584
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-05-11 13:21:56 -0500

    Measure PQ192-HH (#1019)
    * Fix data issues
    
    * remove comma
    
    * add new health home
    
    * add totals and health home info
    
    * add total calc to optional measure strat
    
    * remove adult
    
    * Fix dual pop validation
    
    * add cypress tests (#1041)
    
    Co-authored-by: Daniel Belcher <47116905+daniel-belcher@users.noreply.github.com>

---
+++ services/ui-src/src/measures/2021/PQI92HH/validation.ts
@@ -0,0 +1,76 @@
+import * as DC from "dataConstants";
+import * as GV from "measures/globalValidations";
+import * as PMD from "./data";
+import { FormData } from "./types";
+import { OMSData } from "measures/CommonQuestions/OptionalMeasureStrat/data";
+
+const PQI92Validation = (data: FormData) => {
+  const definitionOfDenominator = data[DC.DEFINITION_OF_DENOMINATOR];
+  const OPM = data[DC.OPM_RATES];
+  const ageGroups = PMD.qualifiers;
+  const whyNotReporting = data[DC.WHY_ARE_YOU_NOT_REPORTING];
+  const didCalculationsDeviate = data[DC.DID_CALCS_DEVIATE] === DC.YES;
+  const deviationArray = GV.getDeviationNDRArray(
+    data.DeviationOptions,
+    data.Deviations
+  );
+  const dateRange = data[DC.DATE_RANGE];
+  const performanceMeasureArray = GV.getPerfMeasureRateArray(data, PMD.data);
+  let errorArray: any[] = [];
+
+  if (data[DC.DID_REPORT] === DC.NO) {
+    errorArray = [...GV.validateReasonForNotReporting(whyNotReporting)];
+    return errorArray;
+  }
+  const validateDualPopInformationArray = [
+    performanceMeasureArray?.[0].filter((pm) => {
+      return pm?.label === "Age 65 and older";
+    }),
+  ];
+
+  const age65PlusIndex = 0;
+
+  errorArray = [
+    ...GV.validateRequiredRadioButtonForCombinedRates(data),
+    ...GV.validateOneDataSource(data),
+    ...GV.ensureBothDatesCompletedInRange(dateRange),
+
+    // Performance Measure Validations
+    ...GV.atLeastOneRateComplete(performanceMeasureArray, OPM, ageGroups),
+    ...GV.validateNoNonZeroNumOrDenom(
+      performanceMeasureArray,
+      OPM,
+      ageGroups,
+      data
+    ),
+    ...GV.validateDualPopInformation(
+      validateDualPopInformationArray,
+      OPM,
+      age65PlusIndex,
+      definitionOfDenominator
+    ),
+    ...GV.validateAtLeastOneNDRInDeviationOfMeasureSpec(
+      performanceMeasureArray,
+      PMD.qualifiers,
+      deviationArray,
+      didCalculationsDeviate
+    ),
+
+    // OMS Validations
+    ...GV.omsValidations({
+      data,
+      qualifiers: PMD.qualifiers,
+      categories: PMD.categories,
+      locationDictionary: GV.omsLocationDictionary(
+        OMSData(true),
+        PMD.qualifiers,
+        PMD.categories
+      ),
+      validationCallbacks: [GV.validateRateZero, GV.validateRateNotZero],
+    }),
+  ];
+
+  return errorArray;
+};
+
+export const validationFunctions = [PQI92Validation];
