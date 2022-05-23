c462934cf8ba0fa87b2e64ebe201457e3be4aff3
JP Hechter <jhechter@fearless.tech>
2022-05-23 18:41:51 -0400

    add some types

--- services/ui-src/src/measures/globalValidations/IUHHValidations/IUHHvalidateNDRTotals/index.tsx
+++ services/ui-src/src/measures/globalValidations/IUHHValidations/IUHHvalidateNDRTotals/index.tsx
@@ -4,6 +4,16 @@ interface NDRforumla {
   rateIndex: number;
 }
 
+interface Qualifier {
+  fields: Field[];
+  label: string;
+}
+
+interface Field {
+  label: string;
+  value?: string;
+}
+
 /* At least one NDR set must be complete (OPM or PM) */
 export const IUHHvalidateNDRTotals = (
   performanceMeasureArray: any,
@@ -14,11 +24,11 @@ export const IUHHvalidateNDRTotals = (
   let errorArray: any[] = [];
   const rateLocations = ndrFormulas.map((ndr: NDRforumla) => ndr.rateIndex);
 
-  performanceMeasureArray.forEach((category: any, i: number) => {
+  performanceMeasureArray.forEach((category: Qualifier[], i: number) => {
     // Sum all fields for each qualifier
     let categorySums: any[] = [];
     for (const qualifier of category.slice(0, -1)) {
-      qualifier.fields.forEach((field: any, x: number) => {
+      qualifier.fields.forEach((field: Field, x: number) => {
         if (field?.value) {
           categorySums[x] ??= 0;
           categorySums[x] += parseFloat(field.value);
@@ -28,7 +38,7 @@ export const IUHHvalidateNDRTotals = (
 
     // Compare calculated sums to values in Total qualifier
     const categoryTotal = category.slice(-1)[0];
-    categoryTotal.fields.forEach((field: any, x: number) => {
+    categoryTotal.fields.forEach((field: Field, x: number) => {
       if (
         rateLocations.includes(x) &&
         ((!field?.value && categorySums[x] !== undefined) ||
