290a62f96eedacba915246d18f0e0d5b7990db59
Samuel Sierra <35947499+gitsierra@users.noreply.github.com>
2022-05-25 15:31:24 -0400

    this will fix destroy issues (#1187)
    * this will fix destroy issues
    
    * add sleep

--- destroy.sh
+++ destroy.sh
@@ -130,16 +130,12 @@ certToDestroy=$(aws apigateway get-client-certificates\
     | grep -o '"clientCertificateId": "[^"]*' \
     | grep -o '[^"]*$')
 
-sleep 60
-
 until [ -z $certToDestroy ];
 do 
   aws apigateway delete-client-certificate --client-certificate-id $certToDestroy || true
-  sleep 60
+  sleep 10
   certToDestroy=$(aws apigateway get-client-certificates\
     | grep \"app-api-${stage}\" -B 2 \
     | grep -o '"clientCertificateId": "[^"]*' \
-    | grep -o '[^"]*$')
+    | grep -o '[^"]*$' || true) 
 done 
-
-exit 0
