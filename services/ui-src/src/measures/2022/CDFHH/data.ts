9fb78cbb2d07535c035b03950219dde6b5bc7c83
James Dinh <jdinh@collabralink.com>
2022-05-25 10:20:36 -0700

    added cdfhh to 2022

---
+++ services/ui-src/src/measures/2022/CDFHH/data.ts
@@ -0,0 +1,50 @@
+import * as DC from "dataConstants";
+import { DataDrivenTypes } from "measures/CommonQuestions/types";
+
+export const categories = [];
+export const qualifiers = [
+  "Ages 12 to 17",
+  "Ages 18 to 64",
+  "Age 65 and older",
+  "Total",
+];
+
+export const data: DataDrivenTypes.PerformanceMeasure = {
+  questionText: [
+    "Percentage of Health Home enrollees age 12 and older screened for depression on the date of the encounter or 14 days prior to the date of the encounter using an age appropriate standardized depression screening tool, and if positive, a follow-up plan is documented on the date of the eligible encounter.",
+  ],
+  categories,
+  qualifiers,
+};
+
+export const dataSourceData: DataDrivenTypes.DataSource = {
+  optionsLabel:
+    "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
+  options: [
+    {
+      value: DC.ADMINISTRATIVE_DATA,
+      subOptions: [
+        {
+          label: "What is the Administrative Data Source?",
+          options: [
+            {
+              value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM,
+            },
+            {
+              value: DC.ADMINISTRATIVE_DATA_OTHER,
+              description: true,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: DC.ELECTRONIC_HEALTH_RECORDS,
+      description: true,
+    },
+    {
+      value: DC.OTHER_DATA_SOURCE,
+      description: true,
+    },
+  ],
+};
---
+++ services/ui-src/src/measures/2022/CDFHH/index.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
+import * as CMQ from "measures/CommonQuestions";
+import * as PMD from "./data";
+import * as QMR from "components";
+import { validationFunctions } from "./validation";
+import { getPerfMeasureRateArray } from "measures/globalValidations";
+import { FormData } from "./types";
+
+export const CDFHH = ({
+  name,
+  year,
+  measureId,
+  setValidationFunctions,
+  isNotReportingData,
+  isPrimaryMeasureSpecSelected,
+  showOptionalMeasureStrat,
+  isOtherMeasureSpecSelected,
+}: QMR.MeasureWrapperProps) => {
+  const { watch } = useFormContext<FormData>();
+  const data = watch();
+  useEffect(() => {
+    if (setValidationFunctions) {
+      setValidationFunctions(validationFunctions);
+    }
+  }, [setValidationFunctions]);
+
+  const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
+
+  return (
+    <>
+      <CMQ.Reporting
+        reportingYear={year}
+        measureName={name}
+        measureAbbreviation={measureId}
+        healthHomeMeasure
+      />
+
+      {!isNotReportingData && (
+        <>
+          <CMQ.StatusOfData />
+          <CMQ.MeasurementSpecification type="CMS" />
+          <CMQ.DataSource data={PMD.dataSourceData} />
+          <CMQ.DateRange type="health" />
+          <CMQ.DefinitionOfPopulation healthHomeMeasure />
+          {isPrimaryMeasureSpecSelected && (
+            <>
+              <CMQ.PerformanceMeasure
+                data={PMD.data}
+                rateScale={100}
+                calcTotal
+              />
+              <CMQ.DeviationFromMeasureSpec categories={PMD.categories} />
+            </>
+          )}
+          {isOtherMeasureSpecSelected && (
+            <CMQ.OtherPerformanceMeasure rateMultiplicationValue={100} />
+          )}
+          <CMQ.CombinedRates healthHomeMeasure />
+          {showOptionalMeasureStrat && (
+            <CMQ.OptionalMeasureStrat
+              categories={PMD.categories}
+              qualifiers={PMD.qualifiers}
+              rateMultiplicationValue={100}
+              performanceMeasureArray={performanceMeasureArray}
+              adultMeasure={false}
+              calcTotal
+            />
+          )}
+        </>
+      )}
+      <CMQ.AdditionalNotes />
+    </>
+  );
+};
