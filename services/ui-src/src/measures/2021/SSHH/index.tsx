5658eb2638a939225dd126bbcd473d90f44f0aa4
Benjamin Paige <benjamin.paige@gmail.com>
2022-06-07 21:44:51 -0600

    Remove are you reporting

--- services/ui-src/src/measures/2021/SSHH/index.tsx
+++ services/ui-src/src/measures/2021/SSHH/index.tsx
@@ -6,12 +6,8 @@ import { validationFunctions } from "./validation";
 import { PerformanceMeasure } from "./questions/PerformanceMeasure";
 
 export const SSHH = ({
-  name,
   detailedDescription,
-  year,
-  measureId,
   setValidationFunctions,
-  isNotReportingData,
 }: QMR.MeasureWrapperProps) => {
   useEffect(() => {
     if (setValidationFunctions) {
@@ -24,23 +20,11 @@ export const SSHH = ({
       <CUI.Text fontSize="xl" my="6" fontWeight={400}>
         {detailedDescription}
       </CUI.Text>
-      <CMQ.Reporting
-        reportingYear={year}
-        measureName={name}
-        measureAbbreviation={measureId}
-        healthHomeMeasure
-      />
-
-      {!isNotReportingData && (
-        <>
-          <CMQ.StatusOfData />
-          <CMQ.DataSource />
-          <CMQ.DateRange type="health" />
-          <CMQ.DefinitionOfPopulation healthHomeMeasure />
-
-          <PerformanceMeasure />
-        </>
-      )}
+      <CMQ.StatusOfData />
+      <CMQ.DataSource />
+      <CMQ.DateRange type="health" />
+      <CMQ.DefinitionOfPopulation healthHomeMeasure />
+      <PerformanceMeasure />
       <CMQ.AdditionalNotes />
     </>
   );
--- services/ui-src/src/measures/2021/SSHH/validation.ts
+++ services/ui-src/src/measures/2021/SSHH/validation.ts
@@ -5,12 +5,6 @@ import { FormData } from "./types";
 const SSHHValidation = (data: FormData) => {
   let errorArray: any[] = [];
   const dateRange = data[DC.DATE_RANGE];
-  const whyNotReporting = data[DC.WHY_ARE_YOU_NOT_REPORTING];
-
-  if (data[DC.DID_REPORT] === DC.NO) {
-    errorArray = [...GV.validateReasonForNotReporting(whyNotReporting)];
-    return errorArray;
-  }
 
   errorArray = [
     ...GV.validateAtLeastOneDataSource(data),
