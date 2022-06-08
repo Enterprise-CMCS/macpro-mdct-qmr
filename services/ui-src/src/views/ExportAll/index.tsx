2985ebdaf4e6939986959014d9146c492f203a0d
13bfrancis <13bfrancis@gmail.com>
2022-06-08 10:50:37 -0400

    remove prince code

--- services/ui-src/src/views/ExportAll/index.tsx
+++ services/ui-src/src/views/ExportAll/index.tsx
@@ -1,26 +1,23 @@
 import * as QMR from "components";
+import * as CUI from "@chakra-ui/react";
 import Measures from "measures";
 import { useGetMeasures } from "hooks/api";
 import { createElement } from "react";
-import { getPDF } from "libs/api";
-import { useParams } from "react-router-dom";
-
-// type CoreSetType = "A" | "C" | "H";
+import "index.scss";
 
 export const ExportAll = () => {
-  const openPdf = (basePdf: string) => {
-    let byteCharacters = atob(basePdf);
-    let byteNumbers = new Array(byteCharacters.length);
-    for (let i = 0; i < byteCharacters.length; i++) {
-      byteNumbers[i] = byteCharacters.charCodeAt(i);
-    }
-    let byteArray = new Uint8Array(byteNumbers);
-    let file = new Blob([byteArray], { type: "application/pdf;base64" });
-    let fileURL = URL.createObjectURL(file);
-    window.open(fileURL);
-  };
-
-  const { state, coreSetId, year } = useParams();
+  // This is for PrinceXML. Uncomment once figured out how to use it.
+  // const openPdf = (basePdf: string) => {
+  //   let byteCharacters = atob(basePdf);
+  //   let byteNumbers = new Array(byteCharacters.length);
+  //   for (let i = 0; i < byteCharacters.length; i++) {
+  //     byteNumbers[i] = byteCharacters.charCodeAt(i);
+  //   }
+  //   let byteArray = new Uint8Array(byteNumbers);
+  //   let file = new Blob([byteArray], { type: "application/pdf;base64" });
+  //   let fileURL = URL.createObjectURL(file);
+  //   window.open(fileURL);
+  // };
 
   const { data, isLoading } = useGetMeasures();
   if (isLoading || !data.Items) {
@@ -33,42 +30,13 @@ export const ExportAll = () => {
 
   return (
     <>
-      <button
+      <CUI.Button
         type="button"
-        onClick={async () => {
-          const html = document.querySelector("html")!;
-          html.querySelector("noscript")?.remove();
-
-          const htmlString = html
-            .innerHTML!.replaceAll(
-              '<link href="',
-              `<link href="https://${window.location.host}`
-            )
-            .replaceAll(`’`, `'`)
-            .replaceAll(`‘`, `'`)
-            .replaceAll(`”`, `"`)
-            .replaceAll(`“`, `"`)
-            .replaceAll("\u2013", "-")
-            .replaceAll("\u2014", "-");
-
-          const base64String = btoa(unescape(encodeURIComponent(htmlString)));
-
-          try {
-            const test = await getPDF({
-              body: base64String,
-              state,
-              coreSet: coreSetId,
-              year,
-            });
-            console.log(test);
-            openPdf(test);
-          } catch (err) {
-            console.log(err);
-          }
-        }}
+        onClick={() => window.print()}
+        className="hidden-print-items"
       >
-        Testing
-      </button>
+        Print
+      </CUI.Button>
       {sortedData?.map((measure: any) => {
         const Comp = Measures[measure.year][measure.measure];
 
@@ -85,4 +53,131 @@ export const ExportAll = () => {
       })}
     </>
   );
+};
+
+const Base64 = {
+  // private property
+  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+  // public method for encoding
+  encode: function (input: any) {
+    var output = "";
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+    var i = 0;
+
+    input = Base64._utf8_encode(input);
+
+    while (i < input.length) {
+      chr1 = input.charCodeAt(i++);
+      chr2 = input.charCodeAt(i++);
+      chr3 = input.charCodeAt(i++);
+
+      enc1 = chr1 >> 2;
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+      enc4 = chr3 & 63;
+
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64;
+      } else if (isNaN(chr3)) {
+        enc4 = 64;
+      }
+
+      output =
+        output +
+        this._keyStr.charAt(enc1) +
+        this._keyStr.charAt(enc2) +
+        this._keyStr.charAt(enc3) +
+        this._keyStr.charAt(enc4);
+    } // Whend
+
+    return output;
+  }, // End Function encode
+
+  // public method for decoding
+  decode: function (input: any) {
+    var output = "";
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0;
+
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    while (i < input.length) {
+      enc1 = this._keyStr.indexOf(input.charAt(i++));
+      enc2 = this._keyStr.indexOf(input.charAt(i++));
+      enc3 = this._keyStr.indexOf(input.charAt(i++));
+      enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+
+      output = output + String.fromCharCode(chr1);
+
+      if (enc3 != 64) {
+        output = output + String.fromCharCode(chr2);
+      }
+
+      if (enc4 != 64) {
+        output = output + String.fromCharCode(chr3);
+      }
+    } // Whend
+
+    output = Base64._utf8_decode(output);
+
+    return output;
+  }, // End Function decode
+
+  // private method for UTF-8 encoding
+  _utf8_encode: function (string: any) {
+    var utftext = "";
+    string = string.replace(/\r\n/g, "\n");
+
+    for (var n = 0; n < string.length; n++) {
+      var c = string.charCodeAt(n);
+
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      } else if (c > 127 && c < 2048) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      } else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    } // Next n
+
+    return utftext;
+  }, // End Function _utf8_encode
+
+  // private method for UTF-8 decoding
+  _utf8_decode: function (utftext: any) {
+    var string = "";
+    var i = 0;
+    var c, c1, c2, c3;
+    c = c1 = c2 = 0;
+
+    while (i < utftext.length) {
+      c = utftext.charCodeAt(i);
+
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      } else if (c > 191 && c < 224) {
+        c2 = utftext.charCodeAt(i + 1);
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        i += 2;
+      } else {
+        c2 = utftext.charCodeAt(i + 1);
+        c3 = utftext.charCodeAt(i + 2);
+        string += String.fromCharCode(
+          ((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+        );
+        i += 3;
+      }
+    } // Whend
+
+    return string;
+  }, // End Function _utf8_decode
 };
