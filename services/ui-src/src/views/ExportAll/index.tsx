7298f719a21d14a74fb525946fbb2ff8eddc9ba0
13bfrancis <13bfrancis@gmail.com>
2022-06-09 00:21:00 -0400

    prince works. wooh. gonna clean it up tomorrow

--- services/ui-src/src/views/ExportAll/index.tsx
+++ services/ui-src/src/views/ExportAll/index.tsx
@@ -4,20 +4,61 @@ import Measures from "measures";
 import { useGetMeasures } from "hooks/api";
 import { createElement } from "react";
 import "index.scss";
+import { getPDF } from "libs/api";
+import { useParams } from "react-router-dom";
 
 export const ExportAll = () => {
-  // This is for PrinceXML. Uncomment once figured out how to use it.
-  // const openPdf = (basePdf: string) => {
-  //   let byteCharacters = atob(basePdf);
-  //   let byteNumbers = new Array(byteCharacters.length);
-  //   for (let i = 0; i < byteCharacters.length; i++) {
-  //     byteNumbers[i] = byteCharacters.charCodeAt(i);
-  //   }
-  //   let byteArray = new Uint8Array(byteNumbers);
-  //   let file = new Blob([byteArray], { type: "application/pdf;base64" });
-  //   let fileURL = URL.createObjectURL(file);
-  //   window.open(fileURL);
-  // };
+  const { state, coreSetId, year } = useParams();
+
+  const openPdf = (basePdf: string) => {
+    let byteCharacters = atob(basePdf);
+    let byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    let byteArray = new Uint8Array(byteNumbers);
+    let file = new Blob([byteArray], { type: "application/pdf;base64" });
+    let fileURL = URL.createObjectURL(file);
+    window.open(fileURL);
+  };
+
+  const makePrinceRequest = async () => {
+    const html = document.querySelector("html")!;
+    html.querySelector("noscript")?.remove();
+    document.body.setAttribute(
+      "style",
+      document.querySelectorAll("style")[2].innerHTML.split(/{|}/g)[1]
+    );
+
+    const htmlString = html
+      .innerHTML!.replaceAll(
+        '<link href="',
+        `<link href="https://${window.location.host}`
+      )
+      .replaceAll(`’`, `'`)
+      .replaceAll(`‘`, `'`)
+      .replaceAll(`”`, `"`)
+      .replaceAll(`“`, `"`)
+      .replaceAll(`chakra-space-0\\.5`, `helloworld`)
+      .replaceAll("\u2013", "-")
+      .replaceAll("\u2014", "-");
+    console.log(htmlString);
+
+    const base64String = btoa(unescape(encodeURIComponent(htmlString)));
+
+    try {
+      const test = await getPDF({
+        body: base64String,
+        state,
+        coreSet: coreSetId,
+        year,
+      });
+      console.log(test);
+      openPdf(test);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const { data, isLoading } = useGetMeasures();
   if (isLoading || !data.Items) {
@@ -42,6 +83,17 @@ export const ExportAll = () => {
           onClick={() => window.print()}
           buttonText={"Print"}
         />
+        <QMR.ContainedButton
+          buttonProps={{
+            isFullWidth: true,
+            type: "button",
+            className: "hidden-print-items",
+            background: "blue.500",
+            margin: "1",
+          }}
+          onClick={async () => await makePrinceRequest()}
+          buttonText={"Print 508"}
+        />
       </CUI.Container>
       <CUI.UnorderedList
         display={"grid"}
@@ -53,6 +105,9 @@ export const ExportAll = () => {
         <CUI.Text
           gridColumn={"1 / -1"}
           fontSize={"xl"}
+          style={
+            { fontWeight: "bold", "--testing": "red" } as React.CSSProperties
+          }
           id="top-of-page"
           as="h2"
           my="4"
