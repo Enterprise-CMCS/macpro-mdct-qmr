3952abfad36c27016092aef89143ce6031b7a7e7
James Dinh <jdinh@collabralink.com>
2022-06-14 08:36:31 -0700

    oevch automation testing

---
+++ tests/cypress/cypress/integration/measures/OEVCH.spec.ts
@@ -0,0 +1,129 @@
+describe("Measure 45: OEV-CH", () => {
+  beforeEach(() => {
+    cy.login();
+    cy.get('[data-cy="year-select"]').select("2022");
+    cy.goToChildCoreSetMeasures();
+    cy.goToMeasure("OEV-CH");
+  });
+
+  it("Ensure correct sections display if user is/not reporting", () => {
+    cy.displaysSectionsWhenUserNotReporting();
+    cy.displaysSectionsWhenUserIsReporting();
+  });
+
+  it(
+    "shows an error if user not reporting and doesn't specify why not",
+    cy.showErrorIfNotReportingAndNotWhy
+  );
+
+  it("displays the correct Measurement Specification options", () => {
+    cy.get('[data-cy="measurement-specification-options"]')
+      .should(
+        "contain",
+        "American Dental Association/Dental Quality Alliance (ADA/DQA)"
+      )
+      .and("contain", "Other");
+  });
+
+  it("displays the correct Data Source options", () => {
+    cy.get('[data-cy="data-source-options"]')
+      .should("contain", "Administrative Data")
+      .and("contain", "Other Data Source");
+  });
+
+  it("calculates rates correctly", () => {
+    cy.get('[data-cy="MeasurementSpecification0"').click();
+    cy.enterValidDateRange();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("4");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("5");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "80.0"
+    );
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.1.numerator"]'
+    ).type("5");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.1.denominator"]'
+    ).type("6");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.1.rate"]').should(
+      "have.value",
+      "83.3"
+    );
+  });
+
+  it("rounds the numerical value after the decimal up/down for auto-calculated rates", () => {
+    cy.get('[data-cy="MeasurementSpecification0"]').click();
+    cy.enterValidDateRange();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("1");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("3");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "33.3"
+    );
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).clear();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("2");
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+      "have.value",
+      "66.7"
+    );
+  });
+
+  it("displays a warning when N=0, D>0, and user enters a rate > 0", () => {
+    cy.get('[data-cy="MeasurementSpecification0"').click();
+    cy.get('[data-cy="DataSource1"]').click();
+    cy.enterValidDateRange();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("0");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("12");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
+    ).clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
+      "10.0"
+    );
+    cy.get('[data-cy="Validate Measure"]').click();
+    cy.get('[data-testid="measure-wrapper-form"]').should(
+      "include.text",
+      "Performance Measure/Other Performance Measure ErrorManually entered rate should be 0 if numerator is 0"
+    );
+  });
+
+  it("displays a warning when N>0, D>0, and user enters a rate of 0", () => {
+    cy.get('[data-cy="MeasurementSpecification0"').click();
+    cy.get('[data-cy="DataSource1"]').click();
+    cy.enterValidDateRange();
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
+    ).type("10");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
+    ).type("12");
+    cy.get(
+      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
+    ).clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
+      "0"
+    );
+    cy.get('[data-cy="Validate Measure"]').click();
+    cy.get('[data-testid="measure-wrapper-form"]').should(
+      "include.text",
+      "Rate should not be 0 if numerator and denominator are not 0. If the calculated rate is less than 0.5, disregard this validation."
+    );
+  });
+});
