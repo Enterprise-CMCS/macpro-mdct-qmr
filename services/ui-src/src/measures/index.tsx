d2742e4cb54e073ba9f4ed9a0bd636c48b098dc7
13bfrancis <13bfrancis@gmail.com>
2022-06-15 08:52:43 -0400

    fix 2022 routing for qualifiers

--- services/ui-src/src/measures/index.tsx
+++ services/ui-src/src/measures/index.tsx
@@ -1,6 +1,6 @@
 import { ResolverResult } from "react-hook-form";
 import twentyTwentyOneMeasures, { QualifierData as data2021 } from "./2021";
-import twentyTwentyTwoMeasures from "./2022";
+import twentyTwentyTwoMeasures, { QualifierData as data2022 } from "./2022";
 import * as QMR from "components";
 
 export type CustomValidator = (res: ResolverResult) => ResolverResult;
@@ -24,4 +24,5 @@ const measuresByYear: MeasuresByYear = {
 export default measuresByYear;
 export const QualifierData: IQualifierData[] = [
   { year: "2021", data: data2021 },
+  { year: "2022", data: data2022 },
 ];
