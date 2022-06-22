283ba088b710160720543d583cff1b7cbf7333fc
karla-vm <karla@coforma.io>
2022-06-22 09:57:45 -0400

    Update test to reflect FFY 2022 changes

--- tests/cypress/cypress/integration/features/reporting_in_FY21_tag_for_alt_data_sources.spec.ts
+++ tests/cypress/cypress/integration/features/reporting_in_FY21_tag_for_alt_data_sources.spec.ts
@@ -1,11 +1,11 @@
-describe("OY2 15211 Reporting in FY21 Tag for Alt Data Sources", () => {
+describe("OY2 15211 Reporting in FY22 Tag for Alt Data Sources", () => {
   beforeEach(() => {
     cy.login("stateuser2");
   });
 
   it("N/A And Completed Statuses", () => {
     cy.get("a > [data-cy='ACS']").click();
-    cy.get("[data-cy='Reporting FFY 2021-NCIDDS-AD'] > p").should(
+    cy.get("[data-cy='Reporting FFY 2022-NCIDDS-AD'] > p").should(
       "have.text",
       "N/A"
     );
