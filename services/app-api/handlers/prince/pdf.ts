93ffc583f40d43d2b56efeea7a99472ca0183043
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-06-22 10:15:59 -0400

    small refactors

--- services/app-api/handlers/prince/pdf.ts
+++ services/app-api/handlers/prince/pdf.ts
@@ -2,7 +2,7 @@ import handler from "../../libs/handler-lib";
 import { aws4Interceptor } from "aws4-axios";
 import axios from "axios";
 
-export const getPDF = handler(async (event, context) => {
+export const getPDF = handler(async (event, _context) => {
   const interceptor = aws4Interceptor({
     region: "us-east-1",
     service: "execute-api",
@@ -11,12 +11,11 @@ export const getPDF = handler(async (event, context) => {
   axios.interceptors.request.use(interceptor);
 
   try {
-    const test = await axios.post(
+    const pdf = await axios.post(
       "https://macpro-platform-dev.cms.gov/doc-conv/508html-to-508pdf",
       event.body!
     );
-    console.log(test.data);
-    return test.data;
+    return pdf.data;
   } catch (err) {
     console.log(err);
   }
