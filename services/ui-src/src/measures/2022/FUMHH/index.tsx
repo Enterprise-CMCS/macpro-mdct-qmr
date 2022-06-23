7eddb8e4eaa718edb7c35d132193ab86ccf4559f
Gavin St. Ours <gst-ours@fearless.tech>
2022-06-23 15:01:50 -0400

    Finish building out form

--- services/ui-src/src/measures/2022/FUMHH/index.tsx
+++ services/ui-src/src/measures/2022/FUMHH/index.tsx
@@ -1,12 +1,33 @@
 import * as CMQ from "measures/2022/CommonQuestions";
+import * as PMD from "./data";
 import * as QMR from "components";
+import { FormData } from "./types";
+import { getPerfMeasureRateArray } from "measures/2022/globalValidations";
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
+import { validationFunctions } from "./validation";
 
 export const FUMHH = ({
   isNotReportingData,
+  isOtherMeasureSpecSelected,
+  isPrimaryMeasureSpecSelected,
   measureId,
   name,
+  setValidationFunctions,
+  showOptionalMeasureStrat,
   year,
 }: QMR.MeasureWrapperProps) => {
+  const { watch } = useFormContext<FormData>();
+  const data = watch();
+
+  useEffect(() => {
+    if (setValidationFunctions) {
+      setValidationFunctions(validationFunctions);
+    }
+  }, [setValidationFunctions]);
+
+  const performanceMeasureArray = getPerfMeasureRateArray(data, PMD.data);
+
   return (
     <>
       <CMQ.Reporting
@@ -18,6 +39,30 @@ export const FUMHH = ({
       {!isNotReportingData && (
         <>
           <CMQ.StatusOfData />
+          <CMQ.MeasurementSpecification type="HEDIS" />
+          <CMQ.DataSource data={PMD.dataSourceData} />
+          <CMQ.DateRange type="health" />
+          <CMQ.DefinitionOfPopulation healthHomeMeasure />
+          {isPrimaryMeasureSpecSelected && (
+            <>
+              <CMQ.PerformanceMeasure calcTotal data={PMD.data} />
+              <CMQ.DeviationFromMeasureSpec categories={PMD.categories} />
+            </>
+          )}
+          {isOtherMeasureSpecSelected && (
+            <CMQ.OtherPerformanceMeasure rateMultiplicationValue={100} />
+          )}
+          <CMQ.CombinedRates healthHomeMeasure />
+          {showOptionalMeasureStrat && (
+            <CMQ.OptionalMeasureStrat
+              adultMeasure={false}
+              calcTotal
+              categories={PMD.categories}
+              performanceMeasureArray={performanceMeasureArray}
+              rateMultiplicationValue={100}
+              qualifiers={PMD.qualifiers}
+            />
+          )}
         </>
       )}
     </>
