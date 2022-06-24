9ac493c660b61b63d02f77cb254956be184d31b7
Gavin St. Ours <gst-ours@fearless.tech>
2022-06-24 09:29:49 -0400

    Update with correct text
    Co-authored-by: JP Hechter <jhechter@fearless.tech>

--- services/ui-src/src/measures/2022/FUMHH/data.ts
+++ services/ui-src/src/measures/2022/FUMHH/data.ts
@@ -14,7 +14,7 @@ export const qualifiers = [
 
 export const data: DataDrivenTypes.PerformanceMeasure = {
   questionText: [
-    "Percentage of emergency department (ED) visits for beneficiaries age 18 and Older with a principal diagnosis of mental illness or intentional self-harm and who had a follow-up visit for mental illness. Two rates are reported:",
+    "Percentage of emergency department (ED) visits for health home enrollees ages 6 and older with a principal diagnosis of mental illness or intentional self-harm and who had a follow-up visit for mental illness. Two rates are reported:",
   ],
   questionListItems: [
     "Percentage of ED visits for mental illness for which the beneficiary received follow-up within 30 days of the ED visit (31 total days)",
