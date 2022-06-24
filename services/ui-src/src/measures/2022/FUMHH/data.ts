66bc27df4c89f94094b9d2289d3f2139e942ebfd
Gavin St. Ours <gst-ours@fearless.tech>
2022-06-24 09:30:19 -0400

    Update with correct text
    Co-authored-by: JP Hechter <jhechter@fearless.tech>

--- services/ui-src/src/measures/2022/FUMHH/data.ts
+++ services/ui-src/src/measures/2022/FUMHH/data.ts
@@ -18,7 +18,7 @@ export const data: DataDrivenTypes.PerformanceMeasure = {
   ],
   questionListItems: [
     "Percentage of ED visits for mental illness for which the enrollee received follow-up within 30 days of the ED visit (31 total days)",
-    "Percentage of ED visits for mental illness for which the beneficiary received follow-up within 7 days of the ED visit (8 total days)",
+    "Percentage of ED visits for mental illness for which the enrollee received follow-up within 7 days of the ED visit (8 total days)",
   ],
   categories,
   qualifiers,
