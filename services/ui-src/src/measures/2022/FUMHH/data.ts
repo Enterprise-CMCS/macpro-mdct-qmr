2373d9296be4c2579e68bd9a1175f9cdc0474431
Gavin St. Ours <gst-ours@fearless.tech>
2022-06-27 14:15:48 -0400

    Change “ages” to “age” for consistency across measures

--- services/ui-src/src/measures/2022/FUMHH/data.ts
+++ services/ui-src/src/measures/2022/FUMHH/data.ts
@@ -8,12 +8,12 @@ export const qualifiers = [
   "Ages 6 to 17",
   "Ages 18 to 64",
   "Age 65 and older",
-  "Total (Ages 6 and older)",
+  "Total (Age 6 and older)",
 ];
 
 export const data: DataDrivenTypes.PerformanceMeasure = {
   questionText: [
-    "Percentage of emergency department (ED) visits for health home enrollees ages 6 and older with a principal diagnosis of mental illness or intentional self-harm and who had a follow-up visit for mental illness. Two rates are reported:",
+    "Percentage of emergency department (ED) visits for health home enrollees age 6 and older with a principal diagnosis of mental illness or intentional self-harm and who had a follow-up visit for mental illness. Two rates are reported:",
   ],
   questionListItems: [
     "Percentage of ED visits for mental illness for which the enrollee received follow-up within 30 days of the ED visit (31 total days)",
