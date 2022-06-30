360bff6d37390b272ffe4140da9f9d39e7071269
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-06-30 08:29:45 -0500

    first and final (#1334)
    * first and final
    
    * copy/paste fix

--- services/ui-src/src/measures/2022/CISCH/data.ts
+++ services/ui-src/src/measures/2022/CISCH/data.ts
@@ -12,21 +12,15 @@ export const qualifiers = [
   "Hep A",
   "RV",
   "Flu",
-  "Combo 2",
   "Combo 3",
-  "Combo 4",
-  "Combo 5",
-  "Combo 6",
   "Combo 7",
-  "Combo 8",
-  "Combo 9",
   "Combo 10",
 ];
 export const categories = [];
 
 export const data: DataDrivenTypes.PerformanceMeasure = {
   questionText: [
-    "Percentage of children age 2 who had four diphtheria, tetanus and acellular pertussis (DTaP); three polio (IPV); one measles, mumps and rubella (MMR); three haemophilus influenza type B (HiB); three hepatitis B (Hep B), one chicken pox (VZV); four pneumococcal conjugate (PCV); one hepatitis A (HepA); two or three rotavirus (RV); and two influenza (flu) vaccines by their second birthday. The measure calculates a rate for each vaccine and nine separate combination rates.",
+    "Percentage of children age 2 who had four diphtheria, tetanus and acellular pertussis (DTaP); three polio (IPV); one measles, mumps and rubella (MMR); three haemophilus influenza type B (HiB); three hepatitis B (Hep B), one chicken pox (VZV); four pneumococcal conjugate (PCV); one hepatitis A (HepA); two or three rotavirus (RV); and two influenza (flu) vaccines by their second birthday. The measure calculates a rate for each vaccine and three separate combination rates.",
   ],
   questionListItems: [],
   categories,
