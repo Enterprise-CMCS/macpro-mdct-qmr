628bfa593a0ab4028ceb729c41d236bcbb7fc642
Gavin St. Ours <gst-ours@fearless.tech>
2022-07-08 12:58:12 -0400

    Add additional tests for HEDIS

--- services/ui-src/src/measures/2021/CommonQuestions/MeasurementSpecification/index.test.tsx
+++ services/ui-src/src/measures/2021/CommonQuestions/MeasurementSpecification/index.test.tsx
@@ -1,3 +1,4 @@
+import fireEvent from "@testing-library/user-event";
 import { MeasurementSpecification } from ".";
 import { renderWithHookForm } from "utils/testUtils/reactHookFormRenderer";
 import { screen } from "@testing-library/react";
@@ -13,6 +14,25 @@ describe("MeasurementSpecification component", () => {
     ).toBeInTheDocument();
 
     expect(screen.getByText("Other")).toBeInTheDocument();
+
+    const otherRadio = await screen.getByLabelText("Other");
+    fireEvent.click(otherRadio);
+
+    const textArea = await screen.findByLabelText(
+      "Describe the specifications that were used to calculate the measure and explain how they deviated from Core Set specifications:"
+    );
+
+    expect(textArea).toBeInTheDocument();
+
+    const testText = "This is test text for TextArea";
+    fireEvent.type(textArea, testText);
+    expect(textArea).toHaveDisplayValue(testText);
+
+    expect(
+      screen.getByText(
+        "If you need additional space to describe your state's methodology, please attach further documentation below."
+      )
+    ).toBeInTheDocument();
   });
 });
 
@@ -90,13 +110,26 @@ const specifications: {
 
 describe("all specification types", () => {
   for (const spec in specifications) {
-    it(`renders ${spec} specification type correctly`, () => {
+    it(`renders ${spec} specification type correctly`, async () => {
       renderWithHookForm(
         <MeasurementSpecification type={specifications[spec].propType.type} />
       );
       expect(
         screen.getByText(specifications[spec].displayValue)
       ).toBeInTheDocument();
+
+      if (spec === "HEDIS") {
+        const radio = await screen.getByLabelText(
+          "National Committee for Quality Assurance (NCQA)/Healthcare Effectiveness Data and Information Set (HEDIS)"
+        );
+        fireEvent.click(radio);
+
+        expect(
+          screen.getByText(
+            "NCQA, the measure steward, changed its naming convention. HEDIS MY 2020 refers to a different federal fiscal year (FFY) than HEDIS 2020. Please note the FFY Core Set specification below."
+          )
+        ).toBeInTheDocument();
+      }
     });
   }
 });
