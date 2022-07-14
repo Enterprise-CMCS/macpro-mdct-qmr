789ca75658ba185bc447d7b2b63e221f6f93d1ff
Benjamin Paige <benjamin.paige@gmail.com>
2022-07-14 14:27:26 -0600

    Add prettierignore

--- services/ui-src/src/utils/getLabelText.tsx
+++ services/ui-src/src/utils/getLabelText.tsx
@@ -13,3 +13,26 @@ export const getLabelText = () => {
     };
   }
 };
+
+/**
+ * Attention
+ * Changing the keys of these objects will change how the measure data is shaped and should not be done unless that is the desired result.
+ * Changing the values of these objects will change the text that is displayed to the user.
+ */
+
+export const foo = {
+  AMMAD: {
+    qualifierLabelsAndText: {
+      "Ages 18 to 64": "Ages 18 to 64 - here is the text i would like displayed",
+      "Age 65 and older": "Age 65 and older",
+    },
+    categoryLabelsAndText: {
+      "Effective Acute Phase Treatment": "Effective Acute Phase Treatment - here is some custom text for a category",
+      "Effective Continuation Phase Treatment": "Effective Continuation Phase Treatment",
+    },
+  },
+};
+
+/**
+ * Brice Haire
+ */
