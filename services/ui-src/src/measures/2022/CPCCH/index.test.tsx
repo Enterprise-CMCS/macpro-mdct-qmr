e42774efc3dce497257e073d613e3327f1f606b1
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-08-08 13:14:38 -0500

    Fix test

--- services/ui-src/src/measures/2022/CPCCH/index.test.tsx
+++ services/ui-src/src/measures/2022/CPCCH/index.test.tsx
@@ -13,8 +13,6 @@ import { clearMocks } from "utils/testUtils/validationsMock";
 import { axe, toHaveNoViolations } from "jest-axe";
 expect.extend(toHaveNoViolations);
 
-//TODO: Revisit and add full path
-
 // Test Setup
 const measureAbbr = "CPC-CH";
 const coreSet = "CCSC";
@@ -83,10 +81,57 @@ describe(`Test FFY ${year} ${measureAbbr}`, () => {
   /**
    * Render the measure and confirm that all expected components exist.
    * */
-  it("Always shows Did You Collect Question", async () => {
+  it("Always shows Did you collect question", async () => {
     useApiMock(apiData);
     renderWithHookForm(component);
     expect(screen.getByText("Did you collect this measure?"));
+  });
+
+  it("shows corresponding questions if yes to reporting then ", async () => {
+    apiData.useGetMeasureValues.data.Item.data = completedMeasureData;
+    useApiMock(apiData);
+    renderWithHookForm(component);
+    expect(
+      screen.getByText("How did you report this measure?")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Measurement Specification")).toBeInTheDocument();
+    expect(screen.getByText("Data Source")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Which Supplemental Item Sets were included in the Survey"
+      )
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Which administrative protocol was used to administer the survey?"
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("does not show corresponding questions if no to reporting then ", async () => {
+    apiData.useGetMeasureValues.data.Item.data = notReportingData;
+    useApiMock(apiData);
+    renderWithHookForm(component);
+    expect(
+      screen.queryByText("How did you report this measure?")
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("Measurement Specification")
+    ).not.toBeInTheDocument();
+    expect(screen.queryByText("Data Source")).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        "Which Supplemental Item Sets were included in the Survey"
+      )
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        "Which administrative protocol was used to administer the survey?"
+      )
+    ).not.toBeInTheDocument();
+    expect(
+      screen.getByText("Why did you not collect this measure")
+    ).toBeInTheDocument();
   });
 
   jest.setTimeout(15000);
@@ -98,3 +143,12 @@ describe(`Test FFY ${year} ${measureAbbr}`, () => {
     expect(results).toHaveNoViolations();
   });
 });
+
+const notReportingData = {
+  DidCollect: "no",
+};
+
+const completedMeasureData = {
+  MeasurementSpecification: "AHRQ-NCQA",
+  DidCollect: "yes",
+};
