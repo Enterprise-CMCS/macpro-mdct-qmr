ea49d68401937fe01455e88d8cedc0a430bb7d50
karla-vm <karla@coforma.io>
2022-09-29 11:10:44 -0400

    Removing redundant else condition

--- services/ui-src/src/components/MeasureWrapper/index.tsx
+++ services/ui-src/src/components/MeasureWrapper/index.tsx
@@ -213,7 +213,7 @@ export const MeasureWrapper = ({
       );
     }
     // default loaded data reset
-    else if (!methods.formState.isDirty) {
+    else {
       methods.reset(apiData?.Item?.data);
     }
   }, [apiData, methods, defaultData, coreSetId]);
