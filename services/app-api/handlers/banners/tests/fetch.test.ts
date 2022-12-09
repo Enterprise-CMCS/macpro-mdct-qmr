961fdd12cefd50349a3fa06a8737b78bf954b766
Thomas Blackwell <thomas.blackwell@coforma.io>
2022-12-09 17:01:49 -0500

    fetch test working, create in progess

--- services/app-api/handlers/banners/tests/fetch.test.ts
+++ services/app-api/handlers/banners/tests/fetch.test.ts
@@ -1,19 +1,19 @@
 import { fetchBanner } from "../fetch";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { proxyEvent } from "./proxyEvent";
-import { StatusCodes } from "../../../utils/types/types";
-import error from "../../../utils/constants/constants";
+import dynamoDb from "../../../libs/dynamodb-lib";
+import { Errors, StatusCodes } from "../../../utils/constants/constants";
 import {
   mockBannerResponse,
   mockDocumentClient,
-} from "../../utils/testing/setupJest";
+} from "../../../utils/testing/setupJest";
 
-jest.mock("../../utils/auth/authorization", () => ({
+jest.mock("../../../libs/authorization", () => ({
   isAuthorized: jest.fn().mockReturnValue(true),
   hasPermissions: jest.fn().mockReturnValue(true),
 }));
 
-jest.mock("../../utils/debugging/debug-lib", () => ({
+jest.mock("../../../libs/debug-lib", () => ({
   init: jest.fn(),
   flush: jest.fn(),
 }));
@@ -26,20 +26,28 @@ const testEvent: APIGatewayProxyEvent = {
 
 describe("Test fetchBanner API method", () => {
   test("Test Report not found Fetch", async () => {
-    mockDocumentClient.get.promise.mockReturnValueOnce({ Item: undefined });
+    jest.spyOn(dynamoDb, "get").mockImplementation(
+      mockDocumentClient.get.promise.mockReturnValueOnce({
+        Item: undefined,
+      })
+    );
     const res = await fetchBanner(testEvent, null);
-    expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
+    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+    expect(JSON.parse(res.body).status).toBe(StatusCodes.NOT_FOUND);
   });
 
   test("Test Successful Banner Fetch", async () => {
-    mockDocumentClient.get.promise.mockReturnValueOnce({
-      Item: mockBannerResponse,
-    });
+    jest.spyOn(dynamoDb, "get").mockImplementation(
+      mockDocumentClient.get.promise.mockReturnValueOnce({
+        Item: mockBannerResponse,
+      })
+    );
     const res = await fetchBanner(testEvent, null);
-
+    const parsedRes = JSON.parse(res.body);
     expect(res.statusCode).toBe(StatusCodes.SUCCESS);
-    expect(res.body).toContain("testDesc");
-    expect(res.body).toContain("testTitle");
+    expect(parsedRes.status).toBe(StatusCodes.SUCCESS);
+    expect(parsedRes.body.Item.title).toEqual("testTitle");
+    expect(parsedRes.body.Item.description).toEqual("testDesc");
   });
 
   test("Test bannerKey not provided throws 500 error", async () => {
@@ -49,8 +57,8 @@ describe("Test fetchBanner API method", () => {
     };
     const res = await fetchBanner(noKeyEvent, null);
 
-    expect(res.statusCode).toBe(500);
-    expect(res.body).toContain(error.NO_KEY);
+    expect(res.statusCode).toBe(StatusCodes.SERVER_ERROR);
+    expect(res.body).toContain(Errors.NO_KEY);
   });
 
   test("Test bannerKey empty throws 500 error", async () => {
@@ -60,7 +68,7 @@ describe("Test fetchBanner API method", () => {
     };
     const res = await fetchBanner(noKeyEvent, null);
 
-    expect(res.statusCode).toBe(500);
-    expect(res.body).toContain(error.NO_KEY);
+    expect(res.statusCode).toBe(StatusCodes.SERVER_ERROR);
+    expect(res.body).toContain(Errors.NO_KEY);
   });
 });
