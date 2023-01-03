887ab8049a1a821a791416db953eff4ff8f94d27
Thomas Blackwell <thomas.blackwell@coforma.io>
2023-01-03 15:04:44 -0500

    Updating to use CMS Design

--- services/ui-src/src/components/Banner/CurrentBanner.tsx
+++ services/ui-src/src/components/Banner/CurrentBanner.tsx
@@ -1,18 +1,17 @@
 import { Banner } from "./Banner";
-import * as CUI from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { BannerData } from "types";
 import { checkDateRangeStatus, convertDateUtcToEt } from "utils";
+import { Spinner, Button } from "@cmsgov/design-system";
 
 export const CurrentBanner = ({
   bannerData,
-  sx,
   onDelete: onDeleteHandler,
   ...props
 }: Props) => {
   const [deleting, setDeleting] = useState<boolean>(false);
   const [isBannerActive, setIsBannerActive] = useState<boolean>(false);
-
   useEffect(() => {
     let bannerActivity = false;
     if (bannerData) {
@@ -27,38 +26,60 @@ export const CurrentBanner = ({
   const deleteBanner = async () => {
     setDeleting(true);
     onDeleteHandler();
+    bannerData = undefined;
     setDeleting(false);
   };
 
   return (
-    <CUI.Flex sx={sx.currentBannerFlex}>
-      {bannerData && (
-        <>
-          <CUI.Text sx={sx.currentBannerStatus}>
-            Status:{" "}
-            <span className={isBannerActive ? "active" : "inactive"}>
-              {isBannerActive ? "Active" : "Inactive"}
-            </span>
-          </CUI.Text>
-          <CUI.Text sx={sx.currentBannerDate}>
-            Start Date: <span>{convertDateUtcToEt(bannerData?.startDate)}</span>
-          </CUI.Text>
-          <CUI.Text sx={sx.currentBannerDate}>
-            End Date: <span>{convertDateUtcToEt(bannerData?.endDate)}</span>
-          </CUI.Text>
-          <Banner bannerData={bannerData} {...props} />
-          <CUI.Button sx={sx.deleteBannerButton} onClick={deleteBanner}>
-            {deleting ? <CUI.Spinner size="small" /> : "Delete Current Banner"}
-          </CUI.Button>
-        </>
-      )}
-      {!bannerData && <CUI.Text>There is no current banner</CUI.Text>}
-    </CUI.Flex>
+    <div className="ds-l-row">
+      <div className="ds-l-col">
+        {bannerData && (
+          <Box sx={sx.currentBanner}>
+            <Text>
+              Status:{" "}
+              <span className={isBannerActive ? "active" : "inactive"}>
+                {isBannerActive ? "Active" : "Inactive"}
+              </span>
+            </Text>
+            <Text>
+              Start Date:{" "}
+              <span>{convertDateUtcToEt(bannerData?.startDate)}</span>
+            </Text>
+            <Text>
+              End Date: <span>{convertDateUtcToEt(bannerData?.endDate)}</span>
+            </Text>
+
+            <div className="ds-u-padding-top--2 ds-u-padding-bottom--2">
+              <Banner bannerData={bannerData} {...props} />
+            </div>
+
+            <Button onClick={deleteBanner} variation="solid">
+              {deleting ? <Spinner size="small" /> : "Delete Current Banner"}
+            </Button>
+          </Box>
+        )}
+        {!bannerData && <Text>There is no current banner</Text>}
+      </div>
+    </div>
   );
 };
 interface Props {
   bannerData: BannerData | undefined;
-  sx: any;
   onDelete: () => void;
   [key: string]: any;
 }
+
+const sx = {
+  currentBanner: {
+    marginLeft: "0.5rem",
+    span: {
+      textAlign: "right",
+      "&.active": {
+        color: "#12890e", //$color-success
+      },
+      "&.inactive": {
+        color: "#e31c3d", //$color-error
+      },
+    },
+  },
+};
