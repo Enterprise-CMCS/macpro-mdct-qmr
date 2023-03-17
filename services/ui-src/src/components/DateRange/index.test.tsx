e9e8e5c9a37f95ee7223b25ce0364321d2d7d189
brittbujacich <britt.bujacich@gmail.com>
2023-03-17 11:24:50 -0700

    tests added

--- services/ui-src/src/components/DateRange/index.test.tsx
+++ services/ui-src/src/components/DateRange/index.test.tsx
@@ -40,4 +40,43 @@ describe("Test DateRange", () => {
       screen.queryByText("Please enter start date year in YYYY-format")
     ).not.toBeInTheDocument();
   });
+
+  test("Check that error appears when user inputs a start date after the end date", () => {
+    const screen = renderWithHookForm(<DateRange name="testComponent" />);
+
+    const startMonthLabel = screen.getAllByLabelText(/month/i)[0];
+    const yearLabel = screen.getAllByLabelText(/year/i)[0];
+    const endMonthLabel = screen.getAllByLabelText(/month/i)[1];
+    const endYearLabel = screen.getAllByLabelText(/year/i)[1];
+    userEvent.type(startMonthLabel, "01");
+    userEvent.type(yearLabel, "2023");
+    userEvent.type(endMonthLabel, "02");
+    userEvent.type(endYearLabel, "2022");
+    expect(
+      screen.queryByText("Start Date must be before the End Date")
+    ).toBeInTheDocument();
+  });
+
+  test("Check that error appears when start date is a date in the future", () => {
+    const screen = renderWithHookForm(<DateRange name="testComponent" />);
+    const startMonthLabel = screen.getAllByLabelText(/month/i)[0];
+    const yearLabel = screen.getAllByLabelText(/year/i)[0];
+    userEvent.type(startMonthLabel, "01");
+    userEvent.type(yearLabel, "2080");
+    expect(
+      screen.queryByText("Start date cannot be a future date")
+    ).toBeInTheDocument();
+  });
+
+  test("Check that error appears when end date is a date in the future", () => {
+    const screen = renderWithHookForm(<DateRange name="testComponent" />);
+
+    const endMonthLabel = screen.getAllByLabelText(/month/i)[1];
+    const endYearLabel = screen.getAllByLabelText(/year/i)[1];
+    userEvent.type(endMonthLabel, "02");
+    userEvent.type(endYearLabel, "2080");
+    expect(
+      screen.queryByText("End date cannot be a future date")
+    ).toBeInTheDocument();
+  });
 });
