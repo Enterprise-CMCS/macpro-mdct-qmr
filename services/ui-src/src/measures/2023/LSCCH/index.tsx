feab61b5538c60dc819b3463528d79915d09d9b4
Nathan Keeys <nathankeeys@coformaatekeeys.myfiosgateway.com>
2023-05-04 12:15:51 -0400

    remove spacing

--- services/ui-src/src/measures/2023/LSCCH/index.tsx
+++ services/ui-src/src/measures/2023/LSCCH/index.tsx
@@ -34,7 +34,6 @@ export const LSCCH = ({
         measureName={name}
         measureAbbreviation={measureId}
       />
-
       {!isNotReportingData && (
         <>
           <CMQ.StatusOfData />
