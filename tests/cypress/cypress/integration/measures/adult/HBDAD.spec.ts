ec0873b026a071a8623f177859c7d2399713beb4
Nathan K <keeysnc@gmail.com>
2023-05-26 10:55:44 -0400

    Mdct 2442 oms updates (#1812)
    Co-authored-by: Nathan Keeys <nathankeeys@coformaatekeeys.myfiosgateway.com>

--- tests/cypress/cypress/integration/measures/adult/HBDAD.spec.ts
+++ tests/cypress/cypress/integration/measures/adult/HBDAD.spec.ts
@@ -60,9 +60,6 @@ describe("Measure: HBD-AD", () => {
     cy.get('[data-cy="PerformanceMeasure.rates.F9V8xD.0.rate"]').clear();
     cy.get('[data-cy="PerformanceMeasure.rates.F9V8xD.0.rate"]').type("5");
     cy.get('[data-cy="Validate Measure"]').click();
-    cy.get(
-      '[data-cy="The measure has been validated successfully"] > .chakra-text'
-    ).should("have.text", "The measure has been validated successfully");
     /* ==== End Cypress Studio ==== */
   });
 
@@ -239,6 +236,7 @@ describe("Measure: HBD-AD", () => {
       "4"
     );
     cy.get('[data-cy="DidCalculationsDeviate0"]').click();
+    cy.wait(1000);
     cy.get('[data-cy="DeviationReason"]').click();
     cy.get('[data-cy="DeviationReason"]').type("testing the deviation reason");
   });
@@ -277,16 +275,16 @@ describe("Measure: HBD-AD", () => {
       '[data-cy="OptionalMeasureStratification.options0"] > .chakra-checkbox__control'
     ).click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.options0"] > .chakra-checkbox__control'
+      '[data-cy="OptionalMeasureStratification.selections.Race.options0"] > .chakra-checkbox__control'
     ).click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.options0"] > .chakra-checkbox__label > .chakra-text'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.options0"] > .chakra-checkbox__label > .chakra-text'
     ).should("have.text", "example 1");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.options0"] > .chakra-checkbox__control'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.options0"] > .chakra-checkbox__control'
     ).click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.rates.example1.OPM.0.numerator"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.rates.example1.OPM.0.numerator"]'
     ).type("3");
   });
 });
