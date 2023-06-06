a24f5996f529955da3213b2c6eb8ed83cfcc0392
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2023-06-06 09:47:34 -0400

    Remove Fake-AQ Page (#1821)

--- services/ui-src/src/views/FAQ/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as QMR from "components";
-
-export const FAQ = () => {
-  return (
-    <QMR.StateLayout breadcrumbItems={[{ path: `/faq`, name: "FAQ" }]}>
-      <h1>This will be the FAQ Page</h1>
-    </QMR.StateLayout>
-  );
-};
