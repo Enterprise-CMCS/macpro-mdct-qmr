f2f62503f6edcc0b8884724cfa9c38aefc57acb8
ailZhou <aileen.zhou@coforma.io>
2023-06-12 16:14:26 -0400

    removed hedis val from non hedis measures

--- services/ui-src/src/measures/2023/HPCMIAD/validation.ts
+++ services/ui-src/src/measures/2023/HPCMIAD/validation.ts
@@ -14,7 +14,6 @@ const HPCMIADValidation = (data: FormData) => {
   const DefinitionOfDenominator = data[DC.DEFINITION_OF_DENOMINATOR];
   const didCalculationsDeviate = data[DC.DID_CALCS_DEVIATE] === DC.YES;
   const deviationReason = data[DC.DEVIATION_REASON];
-  const measureSpecifications = data[DC.MEASUREMENT_SPECIFICATION_HEDIS];
 
   let errorArray: any[] = [];
   if (data[DC.DID_REPORT] === DC.NO) {
@@ -47,7 +46,6 @@ const HPCMIADValidation = (data: FormData) => {
     ...GV.validateRequiredRadioButtonForCombinedRates(data),
     ...GV.validateBothDatesCompleted(dateRange),
     ...GV.validateYearFormat(dateRange),
-    ...GV.validateHedisYear(measureSpecifications),
     ...GV.validateOPMRates(OPM),
     ...GV.validateAtLeastOneDataSource(data),
     ...GV.validateAtLeastOneDeviationFieldFilled(
