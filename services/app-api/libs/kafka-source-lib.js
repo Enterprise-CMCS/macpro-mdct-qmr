b1d4078cd1cede9153a587bef8f53e5d5e3a3d5f
Ishmeal Baawuah <ibaawuah@ip-10-232-50-59.ec2.internal>
2023-07-12 15:34:18 -0400

    adding iam auth

--- services/app-api/libs/kafka-source-lib.js
+++ services/app-api/libs/kafka-source-lib.js
@@ -1,30 +1,7 @@
 import AWS from "aws-sdk";
-const { Kafka } = require("kafkajs");
-
-const STAGE = process.env.STAGE;
-const kafka = new Kafka({
-  clientId: `qmr-${STAGE}`,
-  brokers: process.env.BOOTSTRAP_BROKER_STRING_TLS.split(","),
-  retry: {
-    initialRetryTime: 300,
-    retries: 8,
-  },
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-const producer = kafka.producer();
-let connected = false;
-const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2", "beforeExit"];
-
-signalTraps.map((type) => {
-  process.removeListener(type, producer.disconnect);
-});
-
-signalTraps.map((type) => {
-  process.once(type, producer.disconnect);
-});
+import { Kafka } from "kafkajs";
+import { createMechanism } from "@jm18457/kafkajs-msk-iam-authentication-mechanism";
+import { STSClient, AssumeRoleCommand } from "@aws-sdk/client-sts";
 
 class KafkaSourceLib {
   /*
@@ -50,19 +27,17 @@ class KafkaSourceLib {
   }
 
   determineTopicName(streamARN) {
+    const STAGE = process.env.STAGE;
     for (const table of this.tables) {
       if (streamARN.includes(`/${STAGE}-${table}/`)) return this.topic(table);
     }
   }
-
   unmarshall(r) {
     return AWS.DynamoDB.Converter.unmarshall(r, this.unmarshallOptions);
   }
-
   createPayload(record) {
     return this.createDynamoPayload(record);
   }
-
   createDynamoPayload(record) {
     const dynamodb = record.dynamodb;
     const { eventID, eventName } = record;
@@ -78,7 +53,6 @@ class KafkaSourceLib {
       headers: { eventID: eventID, eventName: eventName },
     };
   }
-
   topic(t) {
     if (this.version) {
       return `${this.topicPrefix}.${t}.${this.version}`;
@@ -86,49 +60,87 @@ class KafkaSourceLib {
       return `${this.topicPrefix}.${t}`;
     }
   }
-
   createOutboundEvents(records) {
     let outboundEvents = {};
     for (const record of records) {
       const topicName = this.determineTopicName(
         String(record.eventSourceARN.toString())
       );
-
       const dynamoPayload = this.createPayload(record);
-
       //initialize configuration object keyed to topic for quick lookup
       if (!(outboundEvents[topicName] instanceof Object))
         outboundEvents[topicName] = {
           topic: topicName,
           messages: [],
         };
-
       //add messages to messages array for corresponding topic
       outboundEvents[topicName].messages.push(dynamoPayload);
     }
     return outboundEvents;
   }
-
   async handler(event) {
+    const STAGE = process.env.STAGE;
+    const sasl = await getMechanism("us-east-1", process.env.bigmacRoleArn);
+    const kafka = new Kafka({
+      clientId: `seds-${STAGE}`,
+      brokers: process.env.BOOTSTRAP_BROKER_STRING_TLS.split(","),
+      retry: {
+        initialRetryTime: 300,
+        retries: 8,
+      },
+      ssl: true,
+      sasl: sasl,
+      requestTimeout: 295000,
+    });
+
+    const producer = kafka.producer();
+    let connected = false;
+    const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2", "beforeExit"];
+
+    signalTraps.map((type) => {
+      process.removeListener(type, producer.disconnect);
+    });
+    signalTraps.map((type) => {
+      process.once(type, producer.disconnect);
+    });
+
     if (!connected) {
+      console.log("Attempting connection...");
       await producer.connect();
       connected = true;
     }
     console.log("Raw event", this.stringify(event, true));
-
     if (event.Records) {
       const outboundEvents = this.createOutboundEvents(event.Records);
-
       const topicMessages = Object.values(outboundEvents);
       console.log(
         `Batch configuration: ${this.stringify(topicMessages, true)}`
       );
-
       await producer.sendBatch({ topicMessages });
     }
 
     console.log(`Successfully processed ${event.Records.length} records.`);
   }
 }
-
+async function getMechanism(region, role) {
+  const sts = new STSClient({
+    region,
+  });
+  const crossAccountRoleData = await sts.send(
+    new AssumeRoleCommand({
+      RoleArn: role,
+      RoleSessionName: "LambdaSession",
+      ExternalId: "asdf",
+    })
+  );
+  return createMechanism({
+    region,
+    credentials: {
+      authorizationIdentity: crossAccountRoleData.AssumedRoleUser.AssumeRoleId,
+      accessKeyId: crossAccountRoleData.Credentials.AccessKeyId,
+      secretAccessKey: crossAccountRoleData.Credentials.SecretAccessKey,
+      sessionToken: crossAccountRoleData.Credentials.SessionToken,
+    },
+  });
+}
 export default KafkaSourceLib;
