416fc9f63143c25f28da7ff70339ed0571817127
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2023-07-27 15:55:52 -0400

    MDCT-2762: Bug Fix for Validation in CCW-AD (#1900)

--- services/ui-src/src/measures/2023/OUDAD/validation.ts
+++ services/ui-src/src/measures/2023/OUDAD/validation.ts
@@ -43,11 +43,7 @@ const OUDValidation = (data: FormData) => {
     ),
     ...GV.validateRequiredRadioButtonForCombinedRates(data),
     ...GV.validateAtLeastOneDataSource(data),
-    ...GV.validateEqualCategoryDenominatorsPM(
-      data,
-      PMD.categories,
-      PMD.qualifiers
-    ),
+    ...GV.validateEqualCategoryDenominatorsPM(data, PMD.categories, ageGroups),
     ...GV.omsValidations({
       data,
       qualifiers: PMD.qualifiers,
