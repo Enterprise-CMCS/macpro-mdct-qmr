79cf8e4694e4ef61540792d62b693f0839631a10
brittbujacich <britt.bujacich@gmail.com>
2023-07-28 12:26:38 -0600

    add wait

--- tests/cypress/support/commands/commands.ts
+++ tests/cypress/support/commands/commands.ts
@@ -151,8 +151,8 @@ Cypress.Commands.add("displaysSectionsWhenUserNotReporting", () => {
 
 // helper recursive function to remove added core sets
 const removeCoreSetElements = (kebab: string, coreSetAction: string) => {
-  cy.wait(3000);
   cy.get(kebab).first().click();
+  cy.wait(3000);
   cy.get('[data-cy="Delete"]').first().click({ force: true });
   cy.get('[data-cy="delete-table-item-input"]').type("delete{enter}");
   cy.wait(3000);
@@ -165,6 +165,7 @@ const removeCoreSetElements = (kebab: string, coreSetAction: string) => {
 
 // removes child core set from main page
 Cypress.Commands.add("deleteChildCoreSets", () => {
+  cy.wait(3000);
   cy.get('[data-cy="tableBody"]').then(($tbody) => {
     if ($tbody.find('[data-cy="child-kebab-menu"]').length > 0) {
       removeCoreSetElements(
