3d3935f13391042add42c1a0302f388c7cc2e06e
ailZhou <aileen.zhou@coforma.io>
2023-07-28 11:16:05 -0400

    updated to handle launch darkly flag

--- services/ui-src/src/views/Home/index.tsx
+++ services/ui-src/src/views/Home/index.tsx
@@ -5,9 +5,13 @@ import * as CUI from "@chakra-ui/react";
 import "./index.module.scss";
 import * as QMR from "components";
 import { useUser } from "hooks/authHooks";
+import { useFlags } from "launchdarkly-react-client-sdk";
 
 export function Home() {
   const { userRole, userState } = useUser();
+  const releaseYearByFlag = useFlags()
+    ? config.currentReportingYear
+    : parseInt(config.currentReportingYear) - 1;
   if (
     userRole === UserRoles.HELP ||
     userRole === UserRoles.ADMIN ||
@@ -27,5 +31,5 @@ export function Home() {
       </CUI.Box>
     );
   }
-  return <Navigate to={`/${userState}/${config.currentReportingYear}`} />;
+  return <Navigate to={`/${userState}/${releaseYearByFlag}`} />;
 }
