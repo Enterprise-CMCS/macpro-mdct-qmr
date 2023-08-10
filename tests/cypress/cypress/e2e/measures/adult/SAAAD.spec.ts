a416c5be7b1d63bacde8c554f52cce9b937e38ef
Garrett Rabian <garrett@coforma.io>
2023-08-10 15:47:13 -0400

    update saaad spec

--- tests/cypress/cypress/e2e/measures/adult/SAAAD.spec.ts
+++ tests/cypress/cypress/e2e/measures/adult/SAAAD.spec.ts
@@ -1,7 +1,9 @@
+import { testingYear } from "../../../../support/constants";
+
 describe("Measure: SAA-AD", () => {
   beforeEach(() => {
     cy.login();
-    cy.selectYear("2021");
+    cy.selectYear(testingYear);
     cy.goToAdultMeasures();
     cy.goToMeasure("SAA-AD");
   });
@@ -23,13 +25,13 @@ describe("Measure: SAA-AD", () => {
 
   it("displays the correct calculated rate amount in total", () => {
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("123");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("456");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').type(
+      "123"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').type(
+      "456"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').should(
       "have.value",
       "27.0"
     );
@@ -37,13 +39,13 @@ describe("Measure: SAA-AD", () => {
 
   it("rounds auto-calculated rate to one decimal", () => {
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("25");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("100");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').type(
+      "25"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').type(
+      "100"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').should(
       "have.value",
       "25.0"
     );
@@ -52,32 +54,24 @@ describe("Measure: SAA-AD", () => {
   it("rounds auto-calculated rate up or down as expected", () => {
     // 3/9*100 = 3.333... -> 33.3
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("3");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("9");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').type("3");
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').type(
+      "9"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').should(
       "have.value",
       "33.3"
     );
 
     // 6/9*100 = 66.666... -> 66.7
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("6");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("9");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').type("6");
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').type(
+      "9"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').should(
       "have.value",
       "66.7"
     );
@@ -88,30 +82,22 @@ describe("Measure: SAA-AD", () => {
     cy.get('[data-cy="MeasurementSpecification0"]').click();
     cy.get('[data-cy="DataSource1"]').click();
     cy.enterValidDateRange();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("0");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("456");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "10"
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').type("0");
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').type(
+      "456"
     );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').type("10");
     cy.get('[data-cy="DataSource1"]').click();
     cy.get("#DataSource1-checkbox").uncheck();
     cy.get('[data-cy="Validate Measure"]').click();
-    cy.get(".chakra-alert:nth-of-type(2)").should(
+    cy.get(
+      '[data-cy="Manually entered rate should be 0 if numerator is 0"]'
+    ).should(
       "have.text",
-      "Performance Measure/Other Performance Measure ErrorManually entered rate should be 0 if numerator is 0"
+      "Manually entered rate should be 0 if numerator is 0"
     );
   });
 
@@ -120,30 +106,24 @@ describe("Measure: SAA-AD", () => {
     cy.get('[data-cy="MeasurementSpecification0"]').click();
     cy.get('[data-cy="DataSource1"]').click();
     cy.enterValidDateRange();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("123");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("456");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "0.0"
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.numerator"]').type(
+      "123"
     );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.denominator"]').type(
+      "456"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.bdI1f1.0.rate"]').type("0.0");
     cy.get('[data-cy="DataSource1"]').click();
     cy.get("#DataSource1-checkbox").uncheck();
     cy.get('[data-cy="Validate Measure"]').click();
-    cy.get(".chakra-alert:nth-of-type(2)").should(
+    cy.get(
+      '[data-cy="Rate should not be 0 if numerator and denominator are not 0. If the calculated rate is less than 0.5, disregard this validation."]'
+    ).should(
       "have.text",
-      "Performance Measure/Other Performance Measure ErrorRate should not be 0 if numerator and denominator are not 0. If the calculated rate is less than 0.5, disregard this validation."
+      "Rate should not be 0 if numerator and denominator are not 0. If the calculated rate is less than 0.5, disregard this validation."
     );
   });
 });
