b82b313644b29de0ee07bc95cb90ba27661bfdc0
Garrett Rabian <garrett@coforma.io>
2023-08-10 11:06:38 -0400

    revert

--- tests/cypress/helper.d.ts
+++ tests/cypress/helper.d.ts
@@ -2,6 +2,12 @@
 
 type MeasureList = "OHD-AD";
 declare namespace Cypress {
+  // interface Cypress {
+  //   env(key: "TEST_USER_1"): string;
+  //   env(key: "TEST_USER_2"): string;
+  //   env(key: "TEST_USER_3"): string;
+  //   env(key: "TEST_PASSWORD_1"): string;
+  // }
   interface Chainable {
     // the default stateuser3 is used to login but can also be changed
     // by passing in a user (not including the @test.com) ex. cy.login('bouser')
@@ -24,7 +30,7 @@ declare namespace Cypress {
     goToHealthHomeSetMeasures(): Chainable<Element>;
 
     // Attach file method
-    selectFile(fileName: string, { subjectType: string });
+    attachFile(fileName: string, { subjectType: string });
 
     // Visit Measures based on abbr
     goToMeasure(measure: MeasureList | string): Chainable<Element>;
