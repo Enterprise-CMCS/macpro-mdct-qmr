cc423025940417b07834d12a340326ceaf8c2176
Garrett Rabian <garrett@coforma.io>
2023-08-10 14:34:34 -0400

    update ppcad spec

--- tests/cypress/cypress/e2e/measures/adult/PPCAD.spec.ts
+++ tests/cypress/cypress/e2e/measures/adult/PPCAD.spec.ts
@@ -1,7 +1,9 @@
+import { testingYear } from "../../../../support/constants";
+
 describe("Measure: PPC-AD", () => {
   beforeEach(() => {
     cy.login();
-    cy.selectYear("2021");
+    cy.selectYear(testingYear);
     cy.goToAdultMeasures();
     cy.goToMeasure("PPC-AD");
   });
@@ -12,139 +14,93 @@ describe("Measure: PPC-AD", () => {
   });
 
   it("Ensure Data Source question includes Administrative Data, Hybrid and Other Data Source selections. No warning displayed when N=0, D>0, R>0", () => {
-    // admin data
-    cy.get(
-      '[data-cy="DataSource0"] > .chakra-checkbox__label > .chakra-text'
-    ).should("be.visible");
-
-    // Hybrid data
-    cy.get(
-      '[data-cy="DataSource1"] > .chakra-checkbox__label > .chakra-text'
-    ).should("be.visible");
-
-    // other data source
-    cy.get(
-      '[data-cy="DataSource2"] > .chakra-checkbox__label > .chakra-text'
-    ).should("be.visible");
-
     cy.get('[data-cy="DidReport0"]').click();
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get('[id="DataSource1-checkbox"]').check({ force: true });
-    cy.get('[data-cy="DateRange.startDate-month"]').click({ force: true });
-    cy.get('[data-cy="DateRange.startDate-month"]').type("2");
+    cy.get('[data-cy="MeasurementSpecification-HEDISVersion"]').select(
+      "HEDIS MY 2022"
+    );
+    cy.get('[data-cy="DataSource1"]').click();
+    cy.get(
+      '[data-cy="DataSourceSelections.HybridAdministrativeandMedicalRecordsData0.selected0"]'
+    ).click();
+    cy.get(
+      '[data-cy="DataSourceSelections.HybridAdministrativeandMedicalRecordsData1.selected0"]'
+    ).click();
+    cy.get('[data-cy="DefinitionOfDenominator0"]').click();
+    cy.get('[data-cy="DenominatorDefineTotalTechSpec0"]').click();
 
-    cy.get('[data-cy="DateRange.startDate-year"]').click({ force: true });
-    cy.get('[data-cy="DateRange.startDate-year"]').type("2021");
+    cy.enterValidDateRange();
 
-    cy.get('[data-cy="DateRange.endDate-month"]').click({ force: true });
-    cy.get('[data-cy="DateRange.endDate-month"]').type("5");
-
-    cy.get('[data-cy="DateRange.endDate-year"]').click({ force: true });
-    cy.get('[data-cy="DateRange.endDate-year"]').type("2021");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("0");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("5");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').type("0");
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "5"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "0.0"
     );
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "5"
-    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').type("5");
+
+    cy.get('[data-cy="DeliverySysRepresentationDenominator0"]').click();
+    cy.get('[data-cy="DeliverySys-FeeForService0"]').click();
+
     cy.get('[data-cy="Validate Measure"]').click();
 
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "5"
-    );
-    cy.get('[data-cy="Validate Measure"]').click();
     cy.get(
       '[data-cy="The measure has been validated successfully"] > .chakra-text'
     ).should("have.text", "The measure has been validated successfully");
-    /* ==== End Cypress Studio ==== */
   });
 
   it("Rate calculation should be correct", () => {
     // select is reporting
-    cy.get('[data-cy="DidReport0"]').click({ force: true });
+    cy.get('[data-cy="DidReport0"]').click();
 
     // select AHRQ for measurement specification
     cy.get('[data-cy="MeasurementSpecification0"]').click();
 
-    cy.get('[id="DataSource0-checkbox"]').check({ force: true });
-    cy.get('[id="DataSource1-checkbox"]').uncheck({ force: true });
+    cy.get('[data-cy="DataSource0"]').click();
 
     // Rate calculation should be = (N/D*100)
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("5");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("10");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').type("5");
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "10"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "50.0"
     );
 
     // Ensure that auto calculate rate displays 1 decimal (even if the value is zero)
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("8");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').type("8");
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "80.0"
     );
 
     // Ensure that numerical value after decimal is rounded up/down for auto calculated rate (up)
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("9");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "9"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "88.9"
     );
 
     // Ensure that numerical value after decimal is rounded up/down for auto calculated rate (down)
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("18");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "18"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "44.4"
     );
 
     // Ensure that user cannot manually enter rates if admin data is selected - (already selected)
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.attr",
       "readonly"
     );
@@ -154,66 +110,42 @@ describe("Measure: PPC-AD", () => {
     cy.get('[data-cy="DidReport0"]').click();
     cy.get('[data-cy="DataSource1"]').click();
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("10");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("20");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').type(
+      "10"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "20"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "50.0"
     );
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "not.have.attr",
       "readonly"
     );
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "48.1"
-    );
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').type("48.1");
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "48.1"
     );
   });
 
   it("Ensure that warning appears if N=0, D>0, then R should be = 0 for user entered rates.", () => {
-    cy.wait(1000);
     cy.get('[data-cy="DidReport0"]').click();
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get('[id="DataSource0-checkbox"]').uncheck({ force: true });
-    cy.get('[id="DataSource2-checkbox"]').check({ force: true });
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("0");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("5");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="DataSource2"]').click();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').type("0");
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "5"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "0.0"
     );
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "5"
-    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').type("5");
     cy.get('[data-cy="Validate Measure"]').click();
     cy.get(
       '[data-cy="Manually entered rate should be 0 if numerator is 0"]'
@@ -232,32 +164,17 @@ describe("Measure: PPC-AD", () => {
   it("Ensure that warning appears if N>0, D>0, then R should be >0 for user entered rates.", () => {
     cy.get('[data-cy="DidReport0"]').click();
     cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get('[id="DataSource0-checkbox"]').uncheck({ force: true });
-    cy.get('[id="DataSource1-checkbox"]').check({ force: true });
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear({
-      force: true,
-    });
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("5");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("5");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="DataSource1"]').click();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.numerator"]').type("5");
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.denominator"]').type(
+      "5"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').should(
       "have.value",
       "100.0"
     );
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "0"
-    );
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.SyrrI1.0.rate"]').type("0");
     cy.get('[data-cy="Validate Measure"]').click();
     cy.get(
       '[data-cy="Rate should not be 0 if numerator and denominator are not 0. If the calculated rate is less than 0.5, disregard this validation."]'
@@ -273,71 +190,18 @@ describe("Measure: PPC-AD", () => {
     );
   });
 
-  // If AHRQ is selected (measure specification) each age range for which there are n/d/r values entered are represented in:
-  // - Deviations for measure specifications
-  // - Optional Measure specification
-  it("Age ranges are represented in DMS and OMS", () => {
-    cy.wait(1000);
-    cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get('[data-cy="DataSource0"]').click();
-    cy.get("#DataSource0-checkbox").check();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("2");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("3");
-    cy.get('[data-cy="DidCalculationsDeviate0"]').click();
-    cy.get('[data-cy="DeviationOptions0"] > .chakra-checkbox__control').click();
-    cy.get("#DeviationOptions0-checkbox").check();
-    cy.get(
-      '[data-cy="Deviations.Postpartumvisitbetween7and84days.RateDeviationsSelected0"] > .chakra-checkbox__control'
-    ).click();
-    cy.get(
-      "#Deviations\\.Postpartumvisitbetween7and84days\\.RateDeviationsSelected0-checkbox"
-    ).check();
-    cy.get(
-      '[data-cy="Deviations.Postpartumvisitbetween7and84days.RateDeviationsSelected1"] > .chakra-checkbox__control'
-    ).click();
-    cy.get(
-      "#Deviations\\.Postpartumvisitbetween7and84days\\.RateDeviationsSelected1-checkbox"
-    ).check();
-    cy.get(
-      '[data-cy="Deviations.Postpartumvisitbetween7and84days.RateDeviationsSelected2"] > .chakra-checkbox__control'
-    ).click();
-    cy.get(
-      "#Deviations\\.Postpartumvisitbetween7and84days\\.RateDeviationsSelected2-checkbox"
-    ).check();
-  });
-
   // if Other measure spec is selected each age range/ custom description for which there are n/d/r
   // values entered in other performance measure are presented in:
   //  - Optional Measure specification
   it("Age ranges are represented in OMS when other measure spec", () => {
-    cy.wait(2000);
-    // cy.get('[data-cy="DidReport0"]').click({ force: true });
     cy.get('[data-cy="MeasurementSpecification1"]').click();
-    cy.get('[id="DataSource0-checkbox"]').check({ force: true });
-    cy.get('[id="DataSource1-checkbox"]').uncheck({ force: true });
-    cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.description"]').clear();
+    cy.get('[data-cy="DataSource0"]').click();
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.description"]').type(
       "example 1"
     );
-    cy.get(
-      '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.numerator"]'
-    ).clear();
     cy.get('[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.numerator"]').type(
       "5"
     );
-    cy.get(
-      '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.denominator"]'
-    ).clear();
     cy.get(
       '[data-cy="OtherPerformanceMeasure-Rates.0.rate.0.denominator"]'
     ).type("10");
@@ -345,20 +209,18 @@ describe("Measure: PPC-AD", () => {
       "have.value",
       "50.0"
     );
+    cy.get('[data-cy="OptionalMeasureStratification.options0"]').click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.options0"] > .chakra-checkbox__control'
+      '[data-cy="OptionalMeasureStratification.selections.Race.options0"]'
     ).click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.options0"] > .chakra-checkbox__control'
-    ).click();
-    cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.options0"] > .chakra-checkbox__label > .chakra-text'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.options0"] > .chakra-checkbox__label > .chakra-text'
     ).should("have.text", "example 1");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.options0"] > .chakra-checkbox__control'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.options0"]'
     ).click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.rates.example1.OPM.0.numerator"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.rates.OPM.example1.0.numerator"]'
     ).type("3");
   });
 });
