51630d14c45814da5e68a26d8a3b5b14ae24a1c1
Garrett Rabian <garrett@coforma.io>
2023-08-11 12:33:49 -0400

    update iuhh spec

--- tests/cypress/cypress/e2e/measures/healthhome/IUHH.spec.ts
+++ tests/cypress/cypress/e2e/measures/healthhome/IUHH.spec.ts
@@ -1,7 +1,9 @@
+import { testingYear } from "../../../../support/constants";
+
 describe("Measure: IU-HH", () => {
   beforeEach(() => {
     cy.loginHealthHome();
-    cy.selectYear("2021");
+    cy.selectYear(testingYear);
     cy.goToHealthHomeSetMeasures();
     cy.goToMeasure("IU-HH");
   });
@@ -97,111 +99,102 @@ describe("Measure: IU-HH", () => {
     cy.get("#MeasurementSpecification-CMS").click();
 
     // PM
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.0.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.0.value"]')
       .first()
-      .clear()
       .type("3");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.1.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.1.value"]')
       .first()
-      .clear()
       .type("7");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.3.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.3.value"]')
       .first()
-      .clear()
       .type("1");
 
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.2.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.2.value"]')
       .first()
       .should("have.value", "2333.3");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.4.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.4.value"]')
       .first()
       .should("have.value", "333.3");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.5.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.5.value"]')
       .first()
       .should("have.value", "0.1");
 
     // PM 1st Category Totals
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.0.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.0.value"]')
       .last()
       .should("have.value", "3");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.1.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.1.value"]')
       .last()
       .should("have.value", "7");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.2.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.2.value"]')
       .last()
       .should("have.value", "2333.3");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.3.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.3.value"]')
       .last()
       .should("have.value", "1");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.4.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.4.value"]')
       .last()
       .should("have.value", "333.3");
-    cy.get('[data-cy="PerformanceMeasure.rates.Inpatient.5.value"]')
+    cy.get('[data-cy="PerformanceMeasure.rates.895Rzk.5.value"]')
       .last()
       .should("have.value", "0.1");
 
     // OMS
     cy.get('[data-cy="OptionalMeasureStratification.options0"]').click();
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.options0"]'
-    ).click();
-    cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.options0"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.options0"]'
     ).click();
 
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Ages0to17.Inpatient.0.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.0.value"]'
     )
       .first()
-      .clear()
       .type("3");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Ages0to17.Inpatient.1.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.1.value"]'
     )
       .first()
-      .clear()
       .type("7");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Ages0to17.Inpatient.3.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.3.value"]'
     )
       .first()
-      .clear()
       .type("1");
 
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Ages0to17.Inpatient.2.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.2.value"]'
     )
       .first()
       .should("have.value", "2333.3");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Ages0to17.Inpatient.4.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.4.value"]'
     )
       .first()
       .should("have.value", "333.3");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Ages0to17.Inpatient.5.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.5.value"]'
     )
       .first()
       .should("have.value", "0.1");
 
     // OMS 1st Category Totals
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Total.Inpatient.0.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.0.value"]'
     ).should("have.value", "3");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Total.Inpatient.1.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.1.value"]'
     ).should("have.value", "7");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Total.Inpatient.2.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.2.value"]'
     ).should("have.value", "2333.3");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Total.Inpatient.3.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.3.value"]'
     ).should("have.value", "1");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Total.Inpatient.4.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.4.value"]'
     ).should("have.value", "333.3");
     cy.get(
-      '[data-cy="OptionalMeasureStratification.selections.RaceNonHispanic.selections.White.rateData.iuhh-rate.rates.Total.Inpatient.5.value"]'
+      '[data-cy="OptionalMeasureStratification.selections.Race.selections.AmericanIndianorAlaskaNative.rateData.iuhh-rate.rates.895Rzk.Total.5.value"]'
     ).should("have.value", "0.1");
 
     // validation errors
