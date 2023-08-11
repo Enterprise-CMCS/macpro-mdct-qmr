34091da4110691071d87943344e22bdbb631cb4f
Garrett <57802560+gmrabian@users.noreply.github.com>
2023-08-11 15:26:51 -0400

    Mdct 2554 cy2023 2 (#1929)
    Co-authored-by: Garrett Rabian <garrett@coforma.io>

--- tests/cypress/cypress/e2e/measures/adult/BCSAD.spec.ts
+++ tests/cypress/cypress/e2e/measures/adult/BCSAD.spec.ts
@@ -1,7 +1,9 @@
+import { testingYear } from "../../../../support/constants";
+
 describe("Measure: BCS-AD", () => {
   beforeEach(() => {
     cy.login();
-    cy.selectYear("2021");
+    cy.selectYear(testingYear);
     cy.goToAdultMeasures();
     cy.goToMeasure("BCS-AD");
   });
@@ -55,65 +57,32 @@ describe("Measure: BCS-AD", () => {
   it("if only admin data cannot override, if anything else, rate is editable", () => {
     cy.get('[data-cy="MeasurementSpecification0"]').click();
     cy.get('[data-cy="DataSource0"] > .chakra-checkbox__control').click();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.rate"]').should(
       "have.attr",
       "readonly"
     );
     cy.get('[data-cy="DataSource2"] > .chakra-checkbox__control').click();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.rate"]').should(
       "not.have.attr",
       "readonly"
     );
   });
 
-  it("should have adult eligibility group in OMS", () => {
-    cy.get('[data-cy="MeasurementSpecification0"]').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("4");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("3");
-    cy.get(
-      '[data-cy="Numerator: 4 cannot be greater than Denominator: 3"] > .chakra-text'
-    ).should("have.text", "Numerator: 4 cannot be greater than Denominator: 3");
-    cy.get(
-      '[data-cy="OptionalMeasureStratification.options5"] > .chakra-checkbox__label > .chakra-text'
-    ).should("have.text", "Adult Eligibility Group (ACA Expansion Group)");
-    cy.get('[data-cy="Validate Measure"]').click();
-    cy.get(
-      '[data-cy="Numerators must be less than Denominators for all applicable performance measures"] > .chakra-text'
-    ).should(
-      "have.text",
-      "Numerators must be less than Denominators for all applicable performance measures"
-    );
-  });
-
   it("Rates calculate correctly", () => {
     cy.get("#MeasurementSpecification-NCQAHEDIS").click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("5");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.denominator"]'
-    ).type("6");
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').should(
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.numerator"]').type("5");
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.denominator"]').type(
+      "6"
+    );
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.rate"]').should(
       "have.value",
       "83.3"
     );
     cy.get('[data-cy="DataSource2"] > .chakra-checkbox__control').click();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).clear();
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.numerator"]'
-    ).type("0");
-    cy.get(
-      '[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]'
-    ).clear();
-    cy.get('[data-cy="PerformanceMeasure.rates.singleCategory.0.rate"]').type(
-      "20"
-    );
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.numerator"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.numerator"]').type("0");
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.rate"]').clear();
+    cy.get('[data-cy="PerformanceMeasure.rates.xacc8a.0.rate"]').type("20");
     cy.get('[data-cy="Validate Measure"]').click();
     cy.get(
       '[data-cy="Manually entered rate should be 0 if numerator is 0"] > .chakra-text'
