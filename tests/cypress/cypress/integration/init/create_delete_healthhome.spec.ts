56baee5534ce4b6e7d2b8f08bfe26548a108de92
ailZhou <aileen.zhou@coforma.io>
2023-08-15 12:11:11 -0400

    test

---
+++ tests/cypress/cypress/integration/init/create_delete_healthhome.spec.ts
@@ -0,0 +1,33 @@
+describe("Health Home Sets Should be able to be deleted and created", () => {
+  beforeEach(() => {
+    cy.loginHealthHome();
+    cy.selectYear("2021");
+    cy.wait(2000);
+  });
+
+  // recursivley makes as many HHCS as possible
+  const addAllHHCoreSet = () => {
+    addSingleHHCoreSet();
+    cy.wait(2000);
+    cy.get('[data-cy="add-hhbutton"]').then(($button) => {
+      if (!$button.is(":disabled")) {
+        addAllHHCoreSet();
+      } else {
+        cy.get('[data-cy="add-hhbutton"]').should("be.disabled");
+      }
+    });
+  };
+
+  // adds first available HH core set
+  const addSingleHHCoreSet = () => {
+    cy.get('[data-cy="add-hhbutton"]').click(); // clicking on adding child core set measures
+    cy.get('[data-cy="HealthHomeCoreSet-SPA"]').select(1); // select first available SPA
+    cy.get('[data-cy="Create"]').click(); //clicking create
+  };
+
+  // NOTE: should recursively loop through all possible SPA creations
+  it("Creates Multiple SPA sets", () => {
+    cy.deleteHealthHomeSets();
+    addAllHHCoreSet();
+  });
+});
