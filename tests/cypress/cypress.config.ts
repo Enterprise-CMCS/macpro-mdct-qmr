d812edb52cbad2fb5a44b8b1bccfdfb7f7389e28
Garrett Rabian <garrett@coforma.io>
2023-09-25 15:58:05 -0400

    remove default settings, set retry to 0, move setupNodeEvents to config

--- tests/cypress/cypress.config.ts
+++ tests/cypress/cypress.config.ts
@@ -1,18 +1,33 @@
 import { defineConfig } from "cypress";
-import setupNodeEvents from "./plugins";
 
 export default defineConfig({
-  redirectionLimit: 20,
-  retries: 2,
-  watchForFileChanges: true,
   fixturesFolder: "fixtures",
   screenshotsFolder: "screenshots",
-  video: false,
   videosFolder: "videos",
   downloadsFolder: "downloads",
-  defaultCommandTimeout: 15000,
   e2e: {
-    setupNodeEvents,
+    setupNodeEvents(on, config) {
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.name === "chrome" && browser.isHeadless) {
+          launchOptions.args.push("--window-size=1400,9000");
+          launchOptions.args.push("--force-device-scale-factor=1");
+        }
+        return launchOptions;
+      });
+
+      on("task", {
+        log(message) {
+          console.log(message);
+
+          return null;
+        },
+        table(message) {
+          console.table(message);
+
+          return null;
+        },
+      });
+    },
     baseUrl: "http://localhost:3000/",
     specPattern: "cypress/e2e/**/*.spec.ts",
     supportFile: "support/index.js",
