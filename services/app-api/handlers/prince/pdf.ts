38f2889d1ef38d7415cd0b67f4240d88bca5264e
benmartin-coforma <Ben.Martin@coforma.io>
2023-12-18 15:43:07 -0700

    Attempt to switch Prince PDF call from axios to fetch

--- services/app-api/handlers/prince/pdf.ts
+++ services/app-api/handlers/prince/pdf.ts
@@ -1,23 +1,62 @@
 import handler from "../../libs/handler-lib";
-import { aws4Interceptor } from "aws4-axios";
-import axios from "axios";
 import { StatusCodes } from "../../utils/constants/constants";
+import { URL } from "url";
+import { SignatureV4 } from "@smithy/signature-v4";
+import { Sha256 } from "@aws-crypto/sha256-js";
+import { fetch } from "cross-fetch"; // TODO remove this polyfill once QMR is on Node 18+
 
 export const getPDF = handler(async (event, _context) => {
-  const interceptor = aws4Interceptor({
-    region: "us-east-1",
+  const body = event.body; // will be base64-encoded HTML, like "PGh0bWw..."
+  if (!body) {
+    throw new Error("Missing request body");
+  }
+
+  const {
+    // princeApiHost: hostname, // JUST the host name, no protocol, ex: "my-site.cms.gov"
+    // princeApiPath: path, // Needs leading slash, ex: "/doc-conv/508html-to-508pdf"
+    princeUrl,
+    AWS_ACCESS_KEY_ID: accessKeyId,
+    AWS_SECRET_ACCESS_KEY: secretAccessKey,
+    AWS_SESSION_TOKEN: sessionToken,
+  } = process.env;
+
+  if (
+    princeUrl === undefined ||
+    accessKeyId === undefined ||
+    secretAccessKey === undefined ||
+    sessionToken === undefined
+  ) {
+    throw new Error("No config found to make request to PDF API");
+  }
+
+  const { hostname, pathname: path } = new URL(princeUrl);
+
+  const request = {
+    method: "POST",
+    protocol: "https",
+    hostname,
+    path,
+    headers: {
+      host: hostname, // Prince requires this to be signed
+    },
+    body,
+  };
+
+  const signer = new SignatureV4({
     service: "execute-api",
+    region: "us-east-1",
+    credentials: { accessKeyId, secretAccessKey, sessionToken },
+    sha256: Sha256,
   });
 
-  axios.interceptors.request.use(interceptor);
+  const signedRequest = await signer.sign(request);
 
-  try {
-    const pdf = await axios.post(process.env.princeUrl!, event.body!);
-    return {
-      status: StatusCodes.SUCCESS,
-      body: pdf.data,
-    };
-  } catch (err) {
-    console.log(err);
-  }
+  const response = await fetch(`https://${hostname}${path}`);
+
+  const base64EncodedPdfData = await response.json();
+
+  return {
+    status: StatusCodes.SUCCESS,
+    body: base64EncodedPdfData,
+  };
 });
