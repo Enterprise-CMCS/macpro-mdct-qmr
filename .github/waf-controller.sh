508029c171c5ea1df383034e8bddf299aa1fbc4c
Berry Davenport <berry.g.davenport.iii@gmail.com>
2024-01-09 14:40:43 -0500

    I'm just a test

--- .github/waf-controller.sh
+++ .github/waf-controller.sh
@@ -11,7 +11,7 @@ NAME="${1}"
 ID="${2}"
 RUNNER_IP="${3}/32"
 
-[[ $DEBUG -le 1 ]] && echo "Inputs:  NAME ${NAME}, ID ${ID}, RUNNER_IP ${RUNNER_IP}"
+[[ $DEBUG -ge 1 ]] && echo "Inputs:  NAME ${NAME}, ID ${ID}, RUNNER_IP ${RUNNER_IP}"
 
 #Exponential backoff with jitter
 jitter() {
@@ -30,8 +30,8 @@ for ((i=1; i <= $CIRCUIT_BREAKER; i++)); do
   for ((j=1; j <= $CIRCUIT_BREAKER; j++)); do
     WAF_CONFIG=$(aws wafv2 get-ip-set --scope CLOUDFRONT --id ${ID} --name ${NAME})
     CMD_CD=$?
-    [[ $DEBUG -le 1 ]] && echo "AWS CLI Read Response Code:  ${CMD_CD}"
-    [[ $DEBUG -le 2 ]] && echo "AWS CLI Read Response:  ${WAF_CONFIG}"
+    [[ $DEBUG -ge 1 ]] && echo "AWS CLI Read Response Code:  ${CMD_CD}"
+    [[ $DEBUG -ge 2 ]] && echo "AWS CLI Read Response:  ${WAF_CONFIG}"
 
     #If the retries exceeded error code is returned, try again, otherwise exit the loop
     [[ $CMD_CD -eq $AWS_RETRY_ERROR ]] || break
@@ -42,35 +42,36 @@ for ((i=1; i <= $CIRCUIT_BREAKER; i++)); do
     sleep ${SLEEP_FOR}
   done
 
-  #The loop was short circuited with an error code other than 0, so something is wrong
-  [[ $CMD_CD -eq 0 ]] || echo "An unexpected read error occurred:  ${CMD_CD}" && exit 2
-
   #Unable to get the lock tocken and IP set so there isn't any point in attempting the write op
   [[ $j -ge $CIRCUIT_BREAKER ]] && echo “Attempts to read WAF IPSet exceeded” && sleep $(jitter ${i}) && continue
 
+  #The loop was short circuited with an error code other than 0, so something is wrong
+  [[ $CMD_CD -eq 0 ]] || echo "An unexpected read error occurred:  ${CMD_CD}" && exit 2
+
   echo "Read was successful."
 
+  #Parse out IP set addresses to array
   IP_ADDRESSES=($(jq -r '.IPSet.Addresses | .[]' <<< ${WAF_CONFIG}))
 
   #This really can't happen because each node in the matrix gets a unique IP
   [[ -n "${IP_ADDRESSES[$RUNNER_IP]}" ]] && echo "IP is present in IP Set." && exit 0
 
-  #Add runner IP to set
+  #Add runner IP to array
   IP_ADDRESSES+=("$RUNNER_IP")
 
   #Stringify IPs
   STRINGIFIED=$(echo $(IFS=" " ; echo "${IP_ADDRESSES[*]}"))
-  [[ $DEBUG -le 2 ]] && echo "Ip Addresses:  ${STRINGIFIED}"
+  [[ $DEBUG -ge 2 ]] && echo "Ip Addresses:  ${STRINGIFIED}"
 
-  #Extract optimistic concurrency control token
+  #Parse out optimistic concurrency control token
   OCC_TOKEN=$(jq -r '.LockToken' <<< ${WAF_CONFIG})
-  [[ $DEBUG -le 2 ]] && echo "LockToken:  ${OCC_TOKEN}"
+  [[ $DEBUG -ge 2 ]] && echo "LockToken:  ${OCC_TOKEN}"
 
   for ((k=1; k <= $CIRCUIT_BREAKER; k++)); do
     OUTPUT=$(aws wafv2 update-ip-set --scope CLOUDFRONT --id ${ID} --name ${NAME} --lock-token ${OCC_TOKEN} --addresses ${STRINGIFIED})
     CMD_CD=$?
-    [[ $DEBUG -le 1 ]] && echo "AWS CLI Write Response Code:  ${CMD_CD}"
-    [[ $DEBUG -le 2 ]] && echo "AWS CLI Write Response:  ${OUTPUT}"
+    [[ $DEBUG -ge 1 ]] && echo "AWS CLI Write Response Code:  ${CMD_CD}"
+    [[ $DEBUG -ge 2 ]] && echo "AWS CLI Write Response:  ${OUTPUT}"
 
     #If the retries exceeded error code is returned, try again, otherwise exit the loop
     [[ $CMD_CD -eq $AWS_RETRY_ERROR ]] || break
@@ -91,7 +92,8 @@ for ((i=1; i <= $CIRCUIT_BREAKER; i++)); do
   sleep ${SLEEP_FOR}
 done
 
-[[ $DEBUG -le 1 ]] && echo "Attempts to update ip set:  $i"
+[[ $DEBUG -ge 1 ]] && echo "Attempts to update ip set:  $i"
+
 [[ $i -ge $CIRCUIT_BREAKER ]] && echo “Attempts to update WAF IPSet exceeded, exiting.” && exit 2
 
 echo "Applied the IP successfully."
