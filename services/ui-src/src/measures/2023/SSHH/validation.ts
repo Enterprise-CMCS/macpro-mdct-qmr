008fc1ca0f2c4b5cb1073ebbfcb72cf9807839f7
ailZhou <aileen.zhou@coforma.io>
2024-02-15 15:52:08 -0500

    remove type file using DefaultFormData (2023)

--- services/ui-src/src/measures/2023/SSHH/validation.ts
+++ services/ui-src/src/measures/2023/SSHH/validation.ts
@@ -1,6 +1,7 @@
 import * as DC from "dataConstants";
 import * as GV from "measures/2023/shared/globalValidations";
-import { FormData } from "./types";
+//form type
+import { DefaultFormData as FormData } from "measures/2023/shared/CommonQuestions/types";
 
 export interface FormRateField {
   denominator?: string;
