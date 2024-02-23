2df60f539a7509519104a8e712c7ac851632bf54
ailZhou <aileen.zhou@coforma.io>
2024-02-23 13:10:43 -0500

    prettier fix

--- services/ui-src/src/labels/2024/commonQuestionsLabel.tsx
+++ services/ui-src/src/labels/2024/commonQuestionsLabel.tsx
@@ -1,9 +1,11 @@
 export const commonQuestionsLabel = {
-    AdditonalNotes:{
-        header: "Additional Notes/Comments on the measure (optional)",
-        section: "Please add any additional notes or comments on the measure not otherwise captured above (<em>text in this field is included in publicly-reported state-specific comments</em>):",
-        upload: "If you need additional space to include comments or supplemental information, please attach further documentation below."
-    }
-}
+  AdditonalNotes: {
+    header: "Additional Notes/Comments on the measure (optional)",
+    section:
+      "Please add any additional notes or comments on the measure not otherwise captured above (<em>text in this field is included in publicly-reported state-specific comments</em>):",
+    upload:
+      "If you need additional space to include comments or supplemental information, please attach further documentation below.",
+  },
+};
 
 export default commonQuestionsLabel;
