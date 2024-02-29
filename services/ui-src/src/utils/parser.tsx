440e6bc9320ff808bdb8d2ae0ec1e92aa98d3a09
ailZhou <aileen.zhou@coforma.io>
2024-02-29 16:09:47 -0500

    update parser to be recursive

--- services/ui-src/src/utils/parser.tsx
+++ services/ui-src/src/utils/parser.tsx
@@ -1,29 +1,49 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
-//convert labels with html tags to JSX elements to be rendered
+//creating a react element from html elements
+const reactElement = (element: Element, reactElements?: any) => {
+  //create a react element with a random key to prevent browser warning
+  return React.createElement(
+    element.tagName.toLowerCase(),
+    { key: uuidv4() },
+    reactElements ?? element.innerHTML
+  );
+};
+
+//recusively look through html elements and convert any tags to react elements
+const convertToReactElement = (element: Element) => {
+  if (
+    element.childNodes.length === 1 &&
+    element.childNodes[0] instanceof Text
+  ) {
+    return reactElement(element);
+  } else {
+    let reactElements: any = [];
+    element.childNodes.forEach((node: ChildNode) => {
+      reactElements.push(convertToReactElement(node as Element));
+    });
+    return reactElement(element, reactElements);
+  }
+};
+
+//convert labels with html tags to JSX elements to be rendered, doesn't work with <br> tags
 export const parseLabelToHTML = (label: string) => {
   const parseLabel = new DOMParser().parseFromString(label, "text/html").body;
-
   let renderArr: (
     | string
     | React.DOMElement<React.DOMAttributes<Element>, Element>
     | null
   )[] = [];
   parseLabel.childNodes.forEach((node: ChildNode) => {
-    //if the node has a nodeValue, it is probably a string and we can just push the value to the array
-    if (node.nodeValue) {
-      renderArr.push((node as Element).nodeValue);
+    if (node instanceof Text) {
+      renderArr.push(node.textContent);
+    } else if (node instanceof Element) {
+      //convert any tags and inner tags into react elements
+      renderArr.push(convertToReactElement(node));
     } else {
-      //if this is no nodeValue, its a tag so retrieve the tag name
-      const tag = (node as Element).tagName.toLowerCase();
-      //create a react element with a random key to prevent browser warning
-      const reactElement = React.createElement(
-        tag,
-        { key: uuidv4() },
-        node.textContent
-      );
-      renderArr.push(reactElement);
+      // throw new Error("Unrecognized node type in label:\n" + label);
+      throw new Error("Unrecognized node type in label:\n" + label);
     }
   });
   return <>{renderArr}</>;
