067a3b0513cee00df7bad67c15f50b2b325f2aeb
brittbujacich <britt.bujacich@gmail.com>
2024-03-13 10:50:52 -0600

    text changes

--- services/ui-src/src/measures/2024/rateLabelText.ts
+++ services/ui-src/src/measures/2024/rateLabelText.ts
@@ -614,18 +614,18 @@ export const data = {
     "COL-HH": {
         "qualifiers": [
             {
-                "label": "Ages 46 to 49",
-                "text": "Ages 46 to 49",
+                "label": "Ages 46 to 50",
+                "text": "Ages 46 to 50",
                 "id": "RHlNkr"
             },
             {
-                "label": "Ages 50 to 64",
-                "text": "Ages 50 to 64",
+                "label": "Ages 51 to 65",
+                "text": "Ages 51 to 65",
                 "id": "u1n5QA"
             },
             {
-                "label": "Ages 65 to 75",
-                "text": "Ages 65 to 75",
+                "label": "Ages 66 to 75",
+                "text": "Ages 66 to 75",
                 "id": "2ATODe"
             }
         ],
