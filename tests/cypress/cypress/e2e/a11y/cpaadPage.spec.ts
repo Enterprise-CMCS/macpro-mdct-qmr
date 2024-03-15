8a29f1d58d4f305a91b15b454ebd115e1ad63cd9
brittbujacich <britt.bujacich@gmail.com>
2024-03-15 08:13:57 -0600

    autocomplete CPA-AD for 2024

--- tests/cypress/cypress/e2e/a11y/cpaadPage.spec.ts
+++ tests/cypress/cypress/e2e/a11y/cpaadPage.spec.ts
@@ -3,7 +3,7 @@ import { testingYear } from "../../../support/constants";
 describe("CPA-AD Page 508 Compliance Test", () => {
   it("Check a11y on CPAAD Page", () => {
     cy.login();
-    cy.selectYear(testingYear);
+    cy.selectYear("2023");
     cy.goToAdultMeasures();
     cy.goToMeasure("CPA-AD");
     cy.checkA11yOfPage();
