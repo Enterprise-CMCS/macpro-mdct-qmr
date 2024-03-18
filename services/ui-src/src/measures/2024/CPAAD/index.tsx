a00dbd9cd4e369677392ce9c442cd649c87f907e
brittbujacich <britt.bujacich@gmail.com>
2024-03-18 11:51:21 -0600

    text fix

--- services/ui-src/src/measures/2024/CPAAD/index.tsx
+++ services/ui-src/src/measures/2024/CPAAD/index.tsx
@@ -8,7 +8,7 @@ export const CPAAD = ({ name, year }: Props) => {
   return (
     <QMR.AutocompletedMeasureTemplate
       year={year}
-      measureTitle={`CAP-AD - ${name}`}
+      measureTitle={`CPA-AD - ${name}`}
       performanceMeasureText="This measure provides information on the experiences of beneficiaries with their health care and gives a general indication of how well the health care meets the beneficiaries’ expectations. Results summarize beneficiaries’ experiences through ratings, composites, and question summary rates."
       performanceMeasureSubtext="To reduce state burden and streamline reporting, CMS will calculate state-level performance results for this measure using data submitted for the state to the AHRQ CAHPS Health Plan Survey Database."
     />
