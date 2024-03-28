e2659dfedc7796a8675559cd4effc2b7b3f0e7ef
ailZhou <aileen.zhou@coforma.io>
2024-03-28 09:51:23 -0400

    add useQual to rates

--- services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/util.tsx
+++ services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/util.tsx
@@ -1,5 +1,6 @@
 import { LabelData, cleanString } from "utils";
 
+//this function is to convert legacy data structure that's used for 2021 & 2022 data to work with year >= 2023 components
 export const stringToLabelData = (arr: string[] | LabelData[]): LabelData[] => {
   if (typeof arr[0] === "string") {
     (arr as string[]).map((qual: string) => {
