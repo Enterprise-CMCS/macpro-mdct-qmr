ed1b25d568de2dbb87d5f254dc36bb7b1c3fecec
ailZhou <aileen.zhou@coforma.io>
2024-04-03 14:50:39 -0400

    remove logs and fix file location

--- services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/ndrSets.tsx
+++ services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/ndrSets.tsx
@@ -130,7 +130,6 @@ const OPMNDRSets = ({ name }: NdrProps) => {
 export const NDRSets = ({ name }: NdrProps) => {
   const { OPM, componentFlag } = usePerformanceMeasureContext();
   const children: JSX.Element[] = [];
-  console.log("NDRSets");
 
   if (OPM) children.push(<OPMNDRSets name={name} key={name} />);
   switch (componentFlag) {
