1994d874a6dd9dbc3d3f3d7ee2210d88b405071c
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2024-04-04 12:13:16 -0400

    Remove cypress a11y tests; ensure unit a11y tests check entire render (#2159)

--- services/ui-src/src/views/Home/index.test.tsx
+++ services/ui-src/src/views/Home/index.test.tsx
@@ -1,16 +1,27 @@
 import { Home } from "./index";
-import { render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
 import { RouterWrappedComp } from "utils/testing";
+import { axe, toHaveNoViolations } from "jest-axe";
+expect.extend(toHaveNoViolations);
+
+const component = (
+  <RouterWrappedComp>
+    <Home />
+  </RouterWrappedComp>
+);
 
 describe("Test Home.tsx", () => {
-  test("Check that the Home renders", () => {
-    const result = render(
-      <RouterWrappedComp>
-        <Home />
-      </RouterWrappedComp>
-    );
-
-    const homeContainer = result.getByTestId("Home-Container");
+  test("Check that the Home renders with no state user", () => {
+    render(component);
+    const homeContainer = screen.getByTestId("Home-Container");
     expect(homeContainer).toBeVisible();
+  });
+
+  test("should pass a11y tests for no user page", async () => {
+    await act(async () => {
+      const { container } = render(component);
+      expect(await axe(container)).toHaveNoViolations();
+    });
   });
 });
--- tests/cypress/cypress/e2e/a11y/ppcadPage.spec.ts
+++
@@ -1,9 +0,0 @@
-describe("PPC-AD Page 508 Compliance Test", () => {
-  it("Check a11y on PPCAD Page", () => {
-    cy.login();
-    cy.selectYear("2023");
-    cy.goToAdultMeasures();
-    cy.goToMeasure("PPC-AD");
-    cy.checkA11yOfPage();
-  });
-});
