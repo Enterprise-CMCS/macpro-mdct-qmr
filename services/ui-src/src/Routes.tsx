9207993f0dd6d06607beb1085f70b52557a350ef
brittbujacich <britt.bujacich@gmail.com>
2024-04-12 11:40:31 -0600

    wip

--- services/ui-src/src/Routes.tsx
+++ services/ui-src/src/Routes.tsx
@@ -33,6 +33,11 @@ const AddChildCoreSet = lazy(() =>
     default: module.AddChildCoreSet,
   }))
 );
+const AddAdultCoreSet = lazy(() =>
+  import("views/AddAdultCoreSet").then((module) => ({
+    default: module.AddAdultCoreSet,
+  }))
+);
 const AddStateSpecificMeasure = lazy(() =>
   import("views/AddStateSpecificMeasure").then((module) => ({
     default: module.AddStateSpecificMeasure,
