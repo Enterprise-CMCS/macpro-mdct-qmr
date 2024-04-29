bc607acc178fd0c3e2986c0287d31ec835152132
ailZhou <127151429+ailZhou@users.noreply.github.com>
2024-04-29 10:30:07 -0400

    Migrate type from String[] to LabelData[] (2021) (#2194)

--- services/ui-src/src/measures/2021/CommonQuestions/PerformanceMeasure/data.ts
+++ services/ui-src/src/measures/2021/CommonQuestions/PerformanceMeasure/data.ts
@@ -1,8 +1,9 @@
 import { ndrFormula } from "types";
+import { LabelData } from "utils";
 
 export interface PerformanceMeasureData {
-  qualifiers?: string[]; // age ranges, etc
-  categories?: string[]; //performance measure descriptions
+  qualifiers?: LabelData[]; // age ranges, etc
+  categories?: LabelData[]; //performance measure descriptions
   measureName?: string;
   inputFieldNames?: string[];
   ndrFormulas?: ndrFormula[];
@@ -23,15 +24,58 @@ export const exampleData: PerformanceMeasureData = {
     "Initiation of AOD Treatment: Percentage of beneficiaries who initiate treatment through an inpatient AOD admission, outpatient visit, intensive outpatient encounter, or partial hospitalization, telehealth, or medication assisted treatment within 14 days of the diagnosis.",
     "Engagement of AOD Treatment: Percentage of beneficiaries who initiated treatment and who were engaged in ongoing AOD treatment within 34 days of the initiation visit.",
   ],
-  qualifiers: ["Ages 18 to 64", "Age 65 and older"],
+  qualifiers: [
+    {
+      label: "Ages 18 to 64",
+      text: "Ages 18 to 64",
+      id: "Ages18to64",
+    },
+    {
+      label: "Age 65 and older",
+      text: "Age 65 and older",
+      id: "Age65andolder",
+    },
+  ],
   categories: [
-    "Initiation of AOD Treatment: Alcohol Abuse or Dependence",
-    "Engagement of AOD Treatment: Alcohol Abuse or Dependence",
-    "Initiation of AOD Treatment: Opioid Abuse or Dependence",
-    "Engagement of AOD Treatment: Opioid Abuse or Dependence",
-    "Initiation of AOD Treatment: Other Drug Abuse or Dependence",
-    "Engagement of AOD Treatment: Other Drug Abuse or Dependence",
-    "Initiation of AOD Treatment: Total AOD Abuse or Dependence",
-    "Engagement of AOD Treatment: Total AOD Abuse or Dependence",
+    {
+      label: "Initiation of AOD Treatment: Alcohol Abuse or Dependence",
+      text: "Initiation of AOD Treatment: Alcohol Abuse or Dependence",
+      id: "InitiationofAODTreatmentAlcoholAbuseorDependence",
+    },
+    {
+      label: "Engagement of AOD Treatment: Alcohol Abuse or Dependence",
+      text: "Engagement of AOD Treatment: Alcohol Abuse or Dependence",
+      id: "EngagementofAODTreatmentAlcoholAbuseorDependence",
+    },
+    {
+      label: "Initiation of AOD Treatment: Opioid Abuse or Dependence",
+      text: "Initiation of AOD Treatment: Opioid Abuse or Dependence",
+      id: "InitiationofAODTreatmentOpioidAbuseorDependence",
+    },
+    {
+      label: "Engagement of AOD Treatment: Opioid Abuse or Dependence",
+      text: "Engagement of AOD Treatment: Opioid Abuse or Dependence",
+      id: "EngagementofAODTreatmentOpioidAbuseorDependence",
+    },
+    {
+      label: "Initiation of AOD Treatment: Other Drug Abuse or Dependence",
+      text: "Initiation of AOD Treatment: Other Drug Abuse or Dependence",
+      id: "InitiationofAODTreatmentOtherDrugAbuseorDependence",
+    },
+    {
+      label: "Engagement of AOD Treatment: Other Drug Abuse or Dependence",
+      text: "Engagement of AOD Treatment: Other Drug Abuse or Dependence",
+      id: "EngagementofAODTreatmentOtherDrugAbuseorDependence",
+    },
+    {
+      label: "Initiation of AOD Treatment: Total AOD Abuse or Dependence",
+      text: "Initiation of AOD Treatment: Total AOD Abuse or Dependence",
+      id: "InitiationofAODTreatmentTotalAODAbuseorDependence",
+    },
+    {
+      label: "Engagement of AOD Treatment: Total AOD Abuse or Dependence",
+      text: "Engagement of AOD Treatment: Total AOD Abuse or Dependence",
+      id: "EngagementofAODTreatmentTotalAODAbuseorDependence",
+    },
   ],
 };
