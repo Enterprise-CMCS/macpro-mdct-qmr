4a3f5b3c6c6ce808bbc86584ba4b83b4f557a42c
Berry Davenport <berry.g.davenport.iii@gmail.com>
2024-04-29 13:10:45 -0400

    Break another test

--- tests/cypress/cypress/e2e/userflow_child.spec.ts
+++ tests/cypress/cypress/e2e/userflow_child.spec.ts
@@ -66,7 +66,7 @@ describe("submit coreset", () => {
     // confirm reset
     cy.get('[data-cy="Status-WY2024CCS"]').should(
       "contain.text",
-      "in progress3 of 27 complete"
+      "in progress3 of 26 complete"
     );
   });
   it("submit and confirm submission", () => {
