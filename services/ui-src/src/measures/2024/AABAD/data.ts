584a6e1b51dcd737378f39e781612fb536c36193
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2024-05-03 11:42:01 -0400

    AAB-AD 2024 content change (#2208)

--- services/ui-src/src/measures/2024/AABAD/data.ts
+++ services/ui-src/src/measures/2024/AABAD/data.ts
@@ -7,7 +7,7 @@ export const data: DataDrivenTypes.PerformanceMeasure = {
   customPrompt:
     "Enter a number for the numerator and the denominator. The measure is reported as an inverted rate. The formula for the Rate = (1 - (Numerator/Denominator)) x 100",
   questionText: [
-    "The percentage of episodes for beneficiaries age 18 and older with a diagnosis of acute bronchitis/bronchiolitis that did not result in an antibiotic dispensing event.",
+    "Percentage of episodes for beneficiaries age 18 and older with a diagnosis of acute bronchitis/bronchiolitis that did not result in an antibiotic dispensing event.",
   ],
   questionListItems: [],
   categories,
