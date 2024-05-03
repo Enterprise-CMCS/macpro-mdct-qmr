49968df92566398b81a0b44ecb1154868639048a
brittbujacich <britt.bujacich@gmail.com>
2024-05-03 15:17:11 -0600

    test

--- tests/cypress/cypress/e2e/userflow_adult.spec.ts
+++ tests/cypress/cypress/e2e/userflow_adult.spec.ts
@@ -16,6 +16,7 @@ describe(`Adult Core Sets should be able to be created for ${testingYear}`, () =
     cy.get('[data-cy="add-adultbutton"]').click(); // clicking on adding adult core set measures
     cy.get("#AdultCoreSet-ReportType-combined").click(); //selecting combined core set
     cy.get('[data-cy="Create"]').click(); //clicking create
+    cy.wait(500);
     cy.get('[data-cy="ACS"]').should("contain.text", "Adult Core Set Measures");
   });
 });
