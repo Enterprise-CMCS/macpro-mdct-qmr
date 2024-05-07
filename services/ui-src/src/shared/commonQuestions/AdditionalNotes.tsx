85afff0392aa3c6c6ae4b673abffb0fb3af6005e
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2024-05-07 13:46:24 -0400

    Update additional notes text area label logic (#2216)

--- services/ui-src/src/shared/commonQuestions/AdditionalNotes.tsx
+++ services/ui-src/src/shared/commonQuestions/AdditionalNotes.tsx
@@ -5,36 +5,38 @@ import { Upload } from "components/Upload";
 import * as Types from "shared/types";
 import * as DC from "dataConstants";
 import { useFormContext } from "react-hook-form";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { parseLabelToHTML } from "utils/parser";
 import SharedContext from "shared/SharedContext";
 
 export const AdditionalNotes = () => {
   const register = useCustomRegister<Types.AdditionalNotes>();
   const { getValues, resetField } = useFormContext();
+  //WIP: using form context to get the labels for this component temporarily.
+  const labels: any = useContext(SharedContext);
+  const { header, section, upload } = labels?.AdditionalNotes ?? {};
+  const [textAreaLabel, setTextAreaLabel] = useState<string>(
+    section.isReportingText
+  );
   const didReport = getValues()["DidReport"];
 
   useEffect(() => {
     resetField("AdditionalNotes-AdditionalNotes");
-  }, [didReport, resetField]);
-
-  //WIP: using form context to get the labels for this component temporarily.
-  const labels: any = useContext(SharedContext);
+    if (didReport === "no") {
+      setTextAreaLabel(section.isNotReportingText);
+    } else {
+      setTextAreaLabel(section.isReportingText);
+    }
+  }, [didReport, resetField]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <QMR.CoreQuestionWrapper
-      testid="additional-notes"
-      label={labels?.AdditonalNotes?.header}
-    >
+    <QMR.CoreQuestionWrapper testid="additional-notes" label={header}>
       <QMR.TextArea
-        label={parseLabelToHTML(labels?.AdditonalNotes?.section)}
+        label={parseLabelToHTML(textAreaLabel)}
         {...register(DC.ADDITIONAL_NOTES)}
       />
       <CUI.Box marginTop={10}>
-        <Upload
-          label={labels?.AdditonalNotes?.upload}
-          {...register(DC.ADDITIONAL_NOTES_UPLOAD)}
-        />
+        <Upload label={upload} {...register(DC.ADDITIONAL_NOTES_UPLOAD)} />
       </CUI.Box>
     </QMR.CoreQuestionWrapper>
   );
