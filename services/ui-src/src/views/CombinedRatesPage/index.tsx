9654b41f96cd069afe2a2b1e03f372f748e29ce7
ailZhou <aileen.zhou@coforma.io>
2024-05-30 11:10:12 -0400

    set up table in tabs

--- services/ui-src/src/views/CombinedRatesPage/index.tsx
+++ services/ui-src/src/views/CombinedRatesPage/index.tsx
@@ -3,25 +3,71 @@ import * as QMR from "components";
 import { useGetMeasures } from "hooks/api";
 import { MeasureData } from "types";
 import { measureDescriptions } from "measures/measureDescriptions";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { MeasureTableItem } from "components/Table/types";
 
-const GetCoreSetPanel = (coreSet: string) => {
+const GetColumns = () => {
+  return [
+    {
+      header: "Abbreviation",
+      id: "aabr_column_header",
+      styleProps: { textAlign: "center", width: "10%" },
+      cell: (data: MeasureTableItem.Data) => {
+        return (
+          <Link to={data.path}>
+            <CUI.Text fontWeight="bold" color="blue.600" data-cy={data.abbr}>
+              {data.abbr}
+            </CUI.Text>
+          </Link>
+        );
+      },
+    },
+    {
+      header: "Measure",
+      id: "title_column_header",
+      cell: (data: MeasureTableItem.Data) => {
+        return (
+          <Link to={data.path}>
+            <CUI.Text fontWeight="bold" color="blue.600" data-cy={data.path}>
+              {data.title}
+            </CUI.Text>
+          </Link>
+        );
+      },
+    },
+  ];
+};
+
+const GetMeasuresByCoreSet = (coreSet: string, year: string) => {
   const { data } = useGetMeasures(coreSet);
-  const items = data?.Items as MeasureData[];
-  return items
+  const measures = data?.Items as MeasureData[];
+  const formatted = measures
     ?.filter(
       // filter out the coreset qualifiers
       (item) => item.measure && item.measure !== "CSQ"
     )
     .sort((a, b) => a?.measure?.localeCompare(b?.measure));
+
+  return formatted?.map((item) => {
+    return {
+      id: item.measure,
+      abbr: item.measure,
+      title: measureDescriptions[year][item.measure],
+      path: "/",
+    } as MeasureTableItem.Data;
+  });
 };
 
 export const CombinedRatesPage = () => {
-  const coreSetAbbr = ["ACSC", "CCSC"];
-  let panelItems = coreSetAbbr.map((coreSet) => GetCoreSetPanel(coreSet));
+  const coreSetAbbr = [
+    { abbr: "CCSC", title: "Child Core Set" },
+    { abbr: "ACSC", title: "Adult Core Set" },
+  ];
   const { state, year } = useParams();
+  let data = coreSetAbbr.map((coreSet) =>
+    GetMeasuresByCoreSet(coreSet.abbr, year!)
+  );
 
-  // const tableData = [];
   return (
     <QMR.StateLayout
       breadcrumbItems={[
@@ -47,24 +93,22 @@ export const CombinedRatesPage = () => {
           Instructions for the user - includes how to interpret the page and
           what they need to do to see rates (i.e. complete all measures)
         </CUI.Text>
-        <CUI.Tabs variant="enclosed">
+        <CUI.Tabs variant="enclosed" width="100%">
           <CUI.TabList>
             {coreSetAbbr.map((coreSet) => (
-              <CUI.Tab>{coreSet}</CUI.Tab>
+              <CUI.Tab key={coreSet.title + "tab"}>{coreSet.title}</CUI.Tab>
             ))}
           </CUI.TabList>
-          <CUI.TabPanels>
-            {panelItems?.map((items) => (
-              <CUI.TabPanel>
-                {/* <QMR.Table data={measures} columns={QMR.measuresColumns(year)} /> */}
-                {items?.map((item: any) => (
-                  <CUI.Link to={`combined-rates`}>
-                    <CUI.Text>{item?.measure}</CUI.Text>
-                    <CUI.Text>
-                      {measureDescriptions?.[year!]?.[item?.measure]}
-                    </CUI.Text>
-                  </CUI.Link>
-                ))}
+          <CUI.TabPanels border="1px">
+            {data?.map((measures, idx) => (
+              <CUI.TabPanel key={"panel" + idx}>
+                {measures?.length > 0 ? (
+                  <QMR.Table data={measures} columns={GetColumns()} />
+                ) : (
+                  <CUI.Text textAlign="center" padding="16">
+                    No core sets have been added
+                  </CUI.Text>
+                )}
               </CUI.TabPanel>
             ))}
           </CUI.TabPanels>
