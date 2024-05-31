c09eef38613ce3d90169328cf047b9d07c7a429c
Doug White <doug.white@stratiform.digital>
2024-05-30 21:45:29 -0400

    initial push of .env stuff for qmr

--- tests/cypress/cypress.config.ts
+++ tests/cypress/cypress.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "cypress";
 import setupNodeEvents from "./plugins";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
 
 export default defineConfig({
   redirectionLimit: 20,
@@ -12,9 +15,10 @@ export default defineConfig({
   downloadsFolder: "downloads",
   defaultCommandTimeout: 15000,
   env: {
-    STATE_USER_2: "stateuser2@test.com",
-    STATE_USER_4: "stateuser4@test.com",
-    ADMIN_USER: "adminuser@test.com",
+    STATE_USER_2: process.env.CYPRESS_STATE_USER_2,
+    STATE_USER_4: process.env.CYPRESS_STATE_USER_4,
+    ADMIN_USER: process.env.CYPRESS_ADMIN_USER,
+    QMR_PASSWORD: process.env.CYPRESS_QMR_PASSWORD,
   },
   e2e: {
     setupNodeEvents,
