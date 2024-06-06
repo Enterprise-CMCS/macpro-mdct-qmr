b18972f0e731840d758f33db29b51cb95028b8f2
Ajaita Saini <ajaita.saini@coforma.io>
2024-06-05 17:43:23 -0700

    wip

--- services/ui-src/src/views/CombinedRatesPage/CombinedRatesMeasure.tsx
+++ services/ui-src/src/views/CombinedRatesPage/CombinedRatesMeasure.tsx
@@ -1,14 +1,16 @@
 import * as QMR from "components";
 import * as CUI from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 interface Props {
   year: string;
   measureId: string;
+  measureName: string;
 }
 
-export const CombinedRatesMeasure = ({ year }: Props) => {
-  const { state, measure } = useParams();
+export const CombinedRatesMeasure = ({ year, measureName }: Props) => {
+  const { state, measure, coreSetId } = useParams();
+  const chipPath = `/${state}/${year}/ACSC/${measure}`;
 
   return (
     <QMR.StateLayout
@@ -20,7 +22,25 @@ export const CombinedRatesMeasure = ({ year }: Props) => {
         },
       ]}
     >
-      <CUI.Text>TBD</CUI.Text>
+      <CUI.Heading fontSize="xl" mt="2" mb="2">
+        {measure} - {measureName}
+      </CUI.Heading>
+      <body> TO-DO: replace placeholder text</body>
+      <CUI.Heading size="sm" as="h2" fontWeight="400" mt="4">
+        Measures used to calculate combined rates:
+      </CUI.Heading>
+      <CUI.UnorderedList m="5" ml="10">
+        <CUI.ListItem>
+          <Link to={chipPath} aria-label="" className="">
+            CHIP - {measure} - {measureName}
+          </Link>
+        </CUI.ListItem>
+        <CUI.ListItem>
+          <Link to={chipPath} aria-label="" className="">
+            Medicaid - {measure} - {measureName}
+          </Link>
+        </CUI.ListItem>
+      </CUI.UnorderedList>
     </QMR.StateLayout>
   );
 };
