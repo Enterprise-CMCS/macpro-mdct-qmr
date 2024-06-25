7fc5efd930dc5c9f9a4c9831213c501b5eac0e93
Angela Cooney <angela.cooney11@gmail.com>
2024-06-25 15:23:34 -0400

    testing a way to fix import statement errors

--- services/uploads/src/libs/dynamodb-lib.js
+++ services/uploads/src/libs/dynamodb-lib.js
@@ -1,5 +1,8 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  ScanCommand,
+} = require("@aws-sdk/lib-dynamodb");
 
 const client = DynamoDBDocumentClient.from(
   new DynamoDBClient({ region: "us-east-1" })
