2d5880ee8122d579fd99947386f6472a1ec12d05
benmartin-coforma <Ben.Martin@coforma.io>
2024-06-26 09:23:28 -0600

    Fix Dynamo to S3 sync

--- services/uploads/src/dynamoSync/handler.js
+++ services/uploads/src/dynamoSync/handler.js
@@ -1,10 +1,10 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { handler } = require("../libs/handler-lib");
-const { scan } = require("../libs/dynamodb-lib");
+const { scanAll } = require("../libs/dynamodb-lib");
 const { flatten } = require("flat");
 const { parseAsync } = require("json2csv");
 
-const csvToS3 = async (scanResult) => {
+const arrayToCsv = async (scanResult) => {
   const flattenedResults = scanResult.map((item) => flatten(item));
   const resultsCsvData = await parseAsync(flattenedResults);
   return resultsCsvData;
@@ -18,44 +18,20 @@ const uploadFileToS3 = async (filePath, scanResult) => {
     Body: scanResult,
   };
   try {
-    await client.send(new PutObjectCommand(params)),
-      console.log(`File (${data.Key}) uploaded to: ${data.Location}`);
+    await client.send(new PutObjectCommand(uploadParams));
+    console.log(`File uploaded to: ${filePath}`);
   } catch (err) {
     throw err;
   }
 };
 
-const scanAll = async (TableName) => {
-  let startingData = await scan({
-    TableName,
-  });
-  let dataList = startingData.Items;
-  let ExclusiveStartKey = startingData.LastEvaluatedKey;
-
-  while (ExclusiveStartKey) {
-    const params = {
-      TableName,
-      ExclusiveStartKey,
-    };
-
-    const results = await scan(params);
-    ExclusiveStartKey = results.LastEvaluatedKey;
-    dataList = [...results.Items, ...dataList];
-    console.log(results);
-  }
-  return dataList;
-};
-
 const syncDynamoToS3 = handler(async (_event, _context) => {
   console.log("Syncing Dynamo to Uploads");
-  const measureScanResults = await scanAll(process.env.measureTableName);
-  const coreSetScanResults = await scanAll(process.env.coreSetTableName);
-  const rateScanResults = await scanAll(process.env.rateTableName);
-  const measureResults = measureScanResults ? measureScanResults : [];
-  const coreSetResults = coreSetScanResults ? coreSetScanResults : [];
-  const rateResults = rateScanResults ? rateScanResults : [];
+  const measureResults = await scanAll(process.env.measureTableName);
+  const coreSetResults = await scanAll(process.env.coreSetTableName);
+  const rateResults = await scanAll(process.env.rateTableName);
 
-  const measureCsv = await csvToS3(measureResults);
+  const measureCsv = await arrayToCsv(measureResults);
   await uploadFileToS3(`coreSetData/CSVmeasures/${Date.now()}.csv`, measureCsv);
   await uploadFileToS3(
     `coreSetData/JSONmeasures/${Date.now()}.json`,
@@ -63,7 +39,7 @@ const syncDynamoToS3 = handler(async (_event, _context) => {
   );
   console.log("Uploaded measures file to s3");
 
-  const coreSetCsv = await csvToS3(coreSetResults);
+  const coreSetCsv = await arrayToCsv(coreSetResults);
   await uploadFileToS3(`coreSetData/CSVcoreSet/${Date.now()}.csv`, coreSetCsv);
   await uploadFileToS3(
     `coreSetData/JSONcoreSet/${Date.now()}.json`,
@@ -71,7 +47,7 @@ const syncDynamoToS3 = handler(async (_event, _context) => {
   );
   console.log("Uploaded coreSet file to s3");
 
-  const rateCsv = await csvToS3(rateResults);
+  const rateCsv = await arrayToCsv(rateResults);
   await uploadFileToS3(`coreSetData/CSVrate/${Date.now()}.csv`, rateCsv);
   await uploadFileToS3(
     `coreSetData/JSONrate/${Date.now()}.json`,
@@ -79,6 +55,7 @@ const syncDynamoToS3 = handler(async (_event, _context) => {
   );
   console.log("Uploaded rate file to s3");
 });
+
 module.exports = {
   syncDynamoToS3: syncDynamoToS3,
 };
--- services/uploads/src/libs/dynamodb-lib.js
+++ services/uploads/src/libs/dynamodb-lib.js
@@ -1,15 +1,22 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
   DynamoDBDocumentClient,
-  ScanCommand,
+  paginateScan,
 } = require("@aws-sdk/lib-dynamodb");
 
 const client = DynamoDBDocumentClient.from(
-  new DynamoDBClient({ region: "us-east-1" })
+  new DynamoDBClient({
+    region: "us-east-1",
+    logger: console,
+  })
 );
 
 module.exports = {
-  scan: async (params) => {
-    await client.send(new ScanCommand(params));
+  scanAll: async (TableName) => {
+    let items = [];
+    for await (let page of paginateScan({ client }, { TableName })) {
+      items = items.concat(page.Items ?? []);
+    }
+    return items;
   },
 };
