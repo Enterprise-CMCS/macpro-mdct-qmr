1de0dea8854cff9d85ee57ea16f7ca5a802a744a
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-07-09 16:45:36 -0500

    add unit tests - draft mode

---
+++ services/ui-src/src/measures/2024/shared/globalValidations/validateNDRTotalsMatchSum/index.test.ts
@@ -0,0 +1,70 @@
+import {
+  pmaNumeratorInitiationSum,
+  pmaDenominatorInitiationSum,
+  pmaNumeratorEngagementSum,
+  pmaDenominatorEngagementSum,
+  pmaCorrectSums,
+} from "./validationHelpers";
+import { validateNDRTotalsMatchSum } from ".";
+
+// import * as VH from "utils/testUtils/2024/validationHelpers";
+
+describe("Testing PM Total vs Sum validations", () => {
+  it("should return no errors - all sums match total values", () => {
+    const bothNumDenCorrect = validateNDRTotalsMatchSum(pmaCorrectSums);
+
+    expect(bothNumDenCorrect.length).toBe(0);
+  });
+
+  it("should return numerator sum error - sums do not match totals for initiation values", () => {
+    const valNumerator = validateNDRTotalsMatchSum(pmaNumeratorInitiationSum);
+
+    expect(valNumerator.length).toBe(1);
+    for (const result of [...valNumerator]) {
+      expect(result.errorLocation).toBe("Performance Measure");
+      expect(result.errorMessage).toBe(
+        "Numerators must be the sum for each category of performance measures"
+      );
+    }
+  });
+
+  it("should return numerator sum error - sums do not match totals for engagement values", () => {
+    const valNumerator = validateNDRTotalsMatchSum(pmaNumeratorEngagementSum);
+
+    expect(valNumerator.length).toBe(1);
+    for (const result of [...valNumerator]) {
+      expect(result.errorLocation).toBe("Performance Measure");
+      expect(result.errorMessage).toBe(
+        "Numerators must be the sum for each category of performance measures"
+      );
+    }
+  });
+
+  it("should return denominator sum error - sums do not match totals for intiation values", () => {
+    const valDenominators = validateNDRTotalsMatchSum(
+      pmaDenominatorInitiationSum
+    );
+
+    expect(valDenominators.length).toBe(1);
+    for (const result of [...valDenominators]) {
+      expect(result.errorLocation).toBe("Performance Measure");
+      expect(result.errorMessage).toBe(
+        "Denominators must be the sum for each category of performance measures"
+      );
+    }
+  });
+
+  it("should return denominator sum error - sums do not match totals for engagement values", () => {
+    const valDenominators = validateNDRTotalsMatchSum(
+      pmaDenominatorEngagementSum
+    );
+
+    expect(valDenominators.length).toBe(1);
+    for (const result of [...valDenominators]) {
+      expect(result.errorLocation).toBe("Performance Measure");
+      expect(result.errorMessage).toBe(
+        "Denominators must be the sum for each category of performance measures"
+      );
+    }
+  });
+});
---
+++ services/ui-src/src/measures/2024/shared/globalValidations/validateNDRTotalsMatchSum/validationHelpers.ts
@@ -0,0 +1,246 @@
+export const pmaNumeratorInitiationSum = [
+  [
+    {
+      isTotal: false,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Alcohol Use Disorder",
+      numerator: "20",
+      denominator: "50",
+    },
+    {
+      isTotal: false,
+      rate: "80.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Opioid Use Disorder",
+      numerator: "20",
+      denominator: "25",
+    },
+    {
+      isTotal: true,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Total",
+      numerator: "20",
+      denominator: "75",
+    },
+  ],
+];
+export const pmaNumeratorEngagementSum = [
+  [
+    {
+      isTotal: false,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Alcohol Use Disorder",
+      numerator: "20",
+      denominator: "50",
+    },
+    {
+      isTotal: false,
+      rate: "80.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Opioid Use Disorder",
+      numerator: "20",
+      denominator: "25",
+    },
+    {
+      isTotal: true,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Total",
+      numerator: "20",
+      denominator: "75",
+    },
+  ],
+];
+export const pmaDenominatorInitiationSum = [
+  [
+    {
+      isTotal: false,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Alcohol Use Disorder",
+      numerator: "20",
+      denominator: "50",
+    },
+    {
+      isTotal: false,
+      rate: "80.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Opioid Use Disorder",
+      numerator: "20",
+      denominator: "25",
+    },
+    {
+      isTotal: true,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Total",
+      numerator: "40",
+      denominator: "50",
+    },
+  ],
+];
+export const pmaDenominatorEngagementSum = [
+  [
+    {
+      isTotal: false,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Alcohol Use Disorder",
+      numerator: "20",
+      denominator: "50",
+    },
+    {
+      isTotal: false,
+      rate: "80.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Opioid Use Disorder",
+      numerator: "20",
+      denominator: "25",
+    },
+    {
+      isTotal: true,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Total",
+      numerator: "40",
+      denominator: "50",
+    },
+  ],
+];
+export const pmaCorrectSums = [
+  [
+    {
+      isTotal: false,
+      rate: "40.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Alcohol Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+    {
+      isTotal: false,
+      rate: "80.0",
+      label: "Age 65 and older",
+      category: "Initiation of SUD Treatment: Alcohol Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+  ],
+  [
+    {
+      isTotal: false,
+      rate: "24.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Alcohol Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+    {
+      isTotal: false,
+      rate: "60.0",
+      label: "Age 65 and older",
+      category: "Engagement of SUD Treatment: Alcohol Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+  ],
+  [
+    {
+      isTotal: false,
+      rate: "24.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Opioid Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+    {
+      isTotal: false,
+      rate: "80.0",
+      label: "Age 65 and older",
+      category: "Initiation of SUD Treatment: Opioid Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+  ],
+  [
+    {
+      isTotal: false,
+      rate: "24.0",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Opioid Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+    {
+      isTotal: false,
+      rate: "60.0",
+      label: "Age 65 and older",
+      category: "Engagement of SUD Treatment: Opioid Use Disorder",
+      numerator: "2",
+      denominator: "3",
+    },
+  ],
+  [
+    {
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Other Substance Use Disorder",
+      isTotal: false,
+    },
+    {
+      label: "Age 65 and older",
+      category: "Initiation of SUD Treatment: Other Substance Use Disorder",
+      isTotal: false,
+    },
+  ],
+  [
+    {
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Other Substance Use Disorder",
+      isTotal: false,
+    },
+    {
+      label: "Age 65 and older",
+      category: "Engagement of SUD Treatment: Other Substance Use Disorder",
+      isTotal: false,
+    },
+  ],
+  [
+    {
+      isTotal: true,
+      rate: "28.0",
+      label: "Ages 18 to 64",
+      category: "Initiation of SUD Treatment: Total",
+      numerator: "4",
+      denominator: "6",
+    },
+    {
+      isTotal: true,
+      rate: "46.0",
+      label: "Age 65 and older",
+      category: "Initiation of SUD Treatment: Total",
+      numerator: "4",
+      denominator: "6",
+    },
+  ],
+  [
+    {
+      isTotal: true,
+      rate: "17.3",
+      label: "Ages 18 to 64",
+      category: "Engagement of SUD Treatment: Total",
+      numerator: "26",
+      denominator: "150",
+    },
+    {
+      isTotal: true,
+      rate: "32.0",
+      label: "Age 65 and older",
+      category: "Engagement of SUD Treatment: Total",
+      numerator: "32",
+      denominator: "100",
+    },
+  ],
+];
