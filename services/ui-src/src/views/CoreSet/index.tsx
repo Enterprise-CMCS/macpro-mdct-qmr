cb42793683abb513a709a9625e1d6975e99ed9df
Ajaita Saini <ajaita.saini@coforma.io>
2024-07-08 20:38:53 -0400

    reduce checks

--- services/ui-src/src/views/CoreSet/index.tsx
+++ services/ui-src/src/views/CoreSet/index.tsx
@@ -313,6 +313,11 @@ export const CoreSet = () => {
     (measure) => measure.id
   );
 
+  const coreSetInstructions: { [key: string]: string } = {
+    CC: "Beginning with FFY 2024 reporting, states are required to report all of the measures on the Child Core Set. More information on mandatory reporting requirements is included in the Initial Core Set Mandatory Reporting Guidance for the Child and Adult Core Sets.",
+    HH: "States with approved Health Home Programs in operation by June 30, 2023 are required to report all of the measures on the Health Home Core Set. More information on mandatory reporting requirements is included in the Initial Core Set Mandatory Reporting Guidance for the Health Home Core Sets.",
+  };
+
   const coreSetPrefix = coreSet[0].slice(0, 2);
 
   return (
@@ -325,18 +330,13 @@ export const CoreSet = () => {
         },
       ]}
     >
-      {Number(year) >= 2024 &&
-        (coreSetPrefix === "CC" || coreSetPrefix === "HH") && (
-          <CUI.Box mb="8">
-            <Alert heading="Mandatory Measure Instructions">
-              <CUI.Text>
-                {coreSetPrefix === "CC"
-                  ? "Beginning with FFY 2024 reporting, states are required to report all of the measures on the Child Core Set. More information on mandatory reporting requirements is included in the Initial Core Set Mandatory Reporting Guidance for the Child and Adult Core Sets."
-                  : "States with approved Health Home Programs in operation by June 30, 2023 are required to report all of the measures on the Health Home Core Set. More information on mandatory reporting requirements is included in the Initial Core Set Mandatory Reporting Guidance for the Health Home Core Sets."}
-              </CUI.Text>
-            </Alert>
-          </CUI.Box>
-        )}
+      {Number(year) >= 2024 && coreSetInstructions[coreSetPrefix] && (
+        <CUI.Box mb="8">
+          <Alert heading="Mandatory Measure Instructions">
+            <CUI.Text>{coreSetInstructions[coreSetPrefix]}</CUI.Text>
+          </Alert>
+        </CUI.Box>
+      )}
       <QMR.UpdateInfoModal
         closeModal={closeModal}
         handleModalResponse={handleModalResponse}
