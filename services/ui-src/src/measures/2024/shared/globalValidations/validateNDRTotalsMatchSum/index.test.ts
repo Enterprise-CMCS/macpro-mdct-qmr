b7e7c016c4c4f829869a1a236fc950b8ad5888ec
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-07-11 11:45:22 -0500

    simplify test - remove rest operator

--- services/ui-src/src/measures/2024/shared/globalValidations/validateNDRTotalsMatchSum/index.test.ts
+++ services/ui-src/src/measures/2024/shared/globalValidations/validateNDRTotalsMatchSum/index.test.ts
@@ -15,53 +15,49 @@ describe("Testing PM Total vs Sum validations", () => {
 
   it("should return numerator sum error - sums do not match totals for initiation values", () => {
     const valNumerator = validateNDRTotalsMatchSum(pmaNumeratorInitiationSum);
-
     expect(valNumerator.length).toBe(1);
-    for (const result of [...valNumerator]) {
-      expect(result.errorLocation).toBe("Performance Measure");
-      expect(result.errorMessage).toBe(
-        "Numerators must be the sum for each category of performance measures"
-      );
-    }
+
+    const result = valNumerator[0];
+    expect(result.errorLocation).toBe("Performance Measure");
+    expect(result.errorMessage).toBe(
+      "Numerators must be the sum for each category of performance measures"
+    );
   });
 
   it("should return numerator sum error - sums do not match totals for engagement values", () => {
     const valNumerator = validateNDRTotalsMatchSum(pmaNumeratorEngagementSum);
-
     expect(valNumerator.length).toBe(1);
-    for (const result of [...valNumerator]) {
-      expect(result.errorLocation).toBe("Performance Measure");
-      expect(result.errorMessage).toBe(
-        "Numerators must be the sum for each category of performance measures"
-      );
-    }
+
+    const result = valNumerator[0];
+    expect(result.errorLocation).toBe("Performance Measure");
+    expect(result.errorMessage).toBe(
+      "Numerators must be the sum for each category of performance measures"
+    );
   });
 
   it("should return denominator sum error - sums do not match totals for intiation values", () => {
     const valDenominators = validateNDRTotalsMatchSum(
       pmaDenominatorInitiationSum
     );
-
     expect(valDenominators.length).toBe(1);
-    for (const result of [...valDenominators]) {
-      expect(result.errorLocation).toBe("Performance Measure");
-      expect(result.errorMessage).toBe(
-        "Denominators must be the sum for each category of performance measures"
-      );
-    }
+
+    const result = valDenominators[0];
+    expect(result.errorLocation).toBe("Performance Measure");
+    expect(result.errorMessage).toBe(
+      "Denominators must be the sum for each category of performance measures"
+    );
   });
 
   it("should return denominator sum error - sums do not match totals for engagement values", () => {
     const valDenominators = validateNDRTotalsMatchSum(
       pmaDenominatorEngagementSum
     );
-
     expect(valDenominators.length).toBe(1);
-    for (const result of [...valDenominators]) {
-      expect(result.errorLocation).toBe("Performance Measure");
-      expect(result.errorMessage).toBe(
-        "Denominators must be the sum for each category of performance measures"
-      );
-    }
+
+    const result = valDenominators[0];
+    expect(result.errorLocation).toBe("Performance Measure");
+    expect(result.errorMessage).toBe(
+      "Denominators must be the sum for each category of performance measures"
+    );
   });
 });
