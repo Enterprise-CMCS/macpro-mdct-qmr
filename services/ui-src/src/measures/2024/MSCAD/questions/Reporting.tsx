9a2568b1b7a603eec177c86847bde7dd5fa559b1
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-08-19 16:46:02 -0500

    add not reporting options to 1st question

--- services/ui-src/src/measures/2024/MSCAD/questions/Reporting.tsx
+++ services/ui-src/src/measures/2024/MSCAD/questions/Reporting.tsx
@@ -1,16 +1,23 @@
 import * as QMR from "components";
 import { useCustomRegister } from "hooks/useCustomRegister";
-import * as Types from "../types";
-import * as DC from "dataConstants";
+import { useFormContext } from "react-hook-form";
+import { FormData } from "../types";
+import { WhyAreYouNotReporting } from "shared/commonQuestions/WhyAreYouNotReporting";
 
 interface Props {
-  measureName: string;
-  measureAbbreviation: string;
   reportingYear: string;
+  healthHomeMeasure?: boolean;
+  removeLessThan30?: boolean;
 }
 
-export const Reporting = ({ reportingYear }: Props) => {
-  const register = useCustomRegister<Types.FormData>();
+export const Reporting = ({
+  reportingYear,
+  healthHomeMeasure,
+  removeLessThan30,
+}: Props) => {
+  const register = useCustomRegister<FormData>();
+  const { watch } = useFormContext<FormData>();
+  const watchRadioStatus = watch("DidCollect");
 
   return (
     <>
@@ -19,19 +26,25 @@ export const Reporting = ({ reportingYear }: Props) => {
         label="Did you collect this measure?"
       >
         <QMR.RadioButton
-          {...register(DC.DID_COLLECT)}
+          {...register("DidCollect")}
           options={[
             {
               displayValue: `Yes, we did collect data for Medical Assistance with Smoking and Tobacco Use Cessation (MSC-AD) for FFY ${reportingYear} quality measure reporting.`,
-              value: DC.YES,
+              value: "yes",
             },
             {
               displayValue: `No, we did not collect data for Medical Assistance with Smoking and Tobacco Use Cessation (MSC-AD) for FFY ${reportingYear} quality measure reporting.`,
-              value: DC.NO,
+              value: "no",
             },
           ]}
         />
       </QMR.CoreQuestionWrapper>
+      {watchRadioStatus?.includes("no") && (
+        <WhyAreYouNotReporting
+          healthHomeMeasure={healthHomeMeasure}
+          removeLessThan30={removeLessThan30}
+        />
+      )}
     </>
   );
 };
