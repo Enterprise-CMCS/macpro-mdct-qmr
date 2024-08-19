ab8868376077f88a88392b5f6571ac8fb4a02ec6
Ajaita Saini <ajaita.saini@coforma.io>
2024-08-19 16:45:37 -0400

    remove combined rates hh for 2024 and beyond

--- services/ui-src/src/labels/2024/commonQuestionsLabel.tsx
+++ services/ui-src/src/labels/2024/commonQuestionsLabel.tsx
@@ -11,22 +11,6 @@ export const commonQuestionsLabel = {
     },
   },
   CombinedRates: {
-    header: "Combined Rate(s) from Multiple Reporting Units",
-    healthHome: {
-      question:
-        "Did you combine rates from multiple reporting units (e.g. Health Home Providers) to create a Health Home Program-Level rate?",
-      optionYes:
-        "Yes, we combined rates from multiple reporting units to create a Health Home Program-Level rate.",
-      optionNo:
-        "No, we did not combine rates from multiple reporting units to create a Program-Level rate for Health Home measures.",
-      notWeightedRate:
-        "The rates are not weighted based on the size of the measure-eligible population. All reporting units are given equal weights when calculating a Program-Level rate.",
-      weightedRate:
-        "The rates are weighted based on the size of the measure-eligible population for each reporting unit.",
-      weightedRateOther:
-        "The rates are weighted based on another weighting factor.",
-      weightedRateOtherExplain: "Describe the other weighting factor:",
-    },
     notHealthHome: {
       question:
         "Did you combine rates from multiple reporting units (e.g. health plans, delivery systems, programs) to create a State-Level rate?",
--- services/ui-src/src/shared/commonQuestions/CombinedRates.tsx
+++ services/ui-src/src/shared/commonQuestions/CombinedRates.tsx
@@ -21,65 +21,67 @@ export const CombinedRates = ({ healthHomeMeasure }: Props) => {
     : labels.CombinedRates.notHealthHome;
 
   return (
-    <QMR.CoreQuestionWrapper
-      testid="combined-rates"
-      label={labels.CombinedRates.header}
-    >
-      <CUI.Text fontWeight={600}>{combinedLabel?.question}</CUI.Text>
-      <CUI.Text mb={2}>
-        For additional information refer to the{" "}
-        <CUI.Link
-          href="https://www.medicaid.gov/medicaid/quality-of-care/downloads/state-level-rates-brief.pdf"
-          color="blue"
-          isExternal
-          aria-label="Additional state level brief information"
-        >
-          <u>State-Level Rate Brief</u>
-        </CUI.Link>
-        .
-      </CUI.Text>
-      <QMR.RadioButton
-        options={[
-          {
-            displayValue: combinedLabel.optionYes,
-            value: DC.YES,
-            children: [
-              <QMR.RadioButton
-                {...register(DC.COMBINED_RATES_COMBINED_RATES)}
-                options={[
-                  {
-                    displayValue: combinedLabel.notWeightedRate,
-                    value: DC.COMBINED_NOT_WEIGHTED_RATES,
-                  },
-                  {
-                    displayValue: combinedLabel.weightedRate,
-                    value: DC.COMBINED_WEIGHTED_RATES,
-                  },
-                  {
-                    displayValue: combinedLabel.weightedRateOther,
-                    value: DC.COMBINED_WEIGHTED_RATES_OTHER,
-                    children: [
-                      <QMR.TextArea
-                        {...register(
-                          DC.COMBINED_WEIGHTED_RATES_OTHER_EXPLAINATION
-                        )}
-                        label={combinedLabel.weightedRateOtherExplain}
-                        formLabelProps={{ fontWeight: 400 }}
-                      />,
-                    ],
-                  },
-                ]}
-              />,
-            ],
-          },
-          {
-            displayValue: combinedLabel.optionNo,
-            value: DC.NO,
-          },
-        ]}
-        renderHelperTextAbove
-        {...register(DC.COMBINED_RATES)}
-      />
-    </QMR.CoreQuestionWrapper>
+    labels.year < 2024 && (
+      <QMR.CoreQuestionWrapper
+        testid="combined-rates"
+        label={labels.CombinedRates.header}
+      >
+        <CUI.Text fontWeight={600}>{combinedLabel?.question}</CUI.Text>
+        <CUI.Text mb={2}>
+          For additional information refer to the{" "}
+          <CUI.Link
+            href="https://www.medicaid.gov/medicaid/quality-of-care/downloads/state-level-rates-brief.pdf"
+            color="blue"
+            isExternal
+            aria-label="Additional state level brief information"
+          >
+            <u>State-Level Rate Brief</u>
+          </CUI.Link>
+          .
+        </CUI.Text>
+        <QMR.RadioButton
+          options={[
+            {
+              displayValue: combinedLabel.optionYes,
+              value: DC.YES,
+              children: [
+                <QMR.RadioButton
+                  {...register(DC.COMBINED_RATES_COMBINED_RATES)}
+                  options={[
+                    {
+                      displayValue: combinedLabel.notWeightedRate,
+                      value: DC.COMBINED_NOT_WEIGHTED_RATES,
+                    },
+                    {
+                      displayValue: combinedLabel.weightedRate,
+                      value: DC.COMBINED_WEIGHTED_RATES,
+                    },
+                    {
+                      displayValue: combinedLabel.weightedRateOther,
+                      value: DC.COMBINED_WEIGHTED_RATES_OTHER,
+                      children: [
+                        <QMR.TextArea
+                          {...register(
+                            DC.COMBINED_WEIGHTED_RATES_OTHER_EXPLAINATION
+                          )}
+                          label={combinedLabel.weightedRateOtherExplain}
+                          formLabelProps={{ fontWeight: 400 }}
+                        />,
+                      ],
+                    },
+                  ]}
+                />,
+              ],
+            },
+            {
+              displayValue: combinedLabel.optionNo,
+              value: DC.NO,
+            },
+          ]}
+          renderHelperTextAbove
+          {...register(DC.COMBINED_RATES)}
+        />
+      </QMR.CoreQuestionWrapper>
+    )
   );
 };
