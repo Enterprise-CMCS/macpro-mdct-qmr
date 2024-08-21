d2a2727b2c3ae9b712edd262604c05013b2e88ea
Rocio De Santiago <rociodesantiago@CoformaSantiago.lan>
2024-08-21 16:31:41 -0500

    update delivery system question with Title XIX verbiage

--- services/ui-src/src/measures/2024/shared/Qualifiers/data.ts
+++ services/ui-src/src/measures/2024/shared/Qualifiers/data.ts
@@ -117,7 +117,7 @@ const ChildData: DataDriven = {
   title: "Child Core Set Qualifiers: Medicaid",
   questionTitle: "Child Core Set Questions: Medicaid",
   qualifierHeader: (year) =>
-    `As of September 30, ${year}, what percentage of your Medicaid/CHIP enrollees (under age 21) were enrolled in each delivery system?`,
+    `As of September 30, ${year}, what percentage of your Medicaid (Title XIX & XXI) enrollees (under age 21) were enrolled in each delivery system?`,
   textTable: [
     ["Medicaid (TITLE XIX & XXI)", "Under Age 21"],
     ["CHIP", "Under Age 21"],
