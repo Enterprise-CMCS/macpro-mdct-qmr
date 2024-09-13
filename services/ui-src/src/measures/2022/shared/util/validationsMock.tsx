e126a809c0309d2ad39a30c96fd115631cddbd93
ailZhou <aileen.zhou@coforma.io>
2024-09-13 12:35:53 -0400

    consolidated

--- services/ui-src/src/measures/2022/shared/util/validationsMock.tsx
+++ services/ui-src/src/measures/2022/shared/util/validationsMock.tsx
@@ -21,7 +21,7 @@ import * as ComplexAtLeastOneRateComplete from "shared/globalValidations/Complex
 import * as ComplexNoNonZeroNumOrDenom from "measures/2022/shared/globalValidations/ComplexValidations/ComplexNoNonZeroNumOrDenom";
 import * as ComplexValidateAtLeastOneNDRInDeviationOfMeasureSpec from "measures/2022/shared/globalValidations/ComplexValidations/ComplexValidateAtLeastOneNDRInDeviationOfMeasureSpec"; //pragma: allowlist secret
 import * as ComplexValidateNDRTotals from "measures/2022/shared/globalValidations/ComplexValidations/ComplexValidateNDRTotals";
-import * as ComplexValidateDualPopInformation from "measures/2022/shared/globalValidations/ComplexValidations/ComplexValidateDualPopInformation";
+import * as ComplexValidateDualPopInformation from "shared/globalValidations/ComplexValidations/ComplexValidateDualPopInformation";
 import * as ComplexValueSameCrossCategory from "measures/2022/shared/globalValidations/ComplexValidations/ComplexValueSameCrossCategory";
 import { DefaultFormDataLegacy as DefaultFormData } from "shared/types/FormData";
 
