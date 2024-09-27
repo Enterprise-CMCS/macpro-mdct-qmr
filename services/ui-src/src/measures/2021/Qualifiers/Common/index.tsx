b840d192a0046259c69c253f69a517879865f7a6
ailZhou <aileen.zhou@coforma.io>
2024-09-27 09:25:56 -0400

    audit

--- services/ui-src/src/measures/2021/Qualifiers/Common/index.tsx
+++ services/ui-src/src/measures/2021/Qualifiers/Common/index.tsx
@@ -1,5 +1,5 @@
 export * from "shared/Qualifiers/Common/qualifierHeader";
-export * from "./audit";
+export * from "shared/Qualifiers/Common/audit";
 export * from "./externalContractor";
 export * from "./costSavingsData";
 export * from "shared/Qualifiers/Common/administrativeQuestions";
--- services/ui-src/src/shared/Qualifiers/Common/index.tsx
+++ services/ui-src/src/shared/Qualifiers/Common/index.tsx
@@ -1,5 +1,5 @@
 export * from "./qualifierHeader";
-// export * from "./audit";
+export * from "./audit";
 // export * from "./externalContractor";
 // export * from "./costSavingsData";
-export * from "shared/Qualifiers/Common/administrativeQuestions";
+export * from "./administrativeQuestions";
