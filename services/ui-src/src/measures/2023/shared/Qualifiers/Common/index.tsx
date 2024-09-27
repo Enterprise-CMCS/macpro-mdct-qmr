e737690ab4d7a894c3653ee7ecf412a3f3b6a178
ailZhou <aileen.zhou@coforma.io>
2024-09-27 14:36:16 -0400

    externalContractor

--- services/ui-src/src/measures/2023/shared/Qualifiers/Common/index.tsx
+++ services/ui-src/src/measures/2023/shared/Qualifiers/Common/index.tsx
@@ -1,5 +1,5 @@
 export * from "shared/Qualifiers/Common/qualifierHeader";
 export * from "shared/Qualifiers/Common/audit";
-export * from "./externalContractor";
+export * from "shared/Qualifiers/Common/externalContractor";
 export * from "shared/Qualifiers/Common/costSavingsData";
 export * from "shared/Qualifiers/Common/administrativeQuestions";
