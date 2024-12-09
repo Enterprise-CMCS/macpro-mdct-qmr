69f08c15a32ff91eff011e0fd1ff4a1abc5e0b83
ailZhou <aileen.zhou@coforma.io>
2024-12-09 14:46:13 -0500

    fixed alert icon size & remove margin block

--- services/ui-src/src/theme.ts
+++ services/ui-src/src/theme.ts
@@ -3,7 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
   styles: {
     global: {
-      body: {
+      "body, p": {
         fontFamily: "Open Sans",
       },
     },
