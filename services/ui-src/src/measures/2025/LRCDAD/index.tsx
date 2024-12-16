81fc3ce560ac7d87398e6ef43c599932f5e02ab0
ailZhou <aileen.zhou@coforma.io>
2024-12-16 13:49:22 -0500

    re-add text

--- services/ui-src/src/measures/2025/LRCDAD/index.tsx
+++ services/ui-src/src/measures/2025/LRCDAD/index.tsx
@@ -10,8 +10,11 @@ export const LRCDAD = ({ name, year }: Props) => {
     <QMR.AutocompletedMeasureTemplate
       year={year}
       measureTitle={`LRCD-AD - ${name}`}
-      performanceMeasureText=""
-      performanceMeasureSubtext=""
+      performanceMeasureText="Percentage of nulliparous (first birth), term (37 or more completed weeks based on the obstetric estimate), singleton (one fetus), in a cephalic presentation (head-first) births delivered by cesarean during the measurement year."
+      performanceMeasureSubtext="To reduce state burden and streamline reporting, CMS will calculate
+      this measure for states using state natality data obtained through
+      the Centers for Disease Control and Prevention Wide-ranging Online
+      Data for Epidemiologic Research (CDC WONDER)."
     />
   );
 };
