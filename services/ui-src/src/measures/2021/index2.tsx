c2eefae61f520c4bb6ba43a461cc3d1fc297cb0c
ailZhou <aileen.zhou@coforma.io>
2025-01-08 09:37:55 -0500

    fixed imports thanks to Brendan

--- services/ui-src/src/measures/2021/index2.tsx
+++ services/ui-src/src/measures/2021/index2.tsx
@@ -1,82 +1,62 @@
 import { Qualifier } from "shared/Qualifiers";
 import { Data } from "labels/2021/qualifierFormsData";
 
-const ADDCH_Data = await import("./ADDCH/data").then((module) => module.data);
-const ADDCH_Validations = await import("./ADDCH/validation").then(
-  (module) => module.validationFunctions
-);
-const AMBCH_Data = await import("./AMBCH/data").then((module) => module.data);
-const AMBCH_Validations = await import("./AMBCH/validation").then(
-  (module) => module.validationFunctions
-);
-const AMRCH_Data = await import("./AMRCH/data").then((module) => module.data);
-const AMRCH_Validations = await import("./AMRCH/validation").then(
-  (module) => module.validationFunctions
-);
-const APMCH_Data = await import("./APMCH/data").then((module) => module.data);
-const APMCH_Validations = await import("./APMCH/validation").then(
-  (module) => module.validationFunctions
-);
-const APPCH_Data = await import("./APPCH/data").then((module) => module.data);
-const APPCH_Validations = await import("./APPCH/validation").then(
-  (module) => module.validationFunctions
-);
-const AUDCH_Data = await import("./AUDCH/data").then((module) => module.data);
-const AUDCH_Validations = await import("./AUDCH/validation").then(
-  (module) => module.validationFunctions
-);
-const CCPCH_Data = await import("./CCPCH/data").then((module) => module.data);
-const CCPCH_Validations = await import("./CCPCH/validation").then(
-  (module) => module.validationFunctions
-);
-const CCWCH_Data = await import("./CCWCH/data").then((module) => module.data);
-const CCWCH_Validations = await import("./CCWCH/validation").then(
-  (module) => module.validationFunctions
-);
-const CDFCH_Data = await import("./CDFCH/data").then((module) => module.data);
-const CDFCH_Validations = await import("./CDFCH/validation").then(
-  (module) => module.validationFunctions
-);
-const CHLCH_Data = await import("./CHLCH/data").then((module) => module.data);
-const CHLCH_Validations = await import("./CHLCH/validation").then(
-  (module) => module.validationFunctions
-);
-const CISCH_Data = await import("./CISCH/data").then((module) => module.data);
-const CISCH_Validations = await import("./CISCH/validation").then(
-  (module) => module.validationFunctions
-);
-const DEVCH_Data = await import("./DEVCH/data").then((module) => module.data);
-const DEVCH_Validations = await import("./DEVCH/validation").then(
-  (module) => module.validationFunctions
-);
-const FUHCH_Data = await import("./FUHCH/data").then((module) => module.data);
-const FUHCH_Validations = await import("./FUHCH/validation").then(
-  (module) => module.validationFunctions
-);
-const IMACH_Data = await import("./IMACH/data").then((module) => module.data);
-const IMACH_Validations = await import("./IMACH/validation").then(
-  (module) => module.validationFunctions
-);
-const PPCCH_Data = await import("./PPCCH/data").then((module) => module.data);
-const PPCCH_Validations = await import("./PPCCH/validation").then(
-  (module) => module.validationFunctions
-);
-const SFMCH_Data = await import("./SFMCH/data").then((module) => module.data);
-const SFMCH_Validations = await import("./SFMCH/validation").then(
-  (module) => module.validationFunctions
-);
-const W30CH_Data = await import("./W30CH/data").then((module) => module.data);
-const W30CH_Validations = await import("./W30CH/validation").then(
-  (module) => module.validationFunctions
-);
-const WCCCH_Data = await import("./WCCCH/data").then((module) => module.data);
-const WCCCH_Validations = await import("./WCCCH/validation").then(
-  (module) => module.validationFunctions
-);
-const WCVCH_Data = await import("./WCVCH/data").then((module) => module.data);
-const WCVCH_Validations = await import("./WCVCH/validation").then(
-  (module) => module.validationFunctions
-);
+import { data as ADDCH_Data } from "./ADDCH/data";
+import { validationFunctions as ADDCH_Validations } from "./ADDCH/validation";
+
+import { data as AMBCH_Data } from "./AMBCH/data";
+import { validationFunctions as AMBCH_Validations } from "./AMBCH/validation";
+
+import { data as AMRCH_Data } from "./AMRCH/data";
+import { validationFunctions as AMRCH_Validations } from "./AMRCH/validation";
+
+import { data as APMCH_Data } from "./APMCH/data";
+import { validationFunctions as APMCH_Validations } from "./APMCH/validation";
+
+import { data as APPCH_Data } from "./APPCH/data";
+import { validationFunctions as APPCH_Validations } from "./APPCH/validation";
+
+import { data as AUDCH_Data } from "./AUDCH/data";
+import { validationFunctions as AUDCH_Validations } from "./AUDCH/validation";
+
+import { data as CCPCH_Data } from "./CCPCH/data";
+import { validationFunctions as CCPCH_Validations } from "./CCPCH/validation";
+
+import { data as CCWCH_Data } from "./CCWCH/data";
+import { validationFunctions as CCWCH_Validations } from "./CCWCH/validation";
+
+import { data as CDFCH_Data } from "./CDFCH/data";
+import { validationFunctions as CDFCH_Validations } from "./CDFCH/validation";
+
+import { data as CHLCH_Data } from "./CHLCH/data";
+import { validationFunctions as CHLCH_Validations } from "./CHLCH/validation";
+
+import { data as CISCH_Data } from "./CISCH/data";
+import { validationFunctions as CISCH_Validations } from "./CISCH/validation";
+
+import { data as DEVCH_Data } from "./DEVCH/data";
+import { validationFunctions as DEVCH_Validations } from "./DEVCH/validation";
+
+import { data as FUHCH_Data } from "./FUHCH/data";
+import { validationFunctions as FUHCH_Validations } from "./FUHCH/validation";
+
+import { data as IMACH_Data } from "./IMACH/data";
+import { validationFunctions as IMACH_Validations } from "./IMACH/validation";
+
+import { data as PPCCH_Data } from "./PPCCH/data";
+import { validationFunctions as PPCCH_Validations } from "./PPCCH/validation";
+
+import { data as SFMCH_Data } from "./SFMCH/data";
+import { validationFunctions as SFMCH_Validations } from "./SFMCH/validation";
+
+import { data as W30CH_Data } from "./W30CH/data";
+import { validationFunctions as W30CH_Validations } from "./W30CH/validation";
+
+import { data as WCCCH_Data } from "./WCCCH/data";
+import { validationFunctions as WCCCH_Validations } from "./WCCCH/validation";
+
+import { data as WCVCH_Data } from "./WCVCH/data";
+import { validationFunctions as WCVCH_Validations } from "./WCVCH/validation";
 
 const measureImports = {
   "ADD-CH": { data: ADDCH_Data, validationFunctions: ADDCH_Validations },
