51dc21af0a09c15d1020ce30a0b4f66906f3b85b
ailZhou <aileen.zhou@coforma.io>
2025-01-15 16:17:55 -0500

    2025 index to data files

--- services/ui-src/src/measures/2025/BCSAD/data.ts
+++ services/ui-src/src/measures/2025/BCSAD/data.ts
@@ -1,50 +1,42 @@
-import { DataDrivenTypes } from "shared/types";
 import { getCatQualLabels } from "../rateLabelText";
+import { MeasureTemplateData } from "shared/types/MeasureTemplate";
 import * as DC from "dataConstants";
 
 export const { categories, qualifiers } = getCatQualLabels("BCS-AD");
 
-export const data: DataDrivenTypes.PerformanceMeasure = {
-  questionText: [
-    "Percentage of women ages 50 to 74 who had a mammogram to screen for breast cancer.",
-  ],
-  questionListItems: [],
-  categories,
-  qualifiers,
-};
-
-export const dataSourceData: DataDrivenTypes.DataSource = {
-  optionsLabel:
-    "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
-  options: [
-    {
-      value: DC.ADMINISTRATIVE_DATA,
-      subOptions: [
-        {
-          label: "What is the Administrative Data Source?",
-          options: [
-            {
-              value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM,
-            },
-            {
-              value: DC.ADMINISTRATIVE_DATA_OTHER,
-              description: true,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      value: DC.ELECTRONIC_CLINIC_DATA_SYSTEMS,
-      description: true,
-    },
-    {
-      value: DC.ELECTRONIC_HEALTH_RECORDS,
-      description: true,
-    },
-    {
-      value: DC.OTHER_DATA_SOURCE,
-      description: true,
-    },
-  ],
+export const data: MeasureTemplateData = {
+  type: "HEDIS",
+  coreset: "adult",
+  performanceMeasure: {
+    questionText: [
+      "Percentage of women ages 50 to 74 who had a mammogram to screen for breast cancer.",
+    ],
+    questionListItems: [],
+    categories,
+    qualifiers,
+  },
+  dataSource: {
+    optionsLabel:
+      "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
+    options: [
+      {
+        value: DC.ADMINISTRATIVE_DATA,
+        subOptions: [
+          {
+            label: "What is the Administrative Data Source?",
+            options: [
+              { value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM },
+              { value: DC.ADMINISTRATIVE_DATA_OTHER, description: true },
+            ],
+          },
+        ],
+      },
+      { value: DC.ELECTRONIC_CLINIC_DATA_SYSTEMS, description: true },
+      { value: DC.ELECTRONIC_HEALTH_RECORDS, description: true },
+      { value: DC.OTHER_DATA_SOURCE, description: true },
+    ],
+  },
+  opm: {
+    excludeOptions: ["Sex"],
+  },
 };
--- services/ui-src/src/measures/2025/OEVPCH/data.ts
+++ services/ui-src/src/measures/2025/OEVPCH/data.ts
@@ -1,10 +1,17 @@
-import { DataDrivenTypes } from "shared/types";
 import { getCatQualLabels } from "../rateLabelText";
+import { MeasureTemplateData } from "shared/types/MeasureTemplate";
 
 export const { categories, qualifiers } = getCatQualLabels("OEVP-CH");
 
-export const data: DataDrivenTypes.PerformanceMeasure = {
-  questionText: ["."],
-  categories,
-  qualifiers,
+export const data: MeasureTemplateData = {
+  type: "ADA-DQA",
+  coreset: "child",
+  performanceMeasure: {
+    questionText: ["."],
+    categories,
+    qualifiers,
+  },
+  custom: {
+    calcTotal: true,
+  },
 };
