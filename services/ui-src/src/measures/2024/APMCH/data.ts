2f6ecab7e252c653bda0bcb1805f7bb8b88937b8
ailZhou <aileen.zhou@coforma.io>
2025-01-15 15:21:48 -0500

    2024 porting index to data files

--- services/ui-src/src/measures/2024/APMCH/data.ts
+++ services/ui-src/src/measures/2024/APMCH/data.ts
@@ -1,50 +1,45 @@
-import { DataDrivenTypes } from "shared/types";
-import * as DC from "dataConstants";
 import { getCatQualLabels } from "../rateLabelText";
+import { MeasureTemplateData } from "shared/types/MeasureTemplate";
+import * as DC from "dataConstants";
 
 export const { categories, qualifiers } = getCatQualLabels("APM-CH");
 
-export const data: DataDrivenTypes.PerformanceMeasure = {
-  questionText: [
-    "Percentage of children and adolescents ages 1 to 17 who had two or more antipsychotic prescriptions and had metabolic testing. Three rates are reported:",
-  ],
-  questionListItems: [
-    "Percentage of children and adolescents on antipsychotics who received blood glucose testing",
-    "Percentage of children and adolescents on antipsychotics who received cholesterol testing",
-    "Percentage of children and adolescents on antipsychotics who received blood glucose and cholesterol testing",
-  ],
-  categories,
-  qualifiers,
-};
-
-export const dataSourceData: DataDrivenTypes.DataSource = {
-  optionsLabel:
-    "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
-  options: [
-    {
-      value: DC.ADMINISTRATIVE_DATA,
-      subOptions: [
-        {
-          label: "What is the Administrative Data Source?",
-          options: [
-            {
-              value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM,
-            },
-            {
-              value: DC.ADMINISTRATIVE_DATA_OTHER,
-              description: true,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      value: DC.ELECTRONIC_CLINIC_DATA_SYSTEMS,
-      description: true,
-    },
-    {
-      value: DC.OTHER_DATA_SOURCE,
-      description: true,
-    },
-  ],
+export const data: MeasureTemplateData = {
+  type: "HEDIS",
+  coreset: "child",
+  performanceMeasure: {
+    questionText: [
+      "Percentage of children and adolescents ages 1 to 17 who had two or more antipsychotic prescriptions and had metabolic testing. Three rates are reported:",
+    ],
+    questionListItems: [
+      "Percentage of children and adolescents on antipsychotics who received blood glucose testing",
+      "Percentage of children and adolescents on antipsychotics who received cholesterol testing",
+      "Percentage of children and adolescents on antipsychotics who received blood glucose and cholesterol testing",
+    ],
+    categories,
+    qualifiers,
+  },
+  dataSource: {
+    optionsLabel:
+      "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
+    options: [
+      {
+        value: DC.ADMINISTRATIVE_DATA,
+        subOptions: [
+          {
+            label: "What is the Administrative Data Source?",
+            options: [
+              { value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM },
+              { value: DC.ADMINISTRATIVE_DATA_OTHER, description: true },
+            ],
+          },
+        ],
+      },
+      { value: DC.ELECTRONIC_CLINIC_DATA_SYSTEMS, description: true },
+      { value: DC.OTHER_DATA_SOURCE, description: true },
+    ],
+  },
+  custom: {
+    calcTotal: true,
+  },
 };
--- services/ui-src/src/measures/2024/IETAD/data.ts
+++ services/ui-src/src/measures/2024/IETAD/data.ts
@@ -1,50 +1,46 @@
-import { DataDrivenTypes } from "shared/types";
-import * as DC from "dataConstants";
 import { getCatQualLabels } from "../rateLabelText";
+import { MeasureTemplateData } from "shared/types/MeasureTemplate";
+import * as DC from "dataConstants";
 
 export const { categories, qualifiers } = getCatQualLabels("IET-AD");
 
-export const data: DataDrivenTypes.PerformanceMeasure = {
-  questionText: [
-    "Percentage of new substance use disorder (SUD) episodes that result in treatment initiation and engagement. Two rates are reported:",
-  ],
-  questionListItems: [
-    "Initiation of SUD Treatment. The percentage of new SUD episodes that result in treatment initiation through an inpatient SUD admission, outpatient visit, intensive outpatient encounter, partial hospitalization, telehealth visit, or medication treatment within 14 days.",
-    "Engagement of SUD Treatment. The percentage of new SUD episodes that have evidence of treatment engagement within 34 days of initiation.",
-  ],
-  categories,
-  qualifiers,
-  measureName: "IET-AD",
-};
-
-export const dataSourceData: DataDrivenTypes.DataSource = {
-  optionsLabel:
-    "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
-  options: [
-    {
-      value: DC.ADMINISTRATIVE_DATA,
-      subOptions: [
-        {
-          label: "What is the Administrative Data Source?",
-          options: [
-            {
-              value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM,
-            },
-            {
-              value: DC.ADMINISTRATIVE_DATA_OTHER,
-              description: true,
-            },
-          ],
-        },
-      ],
-    },
-    {
-      value: DC.ELECTRONIC_HEALTH_RECORDS,
-      description: true,
-    },
-    {
-      value: DC.OTHER_DATA_SOURCE,
-      description: true,
-    },
-  ],
+export const data: MeasureTemplateData = {
+  type: "HEDIS",
+  coreset: "adult",
+  performanceMeasure: {
+    questionText: [
+      "Percentage of new substance use disorder (SUD) episodes that result in treatment initiation and engagement. Two rates are reported:",
+    ],
+    questionListItems: [
+      "Initiation of SUD Treatment. The percentage of new SUD episodes that result in treatment initiation through an inpatient SUD admission, outpatient visit, intensive outpatient encounter, partial hospitalization, telehealth visit, or medication treatment within 14 days.",
+      "Engagement of SUD Treatment. The percentage of new SUD episodes that have evidence of treatment engagement within 34 days of initiation.",
+    ],
+    categories,
+    qualifiers,
+    measureName: "IET-AD",
+  },
+  dataSource: {
+    optionsLabel:
+      "If reporting entities (e.g., health plans) used different data sources, please select all applicable data sources used below.",
+    options: [
+      {
+        value: DC.ADMINISTRATIVE_DATA,
+        subOptions: [
+          {
+            label: "What is the Administrative Data Source?",
+            options: [
+              { value: DC.MEDICAID_MANAGEMENT_INFO_SYSTEM },
+              { value: DC.ADMINISTRATIVE_DATA_OTHER, description: true },
+            ],
+          },
+        ],
+      },
+      { value: DC.ELECTRONIC_HEALTH_RECORDS, description: true },
+      { value: DC.OTHER_DATA_SOURCE, description: true },
+    ],
+  },
+  custom: {
+    calcTotal: true,
+    RateComponent: QMR.IETRate,
+  },
 };
