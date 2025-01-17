1c1ec54cd7d25741939b4a51f423159831a01519
ailZhou <aileen.zhou@coforma.io>
2025-01-17 15:40:08 -0500

    2022 IU-HH

--- services/ui-src/src/measures/2022/IUHH/data.ts
+++ services/ui-src/src/measures/2022/IUHH/data.ts
@@ -1,70 +1,76 @@
-import { DataDrivenTypes } from "shared/types";
+import { ComplexRate } from "components";
 import { getCatQualLabels } from "../rateLabelText";
+import { MeasureTemplateData } from "shared/types/MeasureTemplate";
+import { xNumbersYDecimals } from "utils";
 
 export const { categories, qualifiers } = getCatQualLabels("IU-HH");
 
-const measureName = "IUHH";
-
-const inputFieldNames = [
-  {
-    id: "Number of Enrollee Months",
-    label: "Number of Enrollee Months",
-    text: "Number of Enrollee Months",
+export const data: MeasureTemplateData = {
+  type: "CMS",
+  coreset: "health",
+  performanceMeasure: {
+    customPrompt:
+      "Enter the appropriate data below. Completion of at least one set of Numerator/Denominator/Rate (numeric entry, other than zero) is required.",
+    questionText: [
+      "Rate of acute inpatient care and services (total, maternity, mental and behavioral disorders, surgery, and medicine) per 1,000 enrollee months among health home enrollees.",
+    ],
+    questionListItems: [],
+    categories,
+    qualifiers,
   },
-  { id: "Discharges", label: "Discharges", text: "Discharges" },
-  {
-    id: "Discharges per 1,000 Enrollee Months",
-    label: "Discharges per 1,000 Enrollee Months",
-    text: "Discharges per 1,000 Enrollee Months",
+  custom: {
+    calcTotal: true,
+    customMask: xNumbersYDecimals(12, 1),
+    allowNumeratorGreaterThanDenominator: true,
+    RateComponent: ComplexRate,
   },
-  { id: "Days", label: "Days", text: "Days" },
-  {
-    id: "Days per 1,000 Enrollee Months",
-    label: "Days per 1,000 Enrollee Months",
-    text: "Days per 1,000 Enrollee Months",
+  opm: {
+    inputFieldNames: [
+      {
+        id: "Number of Enrollee Months",
+        label: "Number of Enrollee Months",
+        text: "Number of Enrollee Months",
+      },
+      { id: "Discharges", label: "Discharges", text: "Discharges" },
+      {
+        id: "Discharges per 1,000 Enrollee Months",
+        label: "Discharges per 1,000 Enrollee Months",
+        text: "Discharges per 1,000 Enrollee Months",
+      },
+      { id: "Days", label: "Days", text: "Days" },
+      {
+        id: "Days per 1,000 Enrollee Months",
+        label: "Days per 1,000 Enrollee Months",
+        text: "Days per 1,000 Enrollee Months",
+      },
+      {
+        id: "Average Length of Stay",
+        label: "Average Length of Stay",
+        text: "Average Length of Stay",
+      },
+    ],
+    ndrFormulas: [
+      // Discharges per 1,000 Enrollee Months
+      {
+        num: 1,
+        denom: 0,
+        rate: 2,
+        mult: 1000,
+      },
+      // Days per 1,000 Enrollee Months
+      {
+        num: 3,
+        denom: 0,
+        rate: 4,
+        mult: 1000,
+      },
+      // Average Length of Stay
+      {
+        num: 3,
+        denom: 1,
+        rate: 5,
+        mult: 1,
+      },
+    ],
   },
-  {
-    id: "Average Length of Stay",
-    label: "Average Length of Stay",
-    text: "Average Length of Stay",
-  },
-];
-
-// Rate structure by index in row
-const ndrFormulas = [
-  // Discharges per 1,000 Enrollee Months
-  {
-    num: 1,
-    denom: 0,
-    rate: 2,
-    mult: 1000,
-  },
-  // Days per 1,000 Enrollee Months
-  {
-    num: 3,
-    denom: 0,
-    rate: 4,
-    mult: 1000,
-  },
-  // Average Length of Stay
-  {
-    num: 3,
-    denom: 1,
-    rate: 5,
-    mult: 1,
-  },
-];
-
-export const data: DataDrivenTypes.PerformanceMeasure = {
-  customPrompt:
-    "Enter the appropriate data below. Completion of at least one set of Numerator/Denominator/Rate (numeric entry, other than zero) is required.",
-  questionText: [
-    "Rate of acute inpatient care and services (total, maternity, mental and behavioral disorders, surgery, and medicine) per 1,000 enrollee months among health home enrollees.",
-  ],
-  questionListItems: [],
-  measureName,
-  inputFieldNames,
-  ndrFormulas,
-  categories,
-  qualifiers,
 };
