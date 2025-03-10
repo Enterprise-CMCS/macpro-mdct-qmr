237614c015c1cfcbf131760a14a4fbde4e87eee8
benmartin-coforma <Ben.Martin@coforma.io>
2025-03-10 10:50:12 -0600

    Remove combined rates warning when selecting ECDS in 2025

--- services/ui-src/src/labels/2025/commonQuestionsLabel.tsx
+++ services/ui-src/src/labels/2025/commonQuestionsLabel.tsx
@@ -19,10 +19,6 @@ export const commonQuestionsLabel = {
       "For each data source selected above, describe which reporting entities used each data source (e.g., health plans, FFS). If the data source differed across health plans or delivery systems, identify the number of plans or delivery systems that used each data source (<em>text in this field is included in publicly-reported state-specific comments</em>).",
     otherDataSourceWarning:
       "If you report using Other Data Source, CMS will not be able to produce a combined Medicaid & CHIP rate for public reporting. If the information reported in the Data Source field is accurate, please continue reporting this measure.",
-    warning: {
-      [DC.ELECTRONIC_CLINIC_DATA_SYSTEMS]:
-        "If you report using Electronic Clinical Data Systems (ECDS), CMS will not be able to produce a combined Medicaid & CHIP rate for public reporting for 2025. If the information reported in the Data Source field is accurate, please continue reporting this measure.",
-    },
   },
   DataSourceCahps: {
     describeDataSrc:
