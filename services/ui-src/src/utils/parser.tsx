94f7235567a1cdf6947419bb188c8f6f8723ddcb
ailZhou <aileen.zhou@coforma.io>
2025-04-25 11:28:15 -0400

    Revert "Add a fallback when we cannot parse HTML"
    This reverts commit a4f94a7498c4123bf9d40990e069b571ef30bc16.

--- services/ui-src/src/utils/parser.tsx
+++ services/ui-src/src/utils/parser.tsx
@@ -5,11 +5,6 @@ import { v4 as uuidv4 } from "uuid";
  * Convert labels with HTML tags to JSX elements to be rendered.
  *
  * Doesn't work with `<br>` tags.
- *
- * SECURITY WARNING: Only use this function on input that either
- *  - is trusted code, authored by devs, which can't have been tampered with
- *  - or has been run through DOMPurify, with some very strict settings.
- * Otherwise, it presents similar risks as React's dangerouslySetInnerHTML.
  */
 export const parseLabelToHTML = (label: string) => {
   const attributesOf = (element: Element) => {
@@ -25,21 +20,12 @@ export const parseLabelToHTML = (label: string) => {
     } else if (node instanceof Element) {
       const tagName = node.tagName.toLowerCase();
       const props = {
-        // The random key prevents React warnings in the browser console.
+        // The random key prevents browser warnings.
         key: uuidv4(),
         ...attributesOf(node),
       };
       const children = [...node.childNodes].map(convertNodetoReactElement);
-      try {
-        return React.createElement(tagName, props, children);
-      } catch {
-        /*
-         * Fallback to a bare div if malformed HTML gives nonsensical tag names.
-         * This is needed because DOMParser is extremely lenient compared to
-         * React's createElement, and we need to handle user-authored HTML.
-         */
-        return React.createElement("div", { key: uuidv4() }, children);
-      }
+      return React.createElement(tagName, props, children);
     } else {
       throw new Error(`Unrecognized node type in label:\n${label}`);
     }
