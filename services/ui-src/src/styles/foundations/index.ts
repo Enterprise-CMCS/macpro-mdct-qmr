eb80596d899012233a7d30fcb4b40548467f14b6
ailZhou <aileen.zhou@coforma.io>
2025-07-01 16:09:14 -0400

    Update index.ts

--- services/ui-src/src/styles/foundations/index.ts
+++ services/ui-src/src/styles/foundations/index.ts
@@ -1,13 +1,7 @@
-// import { breakpoints } from "./breakpoints";
 import { fonts } from "./fonts";
-// import { lineHeights } from "./lineHeights";
 import { colors } from "./colors";
-// import { sizes } from "./sizes";
 
 export const foundations = {
-  //   breakpoints: breakpoints,
   colors: colors,
   fonts: fonts,
-  //   lineHeights: lineHeights,
-  //   sizes: sizes,
 };
