1eb4bf4ac1ab9b65ba53854ff6eb3da9e28bf45c
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-08-07 10:22:33 -0400

    Rename CloudFormationHeadersPolicy to CloudFrontHeadersPolicy

--- deployment/stacks/ui.ts
+++ deployment/stacks/ui.ts
@@ -81,7 +81,7 @@ export function createUiComponents(props: CreateUiComponentsProps) {
 
   const securityHeadersPolicy = new cloudfront.ResponseHeadersPolicy(
     scope,
-    "CloudFormationHeadersPolicy",
+    "CloudFrontHeadersPolicy",
     {
       responseHeadersPolicyName: `Headers-Policy-${stage}`,
       comment: "Add Security Headers",
