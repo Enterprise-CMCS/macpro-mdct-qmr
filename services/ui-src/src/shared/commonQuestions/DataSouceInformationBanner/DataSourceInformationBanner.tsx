074f977c99d62efaf611b7f945f9066ec84c83a2
ailZhou <aileen.zhou@coforma.io>
2025-08-18 11:39:14 -0400

    better code for displaying combined rates data source

--- services/ui-src/src/shared/commonQuestions/DataSouceInformationBanner/DataSourceInformationBanner.tsx
+++ services/ui-src/src/shared/commonQuestions/DataSouceInformationBanner/DataSourceInformationBanner.tsx
@@ -123,67 +123,44 @@ export const dataSourceSelections = (
 ) => {
   let selected = [];
 
-  for (const [key, value] of Object.entries(dataSourceSelections)) {
-    if (
-      key === "ElectronicClinicalDataSystemsECDS" &&
-      !(value.description && value.description != "")
-    ) {
-      delete dataSourceSelections[key];
-    }
-  }
+  const remapped = Object.entries(dataSourceSelections).map((selection) => {
+    return { key: selection[0], ...selection?.[1] };
+  });
 
-  //filter the dataSourceSelections object keys that matches the dataSource name
-  const dataSourceKey = Object.keys(dataSourceSelections).filter((key) =>
-    key.split("-")[0].includes(dataSource)
+  //we want only they key for the top layer checkboxes and the current dataSource
+  const parentDataSource = remapped.filter(
+    (selection) =>
+      !selection.key.includes("-") && selection.key.includes(dataSource)
+  );
+  const childDataSource = remapped.filter((selection) =>
+    selection.key.includes("-")
   );
 
-  //we want only they key for the top layer checkboxes
-  const parentKeys = dataSourceKey.filter((key) => !key.includes("-"));
+  const selections = parentDataSource
+    .flatMap((selection) => selection.selected)
+    .filter(isDefined);
+  const descriptions = parentDataSource
+    .flatMap((selection) => selection.description)
+    .filter(isDefined);
 
-  //use the key ids to obtain the values
-  const dataSourceValue = parentKeys.map((key) => dataSourceSelections[key]);
+  //if nothing has been selected or it doesn't have a textbox
+  if (selections.length === 0 && descriptions.length === 0)
+    return ["Not Answered"];
 
-  if (dataSourceKey && dataSourceKey.length > 0) {
-    //if more than one key exist, it is possibly a nested data source
-    if (dataSourceKey.length > 1) {
-      const dataSources = dataSourceValue
-        .map((item) => item.selected ?? item.description)
-        .filter(isDefined)
-        .flat();
+  const filled = selections.map((key) => {
+    //see if there's a textfield associated with this data source selection
+    const textfield = childDataSource.find((source) =>
+      source.key.split("-")[1].includes(key)
+    );
+    const textfieldData = textfield?.description
+      ? `- ${textfield.description}`
+      : "";
+    return lookupDataSource(key) + " " + textfieldData;
+  });
 
-      //addition to handle an option that has both a textbox and checkbox options
-      if (dataSources.every((item) => item === undefined))
-        selected.push("Not Answered");
+  selected.push(...filled);
+  if (descriptions.length > 0) selected.push(...descriptions);
 
-      selected.push(
-        ...dataSources.map((source) => {
-          const sourceKey = dataSourceKey.find((key) => key.includes(source));
-          const formattedSource = lookupDataSource(source);
-          const dash =
-            !formattedSource || !dataSourceSelections[sourceKey!]?.description
-              ? ""
-              : " - ";
-
-          return sourceKey
-            ? `${formattedSource}${dash}${
-                dataSourceSelections[sourceKey]?.description ?? "Not Answered"
-              }`
-            : formattedSource;
-        })
-      );
-    } else {
-      const { description, selected: selectedValue } =
-        dataSourceSelections[dataSourceKey[0]];
-
-      //either description is null or selected is null, only one will exist on the object
-      const value =
-        selectedValue && selectedValue.length > 0
-          ? (selectedValue as any[]).map((item) => lookupDataSource(item))
-          : [!!description ? description : "Not Answered"];
-
-      selected.push(...value);
-    }
-  }
   return selected;
 };
 
