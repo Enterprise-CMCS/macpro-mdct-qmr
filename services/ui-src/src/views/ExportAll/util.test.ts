36e80a0211d9ee4e840c198af3644ded590c5470
benmartin-coforma <Ben.Martin@coforma.io>
2025-08-26 13:52:47 -0600

    Increase test coverage for ExportAll utils

--- services/ui-src/src/views/ExportAll/util.test.ts
+++ services/ui-src/src/views/ExportAll/util.test.ts
@@ -1,4 +1,4 @@
-import { getSpaName } from "./util";
+import { applyPrinceSpecificCss, getSpaName, htmlStringCleanup } from "./util";
 
 describe("ExportAll utils", () => {
   describe("getSpaName", () => {
@@ -29,6 +29,64 @@ describe("ExportAll utils", () => {
 
     it("should find the SPA info for a specific core set ID", () => {
       expect(getSpaName(props)).toBe("DC 24-0020 - Chronic Conditions");
+    });
+  });
+
+  describe("htmlStringCleanup", () => {
+    it("should adjust asset and footer links", () => {
+      if (window.location.host !== "localhost") {
+        throw new Error("Test inconclusive; expected domain to be localhost");
+      }
+      const html = `
+        <img src="https://example.com/assets/foo.png"/>
+        <img src="/assets/bar.jpg"/>
+        <img src="/assets/baz.svg"/>
+        <img src="/footer/quux.gif"/>
+      `;
+      const result = htmlStringCleanup(html);
+      expect(result).toContain("https://example.com/assets/foo.png");
+      expect(result).toContain("https://localhost/assets/bar.jpg");
+      expect(result).toContain("https://localhost/assets/baz.svg");
+      expect(result).toContain("https://localhost/footer/quux.gif");
+    });
+
+    it("should replace certain characters", () => {
+      const html = "‘single’ and “double” quotes \u2013 en dash \u2014 em dash";
+      const result = htmlStringCleanup(html);
+      expect(result).toBe(`'single' and "double" quotes - en dash - em dash`);
+    });
+
+    it("should forbid flex and inline styling for the entire page", () => {
+      const html = "span { display: inline; } .container { display: flex; }";
+      const result = htmlStringCleanup(html);
+      expect(result).toContain("span { display: block; }");
+      expect(result).toContain(".container { display: block; }");
+    });
+
+    it("should turn textareas into paragraphs", () => {
+      const html = `<textarea required class="foo">contents</textarea>`;
+      const result = htmlStringCleanup(html);
+      expect(result).toBe(
+        `<p class="chakra-text replaced-text-area">contents</p>`
+      );
+    });
+
+    it("should turn textareas outside the tab order into hidden paragraphs", () => {
+      const html = `<textarea id="x" tabindex="-1" class="foo">oof</textarea>`;
+      const result = htmlStringCleanup(html);
+      expect(result).toBe(`<p class="hidden-print-items">oof</p>`);
+    });
+  });
+
+  describe("applyPrinceSpecificCss", () => {
+    it("should create a new style tag", () => {
+      const countBefore = document.body.querySelectorAll("style").length;
+
+      applyPrinceSpecificCss();
+
+      const countAfter = document.body.querySelectorAll("style").length;
+
+      expect(countAfter).toBe(countBefore + 1);
     });
   });
 });
