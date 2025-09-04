5207840c706fe13e4efe6a73245be251396fdb02
Filipp Gorbunov <38990142+Filtos@users.noreply.github.com>
2025-09-04 07:20:19 -0700

    CMDCT-4825 - added html sanitizier to banner (#2755)

--- services/ui-src/src/components/Banner/Banner.tsx
+++ services/ui-src/src/components/Banner/Banner.tsx
@@ -1,15 +1,27 @@
 import { Alert } from "@cmsgov/design-system";
 import { BannerData } from "types";
 import { parseLabelToHTML } from "utils";
+import DOMPurify from "dompurify";
+
+const domPurifyBannerConfig = {
+  // Only these tags will be allowed through
+  ALLOWED_TAGS: ["ul", "ol", "li", "a", "#text", "strong", "b", "em"],
+  // On those tags, only these attributes are allowed
+  ALLOWED_ATTR: ["href", "alt"],
+  // If a tag is removed, so will all its child elements & text
+  KEEP_CONTENT: false,
+};
 
 export const Banner = ({ bannerData, ...props }: Props) => {
   return (
     <>
       {bannerData && (
         <Alert heading={bannerData.title} {...props}>
-          <p className="ds-c-alert__text">
-            {parseLabelToHTML(bannerData.description)}
-          </p>
+          <div className="ds-c-alert__text">
+            {parseLabelToHTML(
+              DOMPurify.sanitize(bannerData.description, domPurifyBannerConfig)
+            )}
+          </div>
           {bannerData.link && (
             <p>
               <a href={encodeURI(bannerData.link)}>{bannerData.link}</a>
