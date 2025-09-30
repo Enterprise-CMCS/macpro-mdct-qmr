1e1beff52b65f24e995beb8262f614d070c57076
Filipp Gorbunov <filippgorb@gmail.com>
2025-09-30 09:43:38 -0700

    removed serviceWorker

--- services/ui-src/src/index.tsx
+++ services/ui-src/src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "App";
-import * as serviceWorker from "serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Amplify } from "aws-amplify";
 import "aws-amplify/auth/enable-oauth-listener";
@@ -82,8 +81,3 @@ const ldClientId = config.REACT_APP_LD_SDK_CLIENT;
 })().catch((e) => {
   throw e;
 });
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
