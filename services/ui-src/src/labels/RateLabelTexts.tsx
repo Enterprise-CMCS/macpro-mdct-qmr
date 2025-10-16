0c2bf08b4934bac165d0434032fa9593ad4b653c
ailZhou <aileen.zhou@coforma.io>
2025-10-16 15:09:04 -0400

    duplicate label files for 2026

--- services/ui-src/src/labels/RateLabelTexts.tsx
+++ services/ui-src/src/labels/RateLabelTexts.tsx
@@ -3,3 +3,4 @@ export * as RateLabel2022 from "measures/2022/rateLabelText";
 export * as RateLabel2023 from "measures/2023/rateLabelText";
 export * as RateLabel2024 from "measures/2024/rateLabelText";
 export * as RateLabel2025 from "measures/2025/rateLabelText";
+export * as RateLabel2026 from "measures/2026/rateLabelText";
