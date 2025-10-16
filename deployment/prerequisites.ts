582e840c2470635af8bd82ed3109d13fb9ae0037
Source Files <action@github.com>
2025-10-16 21:04:24 +0000

    sync: update source files

--- deployment/prerequisites.ts
+++ deployment/prerequisites.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+// This file is managed by macpro-mdct-core so if you'd like to change it let's do it there
 import "source-map-support/register";
 import {
   App,
-  Aws,
   aws_apigateway as apigateway,
+  aws_ec2 as ec2,
   aws_iam as iam,
   DefaultStackSynthesizer,
   Stack,
@@ -13,9 +14,11 @@ import {
 import { CloudWatchLogsResourcePolicy } from "./constructs/cloudwatch-logs-resource-policy";
 import { loadDefaultSecret } from "./deployment-config";
 import { Construct } from "constructs";
+import { isLocalStack } from "./local/util";
 
 interface PrerequisiteConfigProps {
   project: string;
+  vpcName: string;
   branchFilter: string;
 }
 
@@ -27,7 +30,14 @@ export class PrerequisiteStack extends Stack {
   ) {
     super(scope, id, props);
 
-    const { project, branchFilter } = props;
+    const { project, vpcName, branchFilter } = props;
+
+    if (!isLocalStack) {
+      const vpc = ec2.Vpc.fromLookup(this, "Vpc", { vpcName });
+      vpc.addGatewayEndpoint("S3Endpoint", {
+        service: ec2.GatewayVpcEndpointAwsService.S3,
+      });
+    }
 
     new CloudWatchLogsResourcePolicy(this, "logPolicy", { project });
 
@@ -67,7 +77,7 @@ export class PrerequisiteStack extends Stack {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
           },
           StringLike: {
-            "token.actions.githubusercontent.com:sub": `repo:Enterprise-CMCS/macpro-mdct-qmr:${branchFilter}`,
+            "token.actions.githubusercontent.com:sub": `repo:Enterprise-CMCS/macpro-mdct-${project}:${branchFilter}`,
           },
         },
         "sts:AssumeRoleWithWebIdentity"
@@ -106,15 +116,20 @@ async function main() {
     }),
   });
 
-  Tags.of(app).add("PROJECT", "QMR");
+  if (!process.env.PROJECT) {
+    throw new Error("PROJECT enironment variable is required but not set");
+  }
 
   const project = process.env.PROJECT!;
-  new PrerequisiteStack(app, "qmr-prerequisites", {
+
+  Tags.of(app).add("PROJECT", project.toUpperCase());
+
+  new PrerequisiteStack(app, `${project}-prerequisites`, {
     project,
     ...(await loadDefaultSecret(project)),
     env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION,
+      region: "us-east-1",
     },
   });
 }
