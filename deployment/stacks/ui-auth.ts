bbec404cf87248ef5c665f568d393e4f3114785f
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-10-27 14:59:20 -0400

    a

--- deployment/stacks/ui-auth.ts
+++ deployment/stacks/ui-auth.ts
@@ -6,8 +6,8 @@ import {
   Aws,
   CfnOutput,
   Duration,
-  custom_resources as cr,
   RemovalPolicy,
+  triggers,
 } from "aws-cdk-lib";
 import { WafConstruct } from "../constructs/waf";
 import { isLocalStack } from "../local/util";
@@ -263,32 +263,10 @@ export function createUiAuthComponents(props: CreateUiAuthComponentsProps) {
   }
 
   if (bootstrapUsersFunction) {
-    const bootstrapUsersInvoke = new cr.AwsCustomResource(
-      scope,
-      "InvokeBootstrapUsersFunction",
-      {
-        onCreate: {
-          service: "Lambda",
-          action: "invoke",
-          parameters: {
-            FunctionName: bootstrapUsersFunction.functionName,
-            InvocationType: "Event",
-            Payload: JSON.stringify({}),
-          },
-          physicalResourceId: cr.PhysicalResourceId.of(
-            `InvokeBootstrapUsersFunction-${stage}`
-          ),
-        },
-        policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
-          resources: [bootstrapUsersFunction.functionArn],
-        }),
-        resourceType: "Custom::InvokeBootstrapUsersFunction",
-      }
-    );
-
-    bootstrapUsersFunction.grantInvoke(bootstrapUsersInvoke.grantPrincipal);
-
-    bootstrapUsersInvoke.node.addDependency(bootstrapUsersFunction);
+    new triggers.Trigger(scope, "InvokeBootstrapUsersFunction", {
+      handler: bootstrapUsersFunction,
+      invocationType: triggers.InvocationType.EVENT,
+    });
   }
 
   new CfnOutput(scope, "CognitoIdentityPoolId", {
