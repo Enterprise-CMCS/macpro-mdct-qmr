54445fbb7a728b56676d6ddcf1c32fe9dd188f17
ailZhou <aileen.zhou@coforma.io>
2025-11-13 16:36:37 -0500

    setting up skeleton for ndrsets unit test

---
+++ services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/ndrSets.test.tsx
@@ -0,0 +1,59 @@
+import { render } from "@testing-library/react";
+import { renderWithHookForm } from "utils/testUtils/reactHookFormRenderer";
+import { NDRSets } from "./ndrSets";
+import { usePerformanceMeasureContext } from "../context";
+import { useAgeGroupsCheckboxes } from "./ndrCheckboxes";
+
+jest.mock("../context", () => ({
+  ...jest.requireActual("../context"),
+  usePerformanceMeasureContext: jest.fn(),
+}));
+
+jest.mock("./ndrCheckboxes", () => ({
+  ...jest.requireActual("./ndrCheckboxes"),
+  useAgeGroupsCheckboxes: () => {
+    return [];
+  },
+  useRenderOPMCheckboxOptions: () => {
+    return [];
+  },
+}));
+
+describe("Test NDRSets component", () => {
+  it("Test Default NDRSet", () => {
+    (usePerformanceMeasureContext as jest.Mock).mockReturnValue({
+      OPM: false,
+      componentFlag: "DEFAULT",
+    });
+    renderWithHookForm(<NDRSets name={""} />);
+  });
+  it("Test AIF NDRSet", () => {
+    (usePerformanceMeasureContext as jest.Mock).mockReturnValue({
+      OPM: false,
+      componentFlag: "AIF",
+    });
+    renderWithHookForm(<NDRSets name={""} />);
+  });
+  it("Test IU NDRSet", () => {
+    (usePerformanceMeasureContext as jest.Mock).mockReturnValue({
+      OPM: false,
+      componentFlag: "IU",
+    });
+    renderWithHookForm(<NDRSets name={""} />);
+  });
+  it("Test PCR NDRSet", () => {
+    (usePerformanceMeasureContext as jest.Mock).mockReturnValue({
+      OPM: false,
+      componentFlag: "PCR",
+      qualifiers: [],
+    });
+    renderWithHookForm(<NDRSets name={""} />);
+  });
+  it("Test Other Performance Measure NDRSet", () => {
+    (usePerformanceMeasureContext as jest.Mock).mockReturnValue({
+      OPM: true,
+      componentFlag: "DEFAULT",
+    });
+    renderWithHookForm(<NDRSets name={""} />);
+  });
+});
