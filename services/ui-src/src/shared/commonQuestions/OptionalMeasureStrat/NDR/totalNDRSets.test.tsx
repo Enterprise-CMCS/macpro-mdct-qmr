e1e21b97d5ac4b085cea77d361825863da038317
ailZhou <aileen.zhou@coforma.io>
2025-11-18 16:04:23 -0500

    more test for additionalCategory and totalNDRSets

--- services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/totalNDRSets.test.tsx
+++ services/ui-src/src/shared/commonQuestions/OptionalMeasureStrat/NDR/totalNDRSets.test.tsx
@@ -1,3 +1,4 @@
+import { screen } from "@testing-library/react";
 import { TotalNDRSets } from "./totalNDRSets";
 import { renderWithHookForm } from "utils";
 
@@ -5,7 +6,6 @@ jest.mock("../context", () => ({
   ...jest.requireActual("../context"),
   usePerformanceMeasureContext: () => {
     return {
-      OPM: false,
       componentFlag: "PCR",
       qualifiers: [
         { id: "mock-id", label: "qual 1" },
@@ -18,6 +18,22 @@ jest.mock("../context", () => ({
 
 describe("Test TotalNDRSets component", () => {
   it("Test TotalNDRSets Render", () => {
-    renderWithHookForm(<TotalNDRSets componentFlag="DEFAULT" name="" />);
+    renderWithHookForm(<TotalNDRSets componentFlag="DEFAULT" name="total" />);
+
+    expect(
+      screen.getByRole("textbox", {
+        name: "total.rates.singleCategory.mock-total.0.numerator",
+      })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("textbox", {
+        name: "total.rates.singleCategory.mock-total.0.denominator",
+      })
+    ).toBeVisible();
+    expect(
+      screen.getByRole("textbox", {
+        name: "total.rates.singleCategory.mock-total.0.rate",
+      })
+    ).toBeVisible();
   });
 });
