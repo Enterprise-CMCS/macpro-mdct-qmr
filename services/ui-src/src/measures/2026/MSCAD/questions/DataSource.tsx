e13911f2a7b226204a71bdaf24b358909eb1b138
Sophie Kwak <sophie.kwak@coforma.io>
2026-05-29 08:47:22 -0700

    missing change added

--- services/ui-src/src/measures/2026/MSCAD/questions/DataSource.tsx
+++ services/ui-src/src/measures/2026/MSCAD/questions/DataSource.tsx
@@ -34,7 +34,7 @@ export const DataSource = ({ type }: DataSourceProps) => {
                 name={DC.DATA_SOURCE_CAHPS_VERSION_OTHER}
                 label={
                   <>
-                    Describe the data source (
+                    Describe the data collection method or data source (
                     <em>
                       text in this field is included in publicly-reported
                       state-specific comments
