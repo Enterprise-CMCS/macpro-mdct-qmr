d7a387090d0ffe3fe0f98b5f90d21f4cc263f395
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2026-06-10 16:27:28 -0400

    fixes based on PR feedback

--- deployment/stacks/deployFrontend.ts
+++ deployment/stacks/deployFrontend.ts
@@ -62,7 +62,6 @@ export function deployFrontend(props: DeployFrontendProps) {
       ],
       destinationBucket: uiBucket,
       distribution,
-      distributionPaths: ["/*"],
       prune: true,
       exclude: ["index.html"],
       cacheControl: [
@@ -80,7 +79,7 @@ export function deployFrontend(props: DeployFrontendProps) {
     ],
     destinationBucket: uiBucket,
     distribution,
-    distributionPaths: ["/index.html"],
+    distributionPaths: ["/*"],
     prune: false,
     cacheControl: [
       s3_deployment.CacheControl.noCache(),
@@ -114,6 +113,7 @@ export function deployFrontend(props: DeployFrontendProps) {
   );
 
   deployTimeConfig.node.addDependency(deployWebsite);
+  deployIndex.node.addDependency(deployTimeConfig);
 
   if (isDev) {
     const denyLogs = new iam.PolicyStatement({
